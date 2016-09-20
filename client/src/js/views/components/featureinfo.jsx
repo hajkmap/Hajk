e2eea00f54ad6f91155395bb704015bbddddb564
sweco-seerem <erik.edmark@sweco.se>
2016-09-20 08:16:16 +0200

    Löser en bugg som har att göra med skalintervallet på utskrifter. Feler berodde på en avrundning till heltal i kartmotorn.

--- client/src/js/views/components/featureinfo.jsx
+++ client/src/js/views/components/featureinfo.jsx
@@ -3,11 +3,11 @@
  * @class
  */
 var FeatureInfo = React.createClass({
-  
+
   objectAsMarkdown: function (o) {
     return Object
       .keys(o)
-      .reduce((str, next, index, arr) => 
+      .reduce((str, next, index, arr) =>
         /^geom$|^geometry$|^the_geom$/.test(arr[index]) ?
         str : str + `**${arr[index]}**: ${o[arr[index]]}\r`
       , "");
@@ -26,9 +26,9 @@ var FeatureInfo = React.createClass({
     ,   icon = ''
     ,   info = this.props.info.information
     ;
-    
-    if (typeof info === 'object') {      
-      info = this.objectAsMarkdown(this.props.info.information);      
+
+    if (typeof info === 'object') {
+      info = this.objectAsMarkdown(this.props.info.information);
     }
 
     html = marked(info, { sanitize: false, gfm: true, breaks: true });
