61fdad62e18364595faaac298426acff450e72c3
Peter Thorin <peter.thorin@decerno.se>
2017-05-30 11:09:30 +0200

    Remove the use of "magic constant" 49.65 when exporting to PDF.
    Why:
    
    * The measurements calculated when using the "magic" constant did not add up to the real world measurements of papers.
    
    This change adresses the need by:
    
    * Calculating the size of papers based on their size in inch multplied by the resolution in DPI.
    * Adding a new function called getPreviewPaperMeasures for showing the extent in the map, using a calculated DPI of about 90 (25.4 / 0.28).
    
    Tickets:
    
    * https://trello.com/c/1gzLsGIW/81-felaktig-begaran-av-bildstorlek-vid-utskrift

--- client/src/js/views/panels/exportpanel.jsx
+++ client/src/js/views/panels/exportpanel.jsx
@@ -126,13 +126,23 @@ var ExportPdfSettings = React.createClass({
       }
     }
 
-    var dpi    = (25.4 / .28)
-    ,   width  = pageSize(this.getFormat()).width
+    var width  = pageSize(this.getFormat()).width
     ,   height = pageSize(this.getFormat()).height;
 
     return {
-      width: ((width / 25.4) * dpi),
-      height:  ((height / 25.4) * dpi)
+      width: ((width / 25.4)),
+      height:  ((height / 25.4))
+    };
+  },
+
+  getPreviewPaperMeasures: function() { 
+    var size = this.getPaperMeasures()
+    ,   inchInMillimeter = 25.4
+    ,   defaultPixelSizeInMillimeter = 0.28
+    ,   dpi = (inchInMillimeter / defaultPixelSizeInMillimeter); // ~90
+    return {
+      width: size.width * dpi,
+      height:  size.height * dpi
     };
   },
 
@@ -182,7 +192,7 @@ var ExportPdfSettings = React.createClass({
 
   addPreview: function (map) {
     var scale  = this.getScale()
-    ,   paper  = this.getPaperMeasures()
+    ,   paper  = this.getPreviewPaperMeasures()
     ,   center = this.props.model.getPreviewFeature() ?
                  ol.extent.getCenter(this.props.model.getPreviewFeature().getGeometry().getExtent()) :
                  map.getView().getCenter();
