11e927cf516309261b3e0045b75d008b5cd3268a
sweco-seviax <victor.axelsson@sweco.se>
2017-09-14 09:14:41 +0200

    Hotfix för selektion vid överlagring. felaktig csproj fixad.

--- client/src/js/oloverrides/wmsurl.js
+++ client/src/js/oloverrides/wmsurl.js
@@ -1,4 +1,3 @@
-
 /**
  * Dessa funktioner är överlagrade från openlayers eftersom det finns en bugg i openlayers implementation
  * som ej tar hänsyn till den förändrade ordningen av koordinater för BBOX i wms-lager av version 1.3.0, vilket skapade ett fel i
@@ -6,58 +5,57 @@
  **/
 
 function getRequestUrl(tileCoord, tileSize, tileExtent, pixelRatio, projection, params) {
-			var urls = this.urls;
-			if (!urls) {
-				return undefined;
-			}
-			const isV13 = params.VERSION == '1.3.0';
-			params['WIDTH'] = tileSize[0];
-			params['HEIGHT'] = tileSize[1];
-			
-			params[isV13 ? 'CRS' : 'SRS'] = projection.getCode();
-			
-			if(!params.hasOwnProperty('STYLES')){
-				params['STYLES'] = '';
-			}
-			
-			if (pixelRatio != 1) {
-			switch (this.get('serverType').toLowerCase()) {
-				case 'geoserver':
-					var dpi = (90 * pixelRatio + 0.5) | 0;
-					if (params.hasOwnProperty('FORMAT_OPTIONS')) {
-						params['FORMAT_OPTIONS'] += ';dpi:' + dpi;
-					} else {
-						params['FORMAT_OPTIONS'] = 'dpi:' + dpi;
-					}
-					break;
-				case 'arcgis':
-					params['MAP_RESOLUTION'] = 90 * pixelRatio;
-					break;
+	var urls = this.urls;
+	if (!urls) {
+		return undefined;
+	}
+	const isV13 = params.VERSION == '1.3.0';
+	params['WIDTH'] = tileSize[0];
+	params['HEIGHT'] = tileSize[1];
+
+	params[isV13 ? 'CRS' : 'SRS'] = projection.getCode();
+
+	if (!params.hasOwnProperty('STYLES')) {
+		params['STYLES'] = '';
+	}
+
+	if (pixelRatio != 1) {
+		switch (this.get('serverType').toLowerCase()) {
+			case 'geoserver':
+				var dpi = (90 * pixelRatio + 0.5) | 0;
+				if (params.hasOwnProperty('FORMAT_OPTIONS')) {
+					params['FORMAT_OPTIONS'] += ';dpi:' + dpi;
+				} else {
+					params['FORMAT_OPTIONS'] = 'dpi:' + dpi;
+				}
+				break;
+			case 'arcgis':
+				params['MAP_RESOLUTION'] = 90 * pixelRatio;
+				break;
 				// case ol.source.WMSServerType.CARMENTA_SERVER:
 				// case ol.source.WMSServerType.QGIS:
 				// 	params['DPI'] = 90 * pixelRatio;
 				// 	break;
-				default:
-					// ol.asserts.assert(false, 52); // Unknown `serverType` configured
-					break;
-				}
-			}
-			
-		//	var axisOrientation = projection.getAxisOrientation();
-			var bbox = tileExtent;
-			//if (this.v13_ && axisOrientation.substr(0, 2) == 'ne') 
-			if (isV13) {
-				var tmp;
-				tmp = tileExtent[0];
-				bbox[0] = tileExtent[1];
-				bbox[1] = tmp;
-				tmp = tileExtent[2];
-				bbox[2] = tileExtent[3];
-				bbox[3] = tmp;
-			}
-			params['BBOX'] = bbox.join(',');
-			var url = urls[0];
-			return url + '?' + $.param(params);
+			default:
+				// ol.asserts.assert(false, 52); // Unknown `serverType` configured
+				break;
+		}
+	}
+
+	//	var axisOrientation = projection.getAxisOrientation();
+	var bbox = tileExtent;
+	if (this.v13_ && axisOrientation.substr(0, 2) == 'ne' || isSweref(projection)) {
+		var tmp;
+		tmp = tileExtent[0];
+		bbox[0] = tileExtent[1];
+		bbox[1] = tmp;
+		tmp = tileExtent[2];
+		bbox[2] = tileExtent[3];
+		bbox[3] = tmp;
+	}
+	params['BBOX'] = bbox.join(',');
+	var url = urls[0];
+	return url + '?' + $.param(params);
 }
 
 module.exports = {
@@ -83,21 +81,21 @@ module.exports = {
 		let coordinate = options.coordinate;
 		let params = options.params;
 
-		if(options.isSingleTile || version != "1.3.0") {
+		if (options.isSingleTile || version != "1.3.0") {
 			return source.getGetFeatureInfoUrl(coordinate,
-        resolution,
-        projection,
-        params);
+				resolution,
+				projection,
+				params);
 		}
 
 		let tileGrid = source.getTileGrid();
-		if(!tileGrid)  return undefined;
+		if (!tileGrid) return undefined;
 
-			// if (!tileGrid) {
+		// if (!tileGrid) {
 		// 	tileGrid = source.getTileGridForProjection(projection);
 		// }
 		var tileCoord = tileGrid.getTileCoordForCoordAndResolution(
-      coordinate, resolution);
+			coordinate, resolution);
 
 		if (tileGrid.getResolutions().length <= tileCoord[0]) {
 			return undefined;
@@ -105,10 +103,10 @@ module.exports = {
 		var tileResolution = tileGrid.getResolution(tileCoord[0]);
 		var tileExtent = tileGrid.getTileCoordExtent(tileCoord, this.tmpExtent_);
 		var tileSize = ol.size.toSize(
-				tileGrid.getTileSize(tileCoord[0]), this.tmpSize);
+			tileGrid.getTileSize(tileCoord[0]), this.tmpSize);
 		let x = Math.floor((coordinate[0] - tileExtent[0]) / tileResolution);
 		let y = Math.floor((tileExtent[3] - coordinate[1]) / tileResolution)
-		
+
 		let baseParams = {
 			'SERVICE': 'WMS',
 			'VERSION': version,
@@ -126,58 +124,63 @@ module.exports = {
 		// //No gutter support
 		return getRequestUrl.call(source, tileCoord, tileSize, tileExtent,
 			1, projection, baseParams);
-	
+
 
 	},
-	
-	customGetTileUrl: function(tileCoord, pixelRatio, projection) {
-			let tileGrid = this.getTileGrid();
-			if (!tileGrid) {
-				tileGrid = this.getTileGridForProjection(projection);
-			}
-		
-			if (tileGrid.getResolutions().length <= tileCoord[0]) {
-				return undefined;
-			}
 
-			if (pixelRatio != 1 && (!this.hidpi_ || this.serverType_ === undefined)) {
-				pixelRatio = 1;
-			}
-		
-			let tileResolution = tileGrid.getResolution(tileCoord[0]);
-			let tileExtent = tileGrid.getTileCoordExtent(tileCoord, this.tmpExtent_);
-			let tileSize = ol.size.toSize(
-					tileGrid.getTileSize(tileCoord[0]), this.tmpSize);
-			
-			let gutter = this.gutter_ || 0; //Vi stödjer ej gutter just nu
-			if (gutter !== 0) {
-				tileSize = ol.size.buffer(tileSize, gutter, this.tmpSize);
-				tileExtent = ol.extent.buffer(tileExtent,
-						tileResolution * gutter, tileExtent);
-			}
+	customGetTileUrl: function (tileCoord, pixelRatio, projection) {
+		let tileGrid = this.getTileGrid();
+		if (!tileGrid) {
+			tileGrid = this.getTileGridForProjection(projection);
+		}
 
-			if (pixelRatio != 1) {
-				tileSize = ol.size.scale(tileSize, pixelRatio, this.tmpSize);
-			}
+		if (tileGrid.getResolutions().length <= tileCoord[0]) {
+			return undefined;
+		}
 
-			let baseParams = Object.assign({
-				'SERVICE': 'WMS',
-				'VERSION': '1.1.0',
-				'REQUEST': 'GetMap',
-				'FORMAT': 'image/png',
-				'TRANSPARENT': true
-			}, this.getParams());
-			
-			//{ SRS: undefined, CRS: undefined}
-			if(baseParams.hasOwnProperty("SRS"))
-				delete baseParams.SRS;
+		if (pixelRatio != 1 && (!this.hidpi_ || this.serverType_ === undefined)) {
+			pixelRatio = 1;
+		}
 
-			if(baseParams.hasOwnProperty("CRS"))
-				delete baseParams.CRS;
-		
-			return getRequestUrl.call(this, tileCoord, tileSize, tileExtent,
-				pixelRatio, projection, baseParams);
-	}};
+		let tileResolution = tileGrid.getResolution(tileCoord[0]);
+		let tileExtent = tileGrid.getTileCoordExtent(tileCoord, this.tmpExtent_);
+		let tileSize = ol.size.toSize(
+			tileGrid.getTileSize(tileCoord[0]), this.tmpSize);
 
+		let gutter = this.gutter_ || 0; //Vi stödjer ej gutter just nu
+		if (gutter !== 0) {
+			tileSize = ol.size.buffer(tileSize, gutter, this.tmpSize);
+			tileExtent = ol.extent.buffer(tileExtent,
+				tileResolution * gutter, tileExtent);
+		}
 
+		if (pixelRatio != 1) {
+			tileSize = ol.size.scale(tileSize, pixelRatio, this.tmpSize);
+		}
 
+		let baseParams = Object.assign({
+			'SERVICE': 'WMS',
+			'VERSION': '1.1.0',
+			'REQUEST': 'GetMap',
+			'FORMAT': 'image/png',
+			'TRANSPARENT': true
+		}, this.getParams());
+
+		//{ SRS: undefined, CRS: undefined}
+		if (baseParams.hasOwnProperty("SRS"))
+			delete baseParams.SRS;
+
+		if (baseParams.hasOwnProperty("CRS"))
+			delete baseParams.CRS;
+
+		return getRequestUrl.call(this, tileCoord, tileSize, tileExtent,
+			pixelRatio, projection, baseParams);
+	}
+};
+
+const isSweref = (projection) => {
+	let code = projection.getCode().split(':');
+	code = parseInt(code[code.length - 1]);
+	let isSweref = isNaN(code) ? false : code >= 3006 && code <= 3018;
+	return isSweref;
+};
