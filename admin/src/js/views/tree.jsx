0d6efa8d8f28c5a37d9c4419d4d9f58527e221b5
sweco-seviax <victor.axelsson@sweco.se>
2018-01-24 14:34:26 +0100

    tar bort kommentar

--- admin/src/js/views/tree.jsx
+++ admin/src/js/views/tree.jsx
@@ -14,7 +14,6 @@ export default class Tree extends Component {
 	}
 
 	toggleHide (layerId) {
-		//this.setState({ checked: !this.state.checked });
 		if (this.refs.hasOwnProperty(layerId)) {
 			let layer = this.refs[layerId];
 		
@@ -28,57 +27,38 @@ export default class Tree extends Component {
 	}
 
 	buildList () {
-        return (
-					<div>
-						<label>Tillgängliga söktjänster</label>
-            <div className="layer-list">
-							<ul>
-								{ this.props.layers.map((layer) => {
-									return (
-										<li key={ layer.id } className="">
-											<input 
-												type="checkbox"
-												ref={ "cb_" + layer.id }
-												className={ "checkbox_" + layer.id } 
-												onChange={ (e) => { 
-													this.props.handleAddSearchable(e, layer);
-													this.toggleHide(layer.id)
-												}} />&nbsp;
-											<label>{ layer.caption }</label>
-											<input ref={layer.id} type="text" hidden="true" placeholder="Tillträde" onChange={ (e) => { this.props.handleAddSearchable(e, layer) } } />
-										</li>
-									);
-								})}
-							</ul>
-            </div>
-					</div>
-        );
-    }
+		return (
+			<div>
+				<label>Tillgängliga söktjänster</label>
+				<div className="layer-list">
+					<ul>
+						{ this.props.layers.map((layer) => {
+							return (
+								<li key={ layer.id }>
+									<input 
+										type="checkbox"
+										ref={ "cb_" + layer.id }
+										className={ "checkbox_" + layer.id } 
+										onChange={ (e) => { 
+											this.props.handleAddSearchable(e, layer);
+											this.toggleHide(layer.id)
+										}} />&nbsp;
+									<label>{ layer.caption }</label>
+									<input ref={layer.id} type="text" hidden="true" placeholder="Tillträde" onChange={ (e) => { this.props.handleAddSearchable(e, layer) } } />
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		);
+	}
   
-    render() {
-				if (this.props.layers) {
-					return <div>{this.buildList()}</div>
-				} else {
-					return <div></div>;
-				}
-    }
+	render() {
+		if (this.props.layers) {
+			return <div>{this.buildList()}</div>
+		} else {
+			return <div></div>;
+		}
+	}
 }
-
-{/* <li key={"fromCapability_" + i} className="list-item">
-            <div className="col-md-6 overflow-hidden">
-              <input
-                ref={layer.Name}
-                id={"layer" + i}
-                type="checkbox"
-                data-type="wms-layer"
-                checked={this.state.addedLayers.find(l => l === layer.Name)}
-                onChange={(e) => {
-                  this.setState({ 'caption': layer.Title });
-                  this.setState({ 'content': layer.Abstract });
-                  this.appendLayer(e, layer.Name);
-                }} />&nbsp;
-                <label htmlFor={"layer" + i}>{layer.Name}</label>{title}
-            </div>
-            <i style={{ display: "none" }} className={classNames} onClick={(e) => this.describeLayer(e, layer.Name)}></i>
-            <span className={queryableIcon + " col-md-1"} />
-          </li> */}
