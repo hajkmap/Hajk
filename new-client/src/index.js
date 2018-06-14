d787b5b98168b385b53e2f5db90124a8d7b9cc25
Jacob Wodzyński <jacob.wod@gmail.com>
2018-06-14 16:34:12 +0200

    Rewrote inital fetch First appConfig.json is fetched. It provides the mapserviceBasepath, which later is used in the following fetches.

--- new-client/src/index.js
+++ new-client/src/index.js
@@ -1,34 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './../node_modules/ol/ol.css';
-import './index.css'
-import App from './views/app.js';
-import registerServiceWorker from './registerServiceWorker';
-import buildConfig from './buildConfig.json';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./../node_modules/ol/ol.css";
+import "./index.css";
+import App from "./views/app.js";
+import registerServiceWorker from "./registerServiceWorker";
+import buildConfig from "./buildConfig.json";
 
-Promise.all([
-  fetch('http://giscloud.se/mapservice/config/layers'),
-  fetch('http://giscloud.se/mapservice/config/map_1'),
-  fetch('config.json')
-]).then(([layersResponse, mapConfigResponse, appConfigResponse]) => {
-  Promise.all([
-    layersResponse.json(),
-    mapConfigResponse.json(),
-    appConfigResponse.json()])
-  .then(([layers, mapConfig, appConfig]) => {
-
-    console.log(layers, mapConfig, appConfig);
-    ReactDOM.render(<App activeTools={buildConfig.activeTools} config={appConfig} />, document.getElementById('root'));
-    registerServiceWorker();
-
-  }).catch(err => {
-
-    console.error("Parse error: ", err);
-    ReactDOM.render(<div>Fel</div>, document.getElementById('root'));
-    registerServiceWorker();
-
+fetch("appConfig.json")
+  .then(appConfigResponse => {
+    appConfigResponse.json().then(appConfig => {
+      console.log(appConfig);
+      Promise.all([
+        fetch(`${appConfig.mapserviceBase}/config/layers`),
+        fetch(`${appConfig.mapserviceBase}/config/op`)
+      ])
+        .then(([layersResponse, mapConfigResponse]) => {
+          Promise.all([layersResponse.json(), mapConfigResponse.json()])
+            .then(([layers, mapConfig]) => {
+              console.log(layers, mapConfig);
+              ReactDOM.render(
+                <App
+                  activeTools={buildConfig.activeTools}
+                  config={appConfig}
+                />,
+                document.getElementById("root")
+              );
+              registerServiceWorker();
+            })
+            .catch(err => {
+              console.error("Parse error: ", err);
+              ReactDOM.render(<div>Fel</div>, document.getElementById("root"));
+              registerServiceWorker();
+            });
+        })
+        .catch(err => {
+          console.error("Network error: ", err);
+        });
+    });
+  })
+  .catch(err => {
+    console.error("Network error: ", err);
   });
-
-}).catch(err => {
-  console.error("Network error: ", err);
-});
