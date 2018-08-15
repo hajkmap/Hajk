8c793172ae25a533d9eb2150ea10f222ad6be21f
erikedmark <erik.edmark@wsp.com>
2018-08-15 13:36:39 +0200

    Add new class to handle the OPTION-request submitted by chrome when cors is activated.

---
+++ backend/mapservice/Attributes/AllowCrossSiteJsonAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapService.Attributes
+{
+	public class CORSActionFilter : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			if (filterContext.HttpContext.Request.HttpMethod == "OPTIONS")
+			{				
+				filterContext.Result = new EmptyResult();
+			}
+			else
+			{
+				base.OnActionExecuting(filterContext);
+			}
+		}
+	}
+}
