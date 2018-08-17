05134ef57c65a7c39bf0c3af8d538632b5c41c1d
erikedmark <erik.edmark@wsp.com>
2018-08-17 13:51:24 +0200

    Add Layer list item view. This is little of a reimplementation, but it can be reused in other components. And it is super fast.

---
+++ new-admin/src/views/components/LayerListItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Component } from 'react';
+
+class LayerListItem extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {      
+      checked: props.checked
+    };
+  }
+
+  componentDidRecieveProps() {
+  }
+
+  componentDidUpdate() {    
+  }  
+
+  componentDidMount() {          
+  }
+
+  render() {    
+    var checkedClass = this.state.checked ? "fa fa-check-square-o" : "fa fa-square-o";
+    return (                      
+      <li className="layer-list-item" key={this.props.layer.id} onClick={() => { 
+          var checked = !this.state.checked;
+          this.setState({checked: checked });
+          this.props.onChange(checked);
+        }}>
+        <span>
+          <span className={checkedClass}></span>
+          <span className="label">{this.props.layer.name}</span>
+        </span>
+      </li>
+    );
+  }
+
+}
+
+export default LayerListItem;
