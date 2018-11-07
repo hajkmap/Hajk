0c0bb9cb0dd90c69e6a6dbc277e4772e52edb0fe
Jacob Wodzyński <jacob.wod@gmail.com>
2018-11-07 10:30:31 +0100

    Let's use SVG icon in PanelHeader as well

--- new-client/src/components/PanelHeader.js
+++ new-client/src/components/PanelHeader.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import CloseIcon from "@material-ui/icons/Close";
 
 const styles = theme => ({
   header: {
     padding: "10px",
     background: "#efefef",
     borderBottom: "1px solid #ccc",
-    cursor: "pointer",
     userSelect: "none"
   },
   headerText: {
     marginBottom: 0
+  },
+  icon: {
+    cursor: "pointer"
   }
 });
 
@@ -20,9 +23,7 @@ class PanelHeader extends Component {
     return (
       <div className={classes.header}>
         <div className="icons pull-right">
-          <i className="material-icons" onClick={this.props.onClose}>
-            close
-          </i>
+          <CloseIcon onClick={this.props.onClose} className={classes.icon} />
         </div>
         <h2 className={classes.headerText}>{this.props.title}</h2>
       </div>
