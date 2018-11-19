66cb79dcde52341198bfa926ccf2b1d9e82cccae
erikedmark <erik.j.edmark@gmail.com>
2018-11-19 05:55:29 +0100

    Better user experience for the breadcrumbs blugin

--- new-client/src/plugins/simple_layerswitcher/components/BreadCrumb.js
+++ new-client/src/plugins/simple_layerswitcher/components/BreadCrumb.js
@@ -1,43 +1,81 @@
 import React, { Component } from "react";
 import CloseIcon from "@material-ui/icons/Close";
-import "./BreadCrumb.css";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  breadCrumb: {
+    background: "white",
+    margin: "3px",
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "10px",
+    border: "1px solid #ccc"
+  },
+  part: {
+    margin: "0 5px",
+    display: "flex"
+  },
+  icon: {
+    cursor: "pointer"
+  }
+});
 
 class BreadCrumb extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { hidden: false };
   }
-  /**
-   * Triggered when the component is successfully mounted into the DOM.
-   * @instance
-   */
-  componentDidMount() {}
 
-  /**
-   * Triggered when component unmounts.
-   * @instance
-   */
-  componentWillUnmount() {}
+  setLayerOpacity = layer => event => {
+    this.setState(
+      {
+        hidden: !this.state.hidden
+      },
+      () => {
+        layer.setOpacity(this.state.hidden ? 0 : 1);
+      }
+    );
+  };
 
   setLayerVisibility = layer => event => {
+    if (layer.get("visible")) {
+      layer.setOpacity(1);
+    }
     layer.set("visible", !layer.get("visible"));
   };
 
   render() {
+    const { classes } = this.props;
+    const { hidden } = this.state;
     return (
-      <div className="bread-crumb">
-        <div className="bread-crumb-header">
-          <span>
-            {this.props.title}{" "}
-            <CloseIcon
-              className="pointer"
-              onClick={this.setLayerVisibility(this.props.layer)}
-            />
-          </span>
+      <div className={classes.breadCrumb}>
+        <div className={classes.part}>
+          <div className={classes.part}>
+            {!hidden ? (
+              <VisibilityIcon
+                className={classes.icon}
+                onClick={this.setLayerOpacity(this.props.layer)}
+              />
+            ) : (
+              <VisibilityOffIcon
+                className={classes.icon}
+                onClick={this.setLayerOpacity(this.props.layer)}
+              />
+            )}
+          </div>
+          <div className={classes.part}>{this.props.title}</div>
+        </div>
+        <div className={classes.part}>
+          <CloseIcon
+            className={classes.icon}
+            onClick={this.setLayerVisibility(this.props.layer)}
+          />
         </div>
       </div>
     );
   }
 }
 
-export default BreadCrumb;
+export default withStyles(styles)(BreadCrumb);
--- new-client/src/plugins/simple_layerswitcher/components/LayerItem.js
+++ new-client/src/plugins/simple_layerswitcher/components/LayerItem.js
@@ -37,7 +37,8 @@ const styles = theme => ({
       "0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12)"
   },
   rightIcon: {
-    marginLeft: theme.spacing.unit
+    marginLeft: theme.spacing.unit,
+    fontSize: "16px"
   }
 });
 
@@ -158,7 +159,7 @@ class LayerItem extends Component {
           <>
             <div>
               Innehåll från denna kategori finns benämnt i följande kapitel i
-              Översiktsplanen:
+              översiktsplanen:
             </div>
             <ul className={classes.links}>
               {chaptersWithLayer.map((chapter, i) => {
