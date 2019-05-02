7b9e59cd97ad988b5e06e6427a879d6ea19c9306
Tobias Johansson <tobias.johansson@sbk.goteborg.se>
2019-05-02 15:46:49 +0200

    Added two new components for Polygonsearch and SearchWithSelection

---
+++ new-client/src/plugins/search/components/SearchWithPolygonButton.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import LoupeIcon from "@material-ui/icons/Loupe";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import Snackbar from "@material-ui/core/Snackbar";
+import { createPortal } from "react-dom";
+
+const styles = theme => {
+  return {
+    button: {},
+    anchorOriginBottomCenter: {
+      bottom: "60px"
+    }
+  };
+};
+
+class SearchWithPolygon extends React.Component {
+  state = {
+    active: false
+  };
+
+  render() {
+    const { classes, buttonText } = this.props;
+    return (
+      <>
+        <Tooltip title="Visa påverkan inom ett område">
+          <Button
+            className={classes.button}
+            color={this.state.active ? "primary" : "default"}
+            onClick={() => {
+              this.setState(
+                {
+                  active: !this.state.active
+                },
+                () => {
+                  this.props.model.toggleDraw(
+                    this.state.active,
+                    { freehand: false, type: "Polygon" },
+                    { useSearchWithinSources: false },
+                    featureCollections => {
+                      this.props.onComplete(featureCollections);
+                    }
+                  );
+                  this.setState({
+                    active: false
+                  });
+                  if (this.state.active) {
+                    this.props.localObserver.publish("minimizeWindow", true);
+                  }
+                }
+              );
+            }}
+          >
+            <LoupeIcon />
+            &nbsp; {buttonText}
+          </Button>
+        </Tooltip>
+        {createPortal(
+          <Snackbar
+            className={classes.anchorOriginBottomCenter}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            open={this.state.active ? true : false}
+            ContentProps={{
+              "aria-describedby": "message-id"
+            }}
+            message={<span id="message-id">Rita en polygon</span>}
+          />,
+          document.getElementById("map-overlay")
+        )}
+      </>
+    );
+  }
+}
+
+export default withStyles(styles)(SearchWithPolygon);
---
+++ new-client/src/plugins/search/components/SearchWithSelectionButton.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import LoupeIcon from "@material-ui/icons/Loupe";
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import Snackbar from "@material-ui/core/Snackbar";
+import { createPortal } from "react-dom";
+
+const styles = theme => {
+  return {
+    button: {},
+    anchorOriginBottomCenter: {
+      bottom: "60px"
+    }
+  };
+};
+
+class SearchWithSelectionButton extends React.Component {
+  state = {
+    active: false
+  };
+
+  render() {
+    const { classes, buttonText } = this.props;
+    return (
+      <>
+        <Tooltip title="SÖK">
+          <Button
+            className={classes.button}
+            color={this.state.active ? "primary" : "default"}
+            onClick={() => {
+              this.setState(
+                {
+                  active: !this.state.active
+                },
+                () => {
+                  this.props.model.doSearch();
+                  if (this.state.active) {
+                    this.props.localObserver.publish("minimizeWindow", true);
+                  }
+                }
+              );
+            }}
+          >
+            <LoupeIcon />
+            &nbsp; {buttonText}
+          </Button>
+        </Tooltip>
+        <Tooltip title="Visa påverkan inom ett område">
+          <Button
+            className={classes.button}
+            color={this.state.active ? "primary" : "default"}
+            onClick={() => {
+              this.setState(
+                {
+                  active: !this.state.active
+                },
+                () => {
+                  this.props.model.selectGeometriesForSpatialSearch(
+                    this.state.active
+                  );
+
+                  /*this.props.model.toggleDraw(
+                    this.state.active,
+                    { freehand: true, type: "Circle" },
+                    { useSearchWithinSources: true },
+                    featureCollections => {
+                      this.setState({
+                        active: false
+                      });
+                      this.props.onSearchWithin(
+                        featureCollections.map(
+                          collection => collection.source.id
+                        )
+                      );
+                    }
+                  );*/
+                  if (this.state.active) {
+                    this.props.localObserver.publish("minimizeWindow", true);
+                  }
+                }
+              );
+            }}
+          >
+            <LoupeIcon />
+            &nbsp; {buttonText}
+          </Button>
+        </Tooltip>
+        {createPortal(
+          <Snackbar
+            className={classes.anchorOriginBottomCenter}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            open={this.state.active ? true : false}
+            ContentProps={{
+              "aria-describedby": "message-id"
+            }}
+            message={
+              <span id="message-id">
+                Dra ut en radie i kartan för att välja storlek på sökområde.
+              </span>
+            }
+          />,
+          document.getElementById("map-overlay")
+        )}
+      </>
+    );
+  }
+}
+
+export default withStyles(styles)(SearchWithSelectionButton);
