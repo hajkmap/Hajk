567dffb90252dc894ce8f1217cb1cc71c812336c
Johansson <t.johansson@sweco.se>
2019-05-22 08:01:58 +0200

    Added dropdown to searchcontainer for spatial search selection

--- new-client/src/plugins/search/components/SpatialSearch.js
+++ new-client/src/plugins/search/components/SpatialSearch.js
@@ -15,7 +15,10 @@ import Icon from "@material-ui/core/Icon";
 import TouchApp from "@material-ui/icons/TouchApp";
 import { createPortal } from "react-dom";
 import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
 
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
 const styles = theme => ({
   root: {
     display: "flex",
@@ -35,84 +38,98 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3
   }
 });
-
+const ITEM_HEIGHT = 48;
 const names = ["Polygon", "Radie", "Välj"];
 
 class SpatialSearchOptions extends React.Component {
   state = {
-    activeTool: ""
+    activeTool: "",
+    anchorEl: null
   };
 
-  handleChange = event => {
-    this.props.onChange(event);
-    this.setState({ [event.target.name]: event.target.value });
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
   };
 
   render() {
     const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
 
     return (
       <div className={classes.root}>
-        <FormControl className={classes.formControl}>
-          <Select
-            disableUnderline
-            value={this.state.activeTool}
-            onChange={this.handleChange}
-            displayEmpty
-            name="activeTool"
-            MenuProps={{
-              getContentAnchorEl: null,
-              anchorOrigin: {
-                vertical: "bottom",
-                horizontal: "left"
-              }
-            }}
-            className={classes.select}
+        <IconButton
+          aria-label="More"
+          aria-owns={open ? "long-menu" : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          <CropSquare />
+          <ArrowDownward style={{ fontSize: 10 }} />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          getContentAnchorEl={null}
+          open={open}
+          onClose={this.handleClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: 200
+            }
+          }}
+        >
+          <MenuItem
+            onClick={this.handleClose}
+            value={"polygon"}
+            alignItems="center"
           >
-            <MenuItem value="" disabled>
-              <Stop />
-            </MenuItem>
-            <MenuItem value={"polygon"} alignItems="center">
-              <Tooltip title="Visa påverkan inom ett område">
-                <Grid container direction="row" alignItems="center">
-                  <Grid item>
-                    <TripOrigin />
-                  </Grid>
-                  <Grid item />
-                  <Typography variant="subtitle1" gutterBottom>
-                    Rita
-                  </Typography>
+            <Tooltip title="Visa påverkan inom ett område">
+              <Grid container direction="row" alignItems="center">
+                <Grid item>
+                  <TripOrigin />
                 </Grid>
-              </Tooltip>
-            </MenuItem>
-            <MenuItem value={"selection"} alignItems="center">
-              <Tooltip title="Visa påverkan inom ett område">
-                <Grid container direction="row" alignItems="center">
-                  <Grid item>
-                    <CropSquare />
-                  </Grid>
-                  <Grid item />
-                  <Typography variant="subtitle1" gutterBottom>
-                    Rita
-                  </Typography>
+                <Grid item />
+                <Typography variant="subtitle1" gutterBottom>
+                  Rita
+                </Typography>
+              </Grid>
+            </Tooltip>
+          </MenuItem>
+          <MenuItem value={"selection"} alignItems="center">
+            <Tooltip title="Visa påverkan inom ett område">
+              <Grid container direction="row" alignItems="center">
+                <Grid item>
+                  <CropSquare />
                 </Grid>
-              </Tooltip>
-            </MenuItem>
-            <MenuItem value={"within"} alignItems="center">
-              <Tooltip title="Visa påverkan inom ett område">
-                <Grid container direction="row" alignItems="center">
-                  <Grid item>
-                    <TouchApp />
-                  </Grid>
-                  <Grid item />
-                  <Typography variant="subtitle1" gutterBottom>
-                    Markera
-                  </Typography>
+                <Grid item />
+                <Typography variant="subtitle1" gutterBottom>
+                  Rita
+                </Typography>
+              </Grid>
+            </Tooltip>
+          </MenuItem>
+          <MenuItem value={"within"} alignItems="center">
+            <Tooltip title="Visa påverkan inom ett område">
+              <Grid container direction="row" alignItems="center">
+                <Grid item>
+                  <TouchApp />
                 </Grid>
-              </Tooltip>
-            </MenuItem>
-          </Select>
-        </FormControl>
+                <Grid item />
+                <Typography variant="subtitle1" gutterBottom>
+                  Markera
+                </Typography>
+              </Grid>
+            </Tooltip>
+          </MenuItem>
+        </Menu>
+
         {createPortal(
           <Snackbar
             className={classes.anchorOriginBottomCenter}
