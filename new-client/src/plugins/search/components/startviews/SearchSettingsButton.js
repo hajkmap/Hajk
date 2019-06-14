e1ad931e90f84a3de766accf0b18cf735ed90661
Tobias Johansson <tobias.johansson@sbk.goteborg.se>
2019-06-14 14:56:36 +0200

    WIP - Skeleton for search options

--- new-client/src/plugins/search/components/startviews/SearchSettingsButton.js
+++ new-client/src/plugins/search/components/startviews/SearchSettingsButton.js
@@ -2,15 +2,57 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import Settings from "@material-ui/icons/Settings";
+import Window from "../../../../components/Window.js";
+import SettingsMenu from "./SettingsMenu";
 
-const styles = theme => {};
+const styles = theme => ({
+  root: {
+    zIndex: 1
+  }
+});
 
 class SearchSettingsButton extends React.Component {
-  render() {
+  state = {
+    open: false
+  };
+
+  handleClick = event => {
+    this.setState({
+      open: true
+    });
+  };
+
+  close = e => {
+    this.setState({ open: false });
+  };
+
+  renderWindow() {
     return (
-      <IconButton>
-        <Settings />
-      </IconButton>
+      <Window
+        globalObserver={null}
+        title={"(NOT IMPLEMENTED!!"}
+        onClose={this.close}
+        open={this.state.open}
+        height={400}
+        width="400px"
+        top={145}
+        left={5}
+        mode={""}
+      >
+        <SettingsMenu />
+      </Window>
+    );
+  }
+
+  render() {
+    console.log(this.state, "this.state");
+    return (
+      <div>
+        <IconButton onClick={this.handleClick}>
+          <Settings />
+        </IconButton>
+        {this.renderWindow()}
+      </div>
     );
   }
 }
