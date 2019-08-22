f69eff91839c4fef39374e8a61812b7ba47b1560
Jacob Wodzyński <jacob.wod@gmail.com>
2019-08-22 14:57:33 +0200

    Minor addition to Dummy plugin

--- new-client/src/plugins/dummy/DummyView.js
+++ new-client/src/plugins/dummy/DummyView.js
@@ -37,13 +37,35 @@ class DummyView extends React.PureComponent {
   };
 
   // Event handler for a button that shows a global info message when clicked
-  handleMessageClick = () => {
+  showDefaultSnackbar = () => {
     this.props.enqueueSnackbar("Yay, a nice message with default styling.");
   };
 
-  handleMessageClick2 = () => {
+  // A more complicate snackbar example, this one with an action button and persistent snackbar
+  showAdvancedSnackbar = () => {
+    const action = key => (
+      <>
+        <Button
+          onClick={() => {
+            alert(`I belong to snackbar with key ${key}`);
+          }}
+        >
+          {"Alert"}
+        </Button>
+        <Button
+          onClick={() => {
+            this.props.closeSnackbar(key);
+          }}
+        >
+          {"Dismiss"}
+        </Button>
+      </>
+    );
+
     this.props.enqueueSnackbar("Oops, a message with error styling!", {
-      variant: "error"
+      variant: "error",
+      persist: true,
+      action
     });
   };
 
@@ -53,8 +75,10 @@ class DummyView extends React.PureComponent {
         <Button onClick={this.buttonClick}>
           {this.state.test || "Click to change state"}
         </Button>
-        <Button onClick={this.handleMessageClick}>Show default snackbar</Button>
-        <Button onClick={this.handleMessageClick2}>Show error snackbar</Button>
+        <Button onClick={this.showDefaultSnackbar}>
+          Show default snackbar
+        </Button>
+        <Button onClick={this.showAdvancedSnackbar}>Show error snackbar</Button>
       </>
     );
   }
