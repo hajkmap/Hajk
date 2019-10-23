fb825c63cea90d3b475591b62b4ebfa7102d8d3a
Jacob Wodzyński <jacob.wod@gmail.com>
2019-10-23 13:33:23 +0200

    Greatly improved Dummy example: - Added propTypes and defaultProps (#301) - More comments - Example on how to use JSS

--- new-client/src/plugins/dummy/DummyView.js
+++ new-client/src/plugins/dummy/DummyView.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
-import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 import { withSnackbar } from "notistack";
+import Button from "@material-ui/core/Button";
 
-const styles = theme => ({});
+// Define JSS styles that will be used in this component.
+// Examle below utilizes the very powerful "theme" object
+// that gives access to some constants, see: https://material-ui.com/customization/default-theme/
+const styles = theme => ({
+  buttonWithBottomMargin: {
+    marginBottom: theme.spacing(2)
+  }
+});
 
 class DummyView extends React.PureComponent {
-  state = {};
+  // Initialize state - this is the correct way of doing it nowadays.
+  state = {
+    counter: 0
+  };
+
+  // propTypes and defaultProps are static properties, declared
+  // as high as possible within the component code. They should
+  // be immediately visible to other devs reading the file,
+  // since they serve as documentation.
+  static propTypes = {
+    model: PropTypes.object.isRequired,
+    app: PropTypes.object.isRequired,
+    localObserver: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
+    enqueueSnackbar: PropTypes.func.isRequired,
+    closeSnackbar: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {};
 
   constructor(props) {
     // If you're not using some of properties defined below, remove them from your code.
@@ -30,9 +55,9 @@ class DummyView extends React.PureComponent {
     );
 
     // And we can of course access this component's state
-    this.setState({
-      test: "State changed!"
-    });
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
   };
 
   // Event handler for a button that shows a global info message when clicked
@@ -69,22 +94,45 @@ class DummyView extends React.PureComponent {
   };
 
   render() {
+    const { classes } = this.props;
     return (
       <>
-        <Button onClick={this.buttonClick}>
-          {this.state.test || "Click to change state"}
+        <Button
+          className={classes.buttonWithBottomMargin}
+          variant="contained"
+          fullWidth={true}
+          // onChange={(e) => { console.log(e) }}
+          // ^ Don't do this. Closures here are inefficient. Use the below:
+          onClick={this.buttonClick}
+        >
+          {this.state.test ||
+            `Clicked ${this.state.counter} ${
+              this.state.counter === 1 ? "time" : "times"
+            }`}
         </Button>
-        <Button onClick={this.showDefaultSnackbar}>
+        <Button
+          className={classes.buttonWithBottomMargin}
+          variant="contained"
+          fullWidth={true}
+          onClick={this.showDefaultSnackbar}
+        >
           Show default snackbar
         </Button>
-        <Button onClick={this.showAdvancedSnackbar}>Show error snackbar</Button>
+        <Button
+          className={classes.buttonWithBottomMargin}
+          variant="contained"
+          fullWidth={true}
+          onClick={this.showAdvancedSnackbar}
+        >
+          Show error snackbar
+        </Button>
       </>
     );
   }
 }
 
-DummyView.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
+// Exporting like this adds some props to DummyView.
+// withStyles will add a 'classes' prop, while withSnackbar
+// adds to functions (enqueueSnackbar() and closeSnackbar())
+// that can be used throughout the Component.
 export default withStyles(styles)(withSnackbar(DummyView));
--- new-client/src/plugins/dummy/dummy.js
+++ new-client/src/plugins/dummy/dummy.js
@@ -1,34 +1,71 @@
 // Generic imports – all plugins need these
 import React from "react";
+import PropTypes from "prop-types";
 import BaseWindowPlugin from "../BaseWindowPlugin";
 
-// All plugins need some icon – make sure to pick a relevant one from Material UI Icons
-import BugReportIcon from "@material-ui/icons/BugReport";
-
-// Finally plugin-specific imports. Most plugins will need Model, View and Observer.
+// Plugin-specific imports. Most plugins will need a Model, View and Observer
+// but make sure to only create and import whatever you need.
 import DummyModel from "./DummyModel";
 import DummyView from "./DummyView";
 import Observer from "react-event-observer";
 
+// All plugins will need to display an icon. Make sure to pick a relevant one from Material UI Icons.
+import BugReportIcon from "@material-ui/icons/BugReport";
+
+/**
+ * @summary Main class for the Dummy plugin.
+ * @description The purpose of having a Dummy plugin is to exemplify
+ * and document how plugins should be constructed in Hajk.
+ * The plugins can also serve as a scaffold for other plugins: simply
+ * copy the directory, rename it and all files within, and change logic
+ * to create the plugin you want to.
+ *
+ * @class Dummy
+ * @extends {React.PureComponent}
+ */
 class Dummy extends React.PureComponent {
+  // Initialize state - this is the correct way of doing it nowadays.
+  state = {};
+
+  // propTypes and defaultProps are static properties, declared
+  // as high as possible within the component code. They should
+  // be immediately visible to other devs reading the file,
+  // since they serve as documentation.
+  // If unsure of what propTypes are or how to use them, see https://reactjs.org/docs/typechecking-with-proptypes.html.
+  static propTypes = {
+    app: PropTypes.object.isRequired,
+    map: PropTypes.object.isRequired,
+    options: PropTypes.object.isRequired
+  };
+
+  static defaultProps = {
+    options: {}
+  };
+
   constructor(props) {
+    // Unsure why we write "super(props)"?
+    // See https://overreacted.io/why-do-we-write-super-props/ for explanation.
     super(props);
 
-    // We can setup a local observer to allow sending messages between here and model/view.
+    // We can setup a local observer to allow sending messages between here (controller) and model/view.
     // It's called 'localObserver' to distinguish it from AppModel's globalObserver.
+    // API docs, see: https://www.npmjs.com/package/react-event-observer
     this.localObserver = Observer();
 
-    // Once created, we can observer listen for events with a distinct name. When events
-    // are published form somewhere else, the callback here will be run.
-    this.localObserver.subscribe("dummyEvent", message => {});
+    // Once created, the observer can subscribe to events with a distinct name. In this example
+    // we subscribe to "dummyEvent" When "dummyEvent" is published (from somewhere else)
+    // the callback below will be run, with "message" as an optional param.
+    this.localObserver.subscribe("dummyEvent", message => {
+      console.log(message);
+    });
 
     // Initiate a model. Although optional, it will probably be used for all except the most simple plugins.
     // In this example, we make our localObserver available for the model as well. This makes it possible
     // to send events between model and main plugin controller.
     this.dummyModel = new DummyModel({
-      map: props.map,
+      localObserver: this.localObserver,
       app: props.app,
-      localObserver: this.localObserver
+      map: props.map
     });
   }
 
@@ -48,15 +85,13 @@ class Dummy extends React.PureComponent {
     return (
       <BaseWindowPlugin
         {...this.props} // Pass on all props
-        type="Dummy"
+        type="Dummy" // Unique name - each plugin needs one. Upper-case first letter, must be valid JS variable name
         custom={{
           icon: <BugReportIcon />, // Custom icon for this plugin
-          title: "Dummy", // Custom title, etc
-          description: "En kort beskrivning som visas i widgeten",
+          title: "Dummy", // Custom title (will be shown in Window's toolbar and on the Drawer/Widget button)
+          description: "En kort beskrivning som visas i widgeten", // Shown on Widget button
           height: 450, // Custom height/width etc | Use "auto" for automatic or leave undefined
-          width: 400,
-          top: undefined, // If undefined, it will fallback to BaseWindowPlugin's defaults
-          left: undefined
+          width: 400
         }}
       >
         {/* This is the child object of BaseWindowPlugin. It will be displayed
