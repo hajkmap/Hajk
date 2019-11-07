hajkmap/Hajk

commit a7a04a5067cf3183b4fb05cdbc413788e8bb424c

Add option to configure cookie notice message - closes #292
Add options in admin to configure message text and URL. Also update the default URL since the previous was outdated.

new-client/src/components/App.js
@@ -385,6 +385,9 @@ class App extends React.PureComponent {
             defaultCookieNoticeMessage={
               this.props.config.mapConfig.map.defaultCookieNoticeMessage
             }
+            defaultCookieNoticeUrl={
+              this.props.config.mapConfig.map.defaultCookieNoticeUrl
+            }
           />
           <Alert
             open={this.state.alert}

new-client/src/components/CookieNotice.js
@@ -12,7 +12,8 @@ const useStyles = makeStyles(theme => ({
 }));
 
 CookieNotice.propTypes = {
-  cookieNoticeMessage: string,
+  defaultCookieNoticeMessage: string,
+  defaultCookieNoticeUrl: string,
   globalObserver: object.isRequired
 };
 
@@ -23,7 +24,12 @@ CookieNotice.propTypes = {
  * @returns React.Component
  */
 function CookieNotice({
-  cookieNoticeMessage = "Vi använder cookies för att följa upp användandet och ge en bra upplevelse av kartan. Du kan blockera cookies i webbläsaren men då visas detta meddelande igen.",
+  defaultCookieNoticeMessage = defaultCookieNoticeMessage
+    ? defaultCookieNoticeMessage
+    : "Vi använder cookies för att följa upp användandet och ge en bra upplevelse av kartan. Du kan blockera cookies i webbläsaren men då visas detta meddelande igen.",
+  defaultCookieNoticeUrl = defaultCookieNoticeUrl
+    ? defaultCookieNoticeUrl
+    : "https://pts.se/sv/bransch/regler/lagar/lag-om-elektronisk-kommunikation/kakor-cookies/",
   globalObserver
 }) {
   const classes = useStyles();
@@ -33,8 +39,7 @@ function CookieNotice({
     <>
       <Button
         onClick={() => {
-          window.location =
-            "https://webbriktlinjer.se/lagkrav/kaklagen-i-praktiken/";
+          window.open(defaultCookieNoticeUrl);
         }}
         variant="text"
         className={classes.textButton}
@@ -58,7 +63,7 @@ function CookieNotice({
   // Display only if "cookie" hasn't been set yet
   parseInt(window.localStorage.getItem("cookieNoticeShown")) !== 1 &&
     globalObserver.subscribe("appLoaded", () => {
-      enqueueSnackbar(cookieNoticeMessage, {
+      enqueueSnackbar(defaultCookieNoticeMessage, {
         persist: true,
         anchorOrigin: {
           vertical: "bottom",

backend/mapservice/Controllers/ConfigController.cs
@@ -469,7 +469,9 @@ namespace MapService.Controllers
 						2.0,
 						1.0,
 						0.5
-					}
+					},
+                    defaultCookieNoticeMessage = "",
+                    defaultCookieNoticeUrl = ""
 				},
 				projections = new List<Projection>()
 				{

backend/mapservice/Models/MapSetting.cs
@@ -49,5 +49,9 @@ namespace MapService.Models
         public bool drawerPermanent { get; set; }
 
         public Colors colors { get; set; }
+
+        public string defaultCookieNoticeMessage { get; set; }
+
+        public string defaultCookieNoticeUrl { get; set; }
     }
 }

new-admin/src/views/mapoptions.jsx
@@ -57,7 +57,9 @@ class MapOptions extends Component {
         title: config.title ? config.title : "",
         geoserverLegendOptions: config.geoserverLegendOptions
           ? config.geoserverLegendOptions
-          : ""
+          : "",
+        defaultCookieNoticeMessage: config.defaultCookieNoticeMessage,
+        defaultCookieNoticeUrl: config.defaultCookieNoticeUrl
       });
       this.validate();
     });
@@ -91,7 +93,13 @@ class MapOptions extends Component {
       mapcleaner: mapConfig.mapcleaner,
       drawerVisible: mapConfig.drawerVisible,
       drawerPermanent: mapConfig.drawerPermanent,
-      geoserverLegendOptions: mapConfig.geoserverLegendOptions
+      geoserverLegendOptions: mapConfig.geoserverLegendOptions,
+      defaultCookieNoticeMessage: mapConfig.defaultCookieNoticeMessage
+        ? mapConfig.defaultCookieNoticeMessage
+        : "Vi använder cookies för att följa upp användandet och ge en bra upplevelse av kartan. Du kan blockera cookies i webbläsaren men då visas detta meddelande igen.",
+      defaultCookieNoticeUrl: mapConfig.defaultCookieNoticeUrl
+        ? mapConfig.defaultCookieNoticeUrl
+        : "https://pts.se/sv/bransch/regler/lagar/lag-om-elektronisk-kommunikation/kakor-cookies/"
     });
   }
 
@@ -244,6 +252,10 @@ class MapOptions extends Component {
         config.drawerVisible = this.getValue("drawerVisible");
         config.drawerPermanent = this.getValue("drawerPermanent");
         config.geoserverLegendOptions = this.getValue("geoserverLegendOptions");
+        config.defaultCookieNoticeMessage = this.getValue(
+          "defaultCookieNoticeMessage"
+        );
+        config.defaultCookieNoticeUrl = this.getValue("defaultCookieNoticeUrl");
         this.props.model.updateMapConfig(config, success => {
           var msg = success
             ? "Uppdateringen lyckades."
@@ -504,6 +516,49 @@ class MapOptions extends Component {
                 className={this.getValidationClass("geoserverLegendOptions")}
                 onChange={e => {
                   this.setState({ geoserverLegendOptions: e.target.value });
+                }}
+              />
+            </div>
+            <div>
+              <label>
+                Cookies-meddelande{" "}
+                <i
+                  className="fa fa-question-circle"
+                  data-toggle="tooltip"
+                  title="Ange meddelande för cookies"
+                />
+              </label>
+              <textarea
+                type="text"
+                ref="input_defaultCookieNoticeMessage"
+                value={this.state.defaultCookieNoticeMessage}
+                className={this.getValidationClass(
+                  "defaultCookieNoticeMessage"
+                )}
+                onChange={e => {
+                  this.setState(
+                    { defaultCookieNoticeMessage: e.target.value },
+                    () => this.validateField("defaultCookieNoticeMessage")
+                  );
+                }}
+              />
+            </div>
+            <div>
+              <label>
+                Cookies-länk{" "}
+                <i
+                  className="fa fa-question-circle"
+                  data-toggle="tooltip"
+                  title="Valfri URL som öppnas med knappen 'Mer information'"
+                />
+              </label>
+              <input
+                type="text"
+                ref="input_defaultCookieNoticeUrl"
+                value={this.state.defaultCookieNoticeUrl}
+                className={this.getValidationClass("defaultCookieNoticeUrl")}
+                onChange={e => {
+                  this.setState({ defaultCookieNoticeUrl: e.target.value });
                 }}
               />
             </div>

commit 6639e40c39e8549ec1575937e9b9296dfc62eb89

Properly closed #292 by correcting bugs introduced in a7a04a5067cf3183b4fb05cdbc413788e8bb424c

new-client/src/components/App.js
@@ -371,6 +371,18 @@ class App extends React.PureComponent {
     // If clean===true, some components won't be rendered below
     const clean = config.mapConfig.map.clean;
 
+    const defaultCookieNoticeMessage =
+      this.props.config.mapConfig.map.defaultCookieNoticeMessage
+        .toString()
+        .trim().length > 0
+        ? this.props.config.mapConfig.map.defaultCookieNoticeMessage
+        : undefined;
+    const defaultCookieNoticeUrl =
+      this.props.config.mapConfig.map.defaultCookieNoticeUrl.toString().trim()
+        .length > 0
+        ? this.props.config.mapConfig.map.defaultCookieNoticeUrl
+        : undefined;
+
     return (
       <SnackbarProvider
         maxSnack={3}
@@ -382,12 +394,8 @@ class App extends React.PureComponent {
         <>
           <CookieNotice
             globalObserver={this.globalObserver}
-            defaultCookieNoticeMessage={
-              this.props.config.mapConfig.map.defaultCookieNoticeMessage
-            }
-            defaultCookieNoticeUrl={
-              this.props.config.mapConfig.map.defaultCookieNoticeUrl
-            }
+            defaultCookieNoticeMessage={defaultCookieNoticeMessage}
+            defaultCookieNoticeUrl={defaultCookieNoticeUrl}
           />
           <Alert
             open={this.state.alert}

new-client/src/components/CookieNotice.js
@@ -24,14 +24,11 @@ CookieNotice.propTypes = {
  * @returns React.Component
  */
 function CookieNotice({
-  defaultCookieNoticeMessage = defaultCookieNoticeMessage
-    ? defaultCookieNoticeMessage
-    : "Vi använder cookies för att följa upp användandet och ge en bra upplevelse av kartan. Du kan blockera cookies i webbläsaren men då visas detta meddelande igen.",
-  defaultCookieNoticeUrl = defaultCookieNoticeUrl
-    ? defaultCookieNoticeUrl
-    : "https://pts.se/sv/bransch/regler/lagar/lag-om-elektronisk-kommunikation/kakor-cookies/",
+  defaultCookieNoticeMessage = "Vi använder cookies för att följa upp användandet och ge en bra upplevelse av kartan. Du kan blockera cookies i webbläsaren men då visas detta meddelande igen.",
+  defaultCookieNoticeUrl = "https://pts.se/sv/bransch/regler/lagar/lag-om-elektronisk-kommunikation/kakor-cookies/",
   globalObserver
 }) {
+  console.log("defaultCookieNoticeUrl: ", defaultCookieNoticeUrl);
   const classes = useStyles();
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 