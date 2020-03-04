677ac43c832869a2087878fedc060fbd7e165add
Tobias Johansson <t.johansson@sweco.se>
2020-03-04 15:15:02 +0100

    WIP - adding structure and FAB to document window
    WIP - adding structure and FAB to document window

--- new-client/src/plugins/documenthandler/documentWindow/DocumentViewer.js
+++ new-client/src/plugins/documenthandler/documentWindow/DocumentViewer.js
@@ -2,8 +2,18 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { withSnackbar } from "notistack";
 import { Typography } from "@material-ui/core";
+import Fab from "@material-ui/core/Fab";
+import NavigationIcon from "@material-ui/icons/Navigation";
+import Grid from "@material-ui/core/Grid";
 
-const styles = theme => ({});
+const styles = theme => ({
+  gridContainer: {
+    height: "100%"
+  },
+  test: {
+    overflow: "scroll"
+  }
+});
 
 class DocumentViewer extends React.PureComponent {
   state = {};
@@ -14,15 +24,95 @@ class DocumentViewer extends React.PureComponent {
 
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
     this.model = this.props.model;
     this.localObserver = this.props.localObserver;
     this.globalObserver = this.props.app.globalObserver;
+    console.log(this.props, "myRef");
   }
 
+  scrollToTop = () => {};
+
   render() {
+    const { classes, baseWindow } = this.props;
+    console.log(baseWindow, "baseWindow");
     return (
       <>
-        <Typography>dsadsadas</Typography>
+        <Grid className={classes.gridContainer} container>
+          <Fab
+            style={{ position: "fixed", bottom: 10, right: 10 }}
+            size="small"
+            color="primary"
+            aria-label="goto-top"
+            onClick={this.scrollToTop}
+          >
+            <NavigationIcon />
+          </Fab>
+          <Grid item>
+            <Typography>
+              dsadsadasdsadsadasdsadsadasdsadsad
+              adasdsadsadasdsadsadasdsadsadasds
+              asdsadsadasdsadsadasdsadsadasdsadsds
+              adsadasdsadsadasdsadsadasdsadsadasdsadasdsa
+              dsadasdsadsadasdsadsadasdsadsadasdsads
+              dsadsadasdsadsadasdsadsadasdsadsadasdsdasdsadsadasds
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+              adsadasdsadsadasdsadsadasdsadsadasdsaddsadas
+              dsadsadasdsadsadasdsadsadasdsadsadasdsads
+            </Typography>
+          </Grid>
+        </Grid>
       </>
     );
   }
--- new-client/src/plugins/documenthandler/documentWindow/DocumentWindowBase.js
+++ new-client/src/plugins/documenthandler/documentWindow/DocumentWindowBase.js
@@ -4,8 +4,31 @@ import { withSnackbar } from "notistack";
 import BaseWindowPlugin from "../../BaseWindowPlugin";
 import DocumentViewer from "./DocumentViewer";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
+import Fab from "@material-ui/core/Fab";
+import NavigationIcon from "@material-ui/icons/Navigation";
 
 const styles = theme => ({});
+
+const BaseWindow = props => {
+  return (
+    <BaseWindowPlugin
+      {...props}
+      type="DocumentViewer"
+      custom={{
+        icon: <MenuBookIcon />,
+        title: "Documents",
+        description: "En kort beskrivning som visas i widgeten",
+        height: 1000,
+        width: 400,
+        draggingEnabled: false,
+        resizingEnabled: false,
+        allowMaximizedWindow: false
+      }}
+    >
+      <DocumentViewer app={props.app} />
+    </BaseWindowPlugin>
+  );
+};
 
 class DocumentWindowBase extends React.PureComponent {
   state = {
@@ -19,7 +42,8 @@ class DocumentWindowBase extends React.PureComponent {
   constructor(props) {
     super(props);
     this.model = this.props.model;
-
+    this.baseWindow = null;
+    console.log(React.createRef(this), "ref");
     this.bindSubscriptions();
   }
 
@@ -33,24 +57,8 @@ class DocumentWindowBase extends React.PureComponent {
   };
 
   render() {
-    return (
-      <BaseWindowPlugin
-        {...this.props}
-        type="DocumentViewer"
-        custom={{
-          icon: <MenuBookIcon />,
-          title: "Documents",
-          description: "En kort beskrivning som visas i widgeten",
-          height: 700,
-          width: 400,
-          draggingEnabled: false,
-          resizingEnabled: false,
-          allowMaximizedWindow: false
-        }}
-      >
-        <DocumentViewer app={this.props.app} />
-      </BaseWindowPlugin>
-    );
+    console.log(BaseWindow(this.props), "BaseWindow");
+    return BaseWindow(this.props);
   }
 }
 
