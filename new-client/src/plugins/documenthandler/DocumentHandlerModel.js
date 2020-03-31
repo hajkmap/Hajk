5a8872b00327312ff85ff2489ba620b0d11fd45c
Tobias Johansson <t.johansson@sweco.se>
2020-03-31 16:10:14 +0200

    Added comments and dynamically fetching open sans
    Added comments and dynamically fetching open sans

--- new-client/src/plugins/documenthandler/DocumentHandlerModel.js
+++ new-client/src/plugins/documenthandler/DocumentHandlerModel.js
@@ -67,7 +67,12 @@ export default class DocumentHandlerModel {
       });
     }
   }
-
+  /**
+   * @summary method to find a certain chapter in a fetched document with a unique headerGUID.
+   * it is used when user clicks a text-link to a certain document and header in the text in the document-window
+   *
+   * @memberof DocumentHandlerModel
+   */
   getHeaderRef = (activeDocument, headerIdentifierToFind) => {
     var foundChapter;
     activeDocument.chapters.some(chapter => {
@@ -77,6 +82,12 @@ export default class DocumentHandlerModel {
     return foundChapter;
   };
 
+  /**
+   * @summary Dynamically adds a React referenceObject to each chapter in fetched document.
+   * it is used by scrollIntoView in the plugin to be able to scroll to a certain chapter/header
+   *
+   * @memberof DocumentHandlerModel
+   */
   setScrollReferences = chapter => {
     chapter.scrollRef = React.createRef();
     if (chapter.chapters.length > 0) {
@@ -96,6 +107,12 @@ export default class DocumentHandlerModel {
     }
   }
 
+  /**
+   * @summary Dynamically adds a object to each chapter in fetched document.
+   * it is used to keep track of the parent when changing menu-views in application
+   *
+   * @memberof DocumentHandlerModel
+   */
   setParentChapter(chapter, parent) {
     chapter.parent = parent;
     if (chapter.chapters.length > 0) {
