7b2fa7f77b6d0bf790557ea06e303bf1a32b546b
Jacob Wodzyński <jacob.wodzynski@halmstad.se>
2020-06-03 09:22:47 +0200

    Revert "Fix/523 update anchor on scroll event"

--- new-client/src/plugins/anchor/AnchorModel.js
+++ new-client/src/plugins/anchor/AnchorModel.js
@@ -5,9 +5,15 @@ class AnchorModel {
     this.cqlFilters = {};
     this.map = settings.map;
     this.localObserver = settings.localObserver;
-
-    this.map.getView().on("change", this.update);
-
+    const update = e => {
+      setTimeout(() => {
+        if (!e.target.getAnimating() && !e.target.getInteracting()) {
+          this.localObserver.publish("mapUpdated", this.getAnchor());
+        }
+      }, 0);
+    };
+    this.map.getView().on("change:resolution", update);
+    this.map.getView().on("change:center", update);
     this.map
       .getLayers()
       .getArray()
@@ -20,7 +26,7 @@ class AnchorModel {
           this.localObserver.publish("mapUpdated", this.getAnchor());
         });
 
-        // Update anchor each time an underlying Source changes in some way (could be new CQL params, for example).
+        // Update anchor each time an underlaying Source changes in some way (could be new CQL params, for example).
         layer.getSource().on("change", ({ target }) => {
           if (typeof target.getParams !== "function") return;
           const cqlFilterForCurrentLayer = target.getParams()?.CQL_FILTER;
@@ -29,12 +35,6 @@ class AnchorModel {
         });
       });
   }
-
-  update = e => {
-    // If view is still animating, postpone updating Anchor
-    e.target.getAnimating() === false &&
-      this.localObserver.publish("mapUpdated", this.getAnchor());
-  };
 
   getMap() {
     return this.map;
