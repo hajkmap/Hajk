a39a66d2a8aa82b4c2180e5cb40e7c9c9944937c
Tobias Johansson <t.johansson@sweco.se>
2020-06-18 11:46:49 +0200

    Responsive pictures in modal
    Responsive pictures in modal

--- new-client/src/plugins/documenthandler/documentWindow/ImagePopupModal.js
+++ new-client/src/plugins/documenthandler/documentWindow/ImagePopupModal.js
@@ -3,27 +3,20 @@ import { withStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import CardMedia from "@material-ui/core/CardMedia";
 import CloseIcon from "@material-ui/icons/Close";
+import Dialog from "@material-ui/core/Dialog";
 import Modal from "@material-ui/core/Modal";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import ModalImage from "react-modal-image";
+import { Lightbox } from "react-modal-image";
+import Paper from "@material-ui/core/Paper";
 
 const mapDiv = document.getElementById("map");
 const blurCss = "filter : blur(7px)";
 
 const styles = theme => ({
-  imageContainer: {
-    position: "absolute",
-    [theme.breakpoints.up("sm")]: {
-      left: "50%",
-      top: "50%",
-      transform: "translate(-50%, -50%)"
-    },
-
-    [theme.breakpoints.down("xs")]: {
-      left: theme.spacing(2),
-      right: theme.spacing(2),
-      top: "50%",
-      transform: "translate(0%, -50%)"
-    }
-  },
   closeButton: {
     position: "absolute",
     right: theme.spacing(0.5),
@@ -31,7 +24,29 @@ const styles = theme => ({
   }
 });
 
+function PaperComponent(props) {
+  return (
+    <>
+      <IconButton
+        style={{
+          backgroundColor: "white",
+          position: "absolute",
+          top: 20,
+          right: 20,
+          zIndex: 100
+        }}
+      >
+        <CloseIcon></CloseIcon>
+      </IconButton>
+      <Paper {...props}></Paper>
+    </>
+  );
+}
+
 class ImagePopupModal extends React.PureComponent {
+  imageHeight = 0;
+  imageWidth = 0;
+
   removeMapBlur = () => {
     mapDiv.removeAttribute("style", blurCss);
   };
@@ -44,30 +59,36 @@ class ImagePopupModal extends React.PureComponent {
     this.removeMapBlur();
   };
 
+  componentDidMount = () => {};
+
   render() {
-    const { open, close, classes, image } = this.props;
+    const { open, close, classes, image, imageType } = this.props;
     open ? this.addMapBlur() : this.removeMapBlur();
+    if (image != null) {
+      console.log(imageType, "imageType");
+    }
+
     return (
-      <Modal
-        aria-label="Modal showing picture"
-        onBackdropClick={close}
-        open={open}
-      >
-        <div className={classes.imageContainer}>
-          <IconButton
-            onClick={close}
-            className={classes.closeButton}
-            aria-label="close menu"
-          >
-            <CloseIcon></CloseIcon>
-          </IconButton>
-          <CardMedia
-            aria-label="popup image"
-            component="img"
-            image={image}
-          ></CardMedia>
-        </div>
-      </Modal>
+      <>
+        <Dialog
+          maxWidth="lg"
+          onBackdropClick={close}
+          PaperComponent={PaperComponent}
+          PaperProps={{
+            component: "img",
+            src: image,
+            style: {
+              maxHeight: "90%",
+              maxWidth: "100%",
+              overflow: "auto",
+              backgroundColor: "transparent",
+              boxShadow: "none",
+              objectFit: "contain"
+            }
+          }}
+          open={open}
+        ></Dialog>
+      </>
     );
   }
 }
