a9ae6425ccde578e12dc151b579b45e753ecef49
Hitomi Westrin <hitomi.westrin@gmail.com>
2020-08-26 16:44:37 +0200

    Added so WFS layers can be used for as map layers. It is needed for the WKT-based layers to work in the Collector tool

--- new-client/src/models/AppModel.js
+++ new-client/src/models/AppModel.js
@@ -7,6 +7,7 @@ import { isMobile } from "./../utils/IsMobile.js";
 import WMSLayer from "./layers/WMSLayer.js";
 import WMTSLayer from "./layers/WMTSLayer.js";
 import WFSVectorLayer from "./layers/VectorLayer.js";
+import WFSLayer from "./layers/WFSLayer";
 import { bindMapClickEvent } from "./Click.js";
 import { defaults as defaultInteractions } from "ol/interaction";
 import { Map, View } from "ol";
@@ -270,6 +271,15 @@ class AppModel {
       case "vector":
         layerConfig = configMapper.mapVectorConfig(layer);
         layerItem = new WFSVectorLayer(
+          layerConfig.options,
+          this.config.appConfig.proxy,
+          map
+        );
+        map.addLayer(layerItem.layer);
+        break;
+      case "wfs":
+        layerConfig = configMapper.mapWFSConfig(layer);
+        layerItem = new WFSLayer(
           layerConfig.options,
           this.config.appConfig.proxy,
           map
@@ -540,6 +550,7 @@ class AppModel {
 
       layers.wmslayers.forEach(l => (l.type = "wms"));
       layers.wmtslayers.forEach(l => (l.type = "wmts"));
+      layers.wfslayers.forEach(l => (l.type = "wfs"));
       layers.wfstlayers.forEach(l => (l.type = "edit"));
       layers.vectorlayers.forEach(l => (l.type = "vector"));
       layers.arcgislayers.forEach(l => (l.type = "arcgis"));
@@ -548,6 +559,7 @@ class AppModel {
         ...layers.wmslayers,
         ...layers.wmtslayers,
         ...layers.vectorlayers,
+        ...layers.wfslayers,
         ...layers.wfstlayers,
         ...layers.arcgislayers
       ];
