9ceaeb727a615ca1c3a58aa742c2497c84e682b5
Jacob Wodzyński <jacob.wod@gmail.com>
2020-09-01 14:35:14 +0200

    Minor changes to Infoclick: - Mostly new comments were added - I also simplified the code and made it look a bit fresher. This still needs a correct rewrite though, especially to handle changed layer visiblity after user click has occurred.

--- new-client/src/components/App.js
+++ new-client/src/components/App.js
@@ -266,10 +266,10 @@ class App extends React.PureComponent {
   componentDidCatch(error) {}
 
   bindHandlers() {
-    this.globalObserver.subscribe("core.mapClick", (mapClickDataResult) => {
+    this.globalObserver.subscribe("core.mapClick", (results) => {
       this.appModel.highlight(false);
       this.setState({
-        mapClickDataResult: mapClickDataResult,
+        mapClickDataResult: results,
       });
     });
 
@@ -324,72 +324,83 @@ class App extends React.PureComponent {
       this.setState({ drawerButtons: newState });
     });
 
-    this.appModel
-      .getMap()
-      .getLayers()
-      .getArray()
-      .forEach((layer) => {
-        layer.on("change:visible", (evt) => {
-          let layer = evt.target;
-          if (
-            this.state.mapClickDataResult &&
-            Array.isArray(this.state.mapClickDataResult.features)
-          ) {
-            this.state.mapClickDataResult.features.forEach((feature) => {
-              if (feature.layer === layer) {
-                let o = { ...this.state.mapClickDataResult };
-                o.features = o.features.filter((f) => f !== feature);
-                this.setState({
-                  mapClickDataResult: o,
-                });
-              }
-            });
-          }
-        });
-      });
+    /**
+     * TODO: Implement correctly a way to remove features from map click
+     * results when layer visibility is changed. The current implementation
+     * has problems with group layers: if we have a group layer and toggle
+     * its visibility, the features are not removed from infoclick window.
+     */
+    // this.appModel
+    //   .getMap()
+    //   .getLayers()
+    //   .getArray()
+    //   .forEach((layer) => {
+    //     layer.on("change:visible", (e) => {
+    //       const layer = e.target;
+    //       if (Array.isArray(this.state.mapClickDataResult.features)) {
+    //         this.state.mapClickDataResult.features.forEach((feature) => {
+    //           if (feature.layer === layer) {
+    //             const o = { ...this.state.mapClickDataResult };
+    //             o.features = o.features.filter((f) => f !== feature);
+    //             this.setState({
+    //               mapClickDataResult: o,
+    //             });
+    //           }
+    //         });
+    //       }
+    //     });
+    //   });
   }
 
   renderInfoclickWindow() {
-    const infoclickConfig = this.props.config.mapConfig.tools.find(
+    // Check if admin wants Infoclick to be active
+    const infoclickOptions = this.props.config.mapConfig.tools.find(
       (t) => t.type === "infoclick"
-    );
+    )?.options;
 
-    if (infoclickConfig === undefined) {
-      return null;
-    }
-
-    const open =
-      this.state.mapClickDataResult &&
-      this.state.mapClickDataResult.features &&
-      this.state.mapClickDataResult.features.length > 0
-        ? true
-        : false;
-    const features =
-      this.state.mapClickDataResult && this.state.mapClickDataResult.features;
-
-    const { title, position, width, height } = infoclickConfig.options;
+    // The 'open' prop, below, will control whether the Window is
+    // currently visible or not. The 'open' property itself
+    // depends on whether there are Features to display or not.
+    //
+    // That, in turn, depends on what's in the current state of 'mapClickDataResult'.
+    //
+    // It will be changed each time user clicks on map (as we have it registered
+    // like that in Click.js), so we can be confident that __after each user
+    // click we do have the most current results in our state__.
+    //
+    // Note however that which layers are included is controlled by
+    // __layer visibility at the time the click event happens!__
+    //
+    // As soon as user starts to show/hide layers __after__ the click, our
+    // 'mapClickDataResult' may contain results from hidden layers (or not
+    // contain results from layers activated after the click occurred).
+    //
+    // This may or may not be a bug (depending on how we see it), and may
+    // be fixed in the future.
 
     return (
-      <Window
-        globalObserver={this.globalObserver}
-        title={title || "Infoclick"}
-        open={open}
-        position={position || "right"}
-        mode="window"
-        width={width || 400}
-        height={height || 300}
-        features={features}
-        map={this.appModel.getMap()}
-        onDisplay={(feature) => {
-          this.appModel.highlight(feature);
-        }}
-        onClose={() => {
-          this.appModel.highlight(false);
-          this.setState({
-            mapClickDataResult: undefined,
-          });
-        }}
-      />
+      infoclickOptions !== undefined && (
+        <Window
+          open={this.state.mapClickDataResult?.features?.length > 0} // Will show window only if there are any features to show
+          globalObserver={this.globalObserver}
+          title={infoclickOptions.title || "Infoclick"}
+          position={infoclickOptions.position || "right"}
+          mode="window"
+          width={infoclickOptions.width || 400}
+          height={infoclickOptions.height || 300}
+          features={this.state.mapClickDataResult?.features}
+          map={this.appModel.getMap()}
+          onDisplay={(feature) => {
+            this.appModel.highlight(feature);
+          }}
+          onClose={() => {
+            this.appModel.highlight(false);
+            this.setState({
+              mapClickDataResult: undefined,
+            });
+          }}
+        />
+      )
     );
   }
 
