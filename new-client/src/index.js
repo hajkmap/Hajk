0de3f393e1241e483a86149d57448d48a39f6d61
Johansson <t.johansson@sweco.se>
2020-09-21 15:34:34 +0200

    Add polyfill for allSettled - regular polyfill now working
    Add polyfill for allSettled - regular polyfill now working

--- new-client/src/index.js
+++ new-client/src/index.js
@@ -4,6 +4,7 @@
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import "abortcontroller-polyfill/dist/polyfill-patch-fetch";
+import "allsettled-polyfill";
 // IE 11 ends here.
 
 import "ol/ol.css";
