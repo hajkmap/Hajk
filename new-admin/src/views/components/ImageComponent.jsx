ecfde3d24062b27498c38087555b9d1ad1b4ed6a
linusfj <linus.frykgard@gmail.com>
2020-10-15 17:16:49 +0200

    Style Edit Image button
    Add styling for the Edit Image button and place it over the image preview.

--- new-admin/src/views/components/ImageComponent.jsx
+++ new-admin/src/views/components/ImageComponent.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
 
 const ImageComponent = (props) => {
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [width, setWidth] = useState("");
 
   const handleOpen = (e) => {
     e.preventDefault();
@@ -15,14 +18,31 @@ const ImageComponent = (props) => {
   };
 
   const body = (
-    <div className={styles.paper}>
+    <div className={classes.paper}>
       <h2 id="image-modal-title">Redigerar bild</h2>
       <p id="image-modal-description">Inställningar för: {props.src}</p>
-      <input type="text" value={props.src} />
-      <input type="number" value={props.width} />
-      <input type="number" value={props.height} />
-      <input type="text" value={props["data-caption"]} />
-      <input type="text" value={props["data-source"]} />
+      <input type="text" value={props.src} placeholder="URL" />
+      <input
+        type="text"
+        value={props["data-image-width"]}
+        placeholder="data-image-width"
+        onChange={(e) => setWidth(e.target.value)}
+      />
+      <input
+        type="text"
+        value={props["data-image-height"]}
+        placeholder="data-image-height"
+      />
+      <input
+        type="text"
+        value={props["data-caption"]}
+        placeholder="data-caption"
+      />
+      <input
+        type="text"
+        value={props["data-source"]}
+        placeholder="data-source"
+      />
       <input type="checkbox" id="data-popup" value={props["data-popup"]} />
       <label for="data-popup">Popup</label>
     </div>
@@ -43,17 +63,25 @@ const ImageComponent = (props) => {
 
   if (props["data-popup"]) {
     return (
-      <div>
+      <div className={classes.imgContainer}>
         <img
           src={props.src}
-          width={props.width}
-          height={props.height}
+          width={props["data-image-width"]}
+          height={props["data-image-height"]}
           alt={props["data-caption"]}
+          data-image-width={props["data-image-width"]}
+          data-image-height={props["data-image-height"]}
           data-caption={props["data-caption"]}
           data-source={props["data-source"]}
           data-popup
         />
-        <button type="button" value="true" onClick={handleOpen}>
+        <button
+          type="button"
+          variant="contained"
+          className="btn btn-primary"
+          value="true"
+          onClick={handleOpen}
+        >
           Redigera bild
         </button>
         {modal}
@@ -61,16 +89,24 @@ const ImageComponent = (props) => {
     );
   } else {
     return (
-      <div>
+      <div className={classes.imgContainer}>
         <img
           src={props.src}
-          width={props.width}
-          height={props.height}
+          width={props["data-image-width"]}
+          height={props["data-image-height"]}
           alt={props["data-caption"]}
+          data-image-width={props["data-image-width"]}
+          data-image-height={props["data-image-height"]}
           data-caption={props["data-caption"]}
           data-source={props["data-source"]}
         />
-        <button type="button" value="true" onClick={handleOpen}>
+        <button
+          type="button"
+          variant="contained"
+          className="btn btn-primary"
+          value="true"
+          onClick={handleOpen}
+        >
           Redigera bild
         </button>
         {modal}
@@ -80,7 +116,7 @@ const ImageComponent = (props) => {
 };
 
 /* CSS styling */
-const styles = {
+const useStyles = makeStyles((theme) => ({
   root: {
     fontFamily: "'Georgia', serif",
     padding: 20,
@@ -113,11 +149,35 @@ const styles = {
   paper: {
     position: "absolute",
     width: 400,
-    //backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
-    //boxShadow: theme.shadows[5],
-    //padding: theme.spacing(2, 4, 3),
   },
-};
+  imgContainer: {
+    position: "relative",
+    width: "100%",
+    maxWidth: 400,
+    "& > img": {
+      width: "100%",
+      height: "auto",
+    },
+    "& > .btn": {
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      "-ms-transform": "translate(-50%, -50%)",
+      backgroundColor: "#555",
+      color: "white",
+      fontSize: "16px",
+      padding: "12px 24px",
+      border: "none",
+      cursor: "pointer",
+      borderRadius: "5px",
+      textAlign: "center",
+    },
+    "& > .btn:hover": {
+      backgroundColor: "black",
+    },
+  },
+}));
 
 export default ImageComponent;
