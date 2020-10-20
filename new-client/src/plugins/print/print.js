212160b24745aa1174eba23201ab5303b78390c6
Henrik Hallberg <43059093+sweco-sesqqq@users.noreply.github.com>
2020-10-20 15:46:04 +0200

    Advanced print options
    * Moved functionality from view to model
    
    * Added advanced printoptions - wip
    
    * Added posibility to include scalebar
    
    * Added north arrow
    
    * Updated admin for new print-functionality
    
    * pdf-print complete-ish png todo - wip
    
    * Added some comments -wip
    
    * Cleanup and added north_arrow image

--- new-client/src/plugins/print/print.js
+++ new-client/src/plugins/print/print.js
@@ -1,30 +1,37 @@
-// Generic imports – all plugins need these
 import React from "react";
 import PropTypes from "prop-types";
 import BaseWindowPlugin from "../BaseWindowPlugin";
-
-// Plugin-specific imports. Most plugins will need a Model, View and Observer
-// but make sure to only create and import whatever you need.
 import PrintModel from "./PrintModel";
 import PrintView from "./PrintView";
 import Observer from "react-event-observer";
-
-// All plugins will need to display an icon. Make sure to pick a relevant one from Material UI Icons.
 import PrintIcon from "@material-ui/icons/Print";
 
-/**
- * @summary Main class for the Dummy plugin.
- * @description The purpose of having a Dummy plugin is to exemplify
- * and document how plugins should be constructed in Hajk.
- * The plugins can also serve as a scaffold for other plugins: simply
- * copy the directory, rename it and all files within, and change logic
- * to create the plugin you want to.
- *
- * @class Dummy
- * @extends {React.PureComponent}
- */
 class Print extends React.PureComponent {
-  state = {};
+  // Paper dimensions: Array[width, height]
+  dims = {
+    a0: [1189, 841],
+    a1: [841, 594],
+    a2: [594, 420],
+    a3: [420, 297],
+    a4: [297, 210],
+    a5: [210, 148],
+  };
+
+  // Default scales, used if none supplied in options
+  scales = [
+    100,
+    250,
+    500,
+    1000,
+    2500,
+    5000,
+    10000,
+    25000,
+    50000,
+    100000,
+    200000,
+    500000,
+  ];
 
   static propTypes = {
     app: PropTypes.object.isRequired,
@@ -32,12 +39,23 @@ class Print extends React.PureComponent {
     options: PropTypes.object.isRequired,
   };
 
-  static defaultProps = {
-    options: {},
-  };
-
   constructor(props) {
     super(props);
+
+    // Prepare scales from admin options, fallback to default if needed
+    if (typeof props?.options?.scales === "string") {
+      props.options.scales = props.options.scales.replace(/\s/g, "").split(",");
+    }
+
+    // If no valid max logo width is supplied, use a hard-coded default
+    props.options.logoMaxWidth =
+      typeof props.options?.logoMaxWidth === "number"
+        ? props.options.logoMaxWidth
+        : 40;
+
+    // Ensure we have a value for the crossOrigin parameter
+    props.options.crossOrigin =
+      props.app.config.mapConfig.map?.crossOrigin || "anonymous";
 
     this.localObserver = Observer();
 
@@ -45,6 +63,11 @@ class Print extends React.PureComponent {
       localObserver: this.localObserver,
       app: props.app,
       map: props.map,
+      scales: props.options.scales,
+      dims: this.dims,
+      logoUrl: props.options.logo,
+      northArrowUrl: props.options.northArrow,
+      logoMaxWidth: props.options.logoMaxWidth,
     });
   }
 
@@ -66,7 +89,7 @@ class Print extends React.PureComponent {
           title: "Skriv ut",
           description: "Skapa en PDF av kartan",
           height: 500,
-          width: 250,
+          width: 350,
           onWindowShow: this.onWindowShow,
           onWindowHide: this.onWindowHide,
         }}
@@ -74,9 +97,12 @@ class Print extends React.PureComponent {
         <PrintView
           model={this.printModel}
           app={this.props.app}
-          map={this.props.map}
           options={this.props.options}
+          map={this.props.map}
           localObserver={this.localObserver}
+          scales={this.props.options.scales}
+          visibleAtStart={this.props.options.visibleAtStart}
+          dims={this.dims}
         />
       </BaseWindowPlugin>
     );
