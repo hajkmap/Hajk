hajkmap/Hajk

commit 825087bd2d1fba4a5ef50c50449bc6db3bc9658c

Added the option to Collector that allows user to restart the guide upon finishing.
* added "stäng" and "Tyck till igen" buttons. "stäng" close the tool-window itself and you will return to the start page of the questionnaire by clicking "Tyck till igen". it's configured in admingränsnitt.

* Changed variable names from tycktilligen to collectAgain

* Update new-client/src/plugins/collector/collector.js

Co-authored-by: Jacob Wodzyński <jacob.wod@gmail.com>

* Formatting update and changed to having only one button but with different texts on it for close and collect again

* Moved from if else to inline conditional

Co-authored-by: Jacob Wodzyński <jacob.wod@gmail.com>

new-client/src/plugins/collector/components/Page.js
@@ -569,12 +569,38 @@ class Page extends Component {
           this.props.model.observer.publish("abort");
         }}
       >
+        {this.props.options.collectAgain ? "Tyck till igen" : "Stäng"}
+      </Button>
+    );
+
+    const closeButton = (
+      <Button
+        variant="outlined"
+        color="primary"
+        className={classes.buttonRight}
+        onClick={() => {
+          this.props.model.app.windows.forEach(window => {
+            if (window.type === "collector") {
+              window.closeWindow();
+            }
+          });
+        }}
+      >
         Stäng
       </Button>
     );
 
     if (this.state.displayThankYou) {
-      return <div>{okButton}</div>;
+      if (this.props.options.collectAgain) {
+        return (
+          <div>
+            {okButton}
+            {closeButton}
+          </div>
+        );
+      } else {
+        return <div>{okButton}</div>;
+      }
     }
 
     if (numPages === 1) {

new-admin/src/views/tools/collector.jsx
@@ -65,17 +65,17 @@ var defaultState = {
   form: [],
   visibleForGroups: [],
   editServices: [],
-  layerDescription: undefined
+  layerDescription: undefined,
 };
 
-const ColorButtonBlue = withStyles(theme => ({
+const ColorButtonBlue = withStyles((theme) => ({
   root: {
     color: theme.palette.getContrastText(blue[500]),
     backgroundColor: blue[500],
     "&:hover": {
-      backgroundColor: blue[700]
-    }
-  }
+      backgroundColor: blue[700],
+    },
+  },
 }))(Button);
 
 class ToolOptions extends Component {
@@ -104,30 +104,31 @@ class ToolOptions extends Component {
         featureNS: tool.options.featureNS,
         showThankYou: tool.options.showThankYou,
         thankYou: tool.options.thankYou,
+        collectAgain: tool.options.collectAgain,
         form: tool.options.form || [],
         visibleAtStart: tool.options.visibleAtStart || false,
         visibleForGroups: tool.options.visibleForGroups || [],
         serviceId: tool.options.serviceId,
-        editServices: []
+        editServices: [],
       };
     } else {
       this.state = {
         ...defaultState,
-        active: false
+        active: false,
       };
     }
   }
 
   componentDidMount() {
     const { model } = this.props;
-    model.getEditServices(services => {
+    model.getEditServices((services) => {
       this.setState(
         {
-          editServices: services
+          editServices: services,
         },
         () => {
           const selectedService = services.find(
-            s => s.id === this.state.serviceId
+            (s) => s.id === this.state.serviceId
           );
           if (selectedService) {
             this.describeLayer(selectedService);
@@ -151,14 +152,14 @@ class ToolOptions extends Component {
       value = !isNaN(Number(value)) ? Number(value) : value;
     }
     this.setState({
-      [name]: value
+      [name]: value,
     });
   }
 
   getTool() {
     return this.props.model
       .get("toolConfig")
-      .find(tool => tool.type === this.type);
+      .find((tool) => tool.type === this.type);
   }
 
   add(tool) {
@@ -169,12 +170,12 @@ class ToolOptions extends Component {
     this.props.model.set({
       toolConfig: this.props.model
         .get("toolConfig")
-        .filter(tool => tool.type !== this.type)
+        .filter((tool) => tool.type !== this.type),
     });
   }
 
   replace(tool) {
-    this.props.model.get("toolConfig").forEach(t => {
+    this.props.model.get("toolConfig").forEach((t) => {
       if (t.type === this.type) {
         t.options = tool.options;
         t.index = tool.index;
@@ -197,6 +198,7 @@ class ToolOptions extends Component {
         abstract: this.state.abstract,
         featureNS: this.state.featureNS,
         showThankYou: this.state.showThankYou,
+        collectAgain: this.state.collectAgain,
         visibleAtStart: this.state.visibleAtStart,
         thankYou: this.state.thankYou,
         visibleForGroups: this.state.visibleForGroups.map(
@@ -204,8 +206,8 @@ class ToolOptions extends Component {
           String.prototype.trim
         ),
         form: this.state.form,
-        serviceId: this.state.serviceId
-      }
+        serviceId: this.state.serviceId,
+      },
     };
 
     var existing = this.getTool();
@@ -216,7 +218,7 @@ class ToolOptions extends Component {
         () => {
           this.props.parent.props.parent.setState({
             alert: true,
-            alertMessage: "Uppdateringen lyckades"
+            alertMessage: "Uppdateringen lyckades",
           });
         }
       );
@@ -233,7 +235,7 @@ class ToolOptions extends Component {
             this.remove();
             update.call(this);
             this.setState(defaultState);
-          }
+          },
         });
       } else {
         this.remove();
@@ -261,13 +263,13 @@ class ToolOptions extends Component {
     }
 
     this.setState({
-      visibleForGroups: value !== "" ? groups : []
+      visibleForGroups: value !== "" ? groups : [],
     });
   }
 
   describeLayer(layer) {
     this.setState({
-      layerDescription: layer.editableFields
+      layerDescription: layer.editableFields,
     });
   }
 
@@ -281,7 +283,7 @@ class ToolOptions extends Component {
             value={this.state.visibleForGroups}
             type="text"
             name="visibleForGroups"
-            onChange={e => {
+            onChange={(e) => {
               this.handleAuthGrpsChange(e);
             }}
           />
@@ -303,7 +305,7 @@ class ToolOptions extends Component {
             <ColorButtonBlue
               variant="contained"
               className="btn"
-              onClick={e => {
+              onClick={(e) => {
                 e.preventDefault();
                 this.save();
               }}
@@ -317,7 +319,7 @@ class ToolOptions extends Component {
               id="active"
               name="active"
               type="checkbox"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               checked={this.state.active}
@@ -334,7 +336,7 @@ class ToolOptions extends Component {
               type="number"
               min="0"
               className="control-fixed-width"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               value={this.state.index}
@@ -346,7 +348,7 @@ class ToolOptions extends Component {
               id="target"
               name="target"
               className="control-fixed-width"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               value={this.state.target}
@@ -370,7 +372,7 @@ class ToolOptions extends Component {
               id="position"
               name="position"
               className="control-fixed-width"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               value={this.state.position}
@@ -394,7 +396,7 @@ class ToolOptions extends Component {
               type="number"
               min="0"
               className="control-fixed-width"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               value={this.state.width}
@@ -415,7 +417,7 @@ class ToolOptions extends Component {
               type="number"
               min="0"
               className="control-fixed-width"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               value={this.state.height}
@@ -429,7 +431,7 @@ class ToolOptions extends Component {
               id="url"
               name="url"
               type="text"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               value={this.state.url}
@@ -448,7 +450,7 @@ class ToolOptions extends Component {
               value={this.state.abstract}
               type="text"
               name="abstract"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
             />
@@ -459,7 +461,7 @@ class ToolOptions extends Component {
               value={this.state.headerText}
               type="text"
               name="headerText"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
             />
@@ -469,7 +471,7 @@ class ToolOptions extends Component {
               id="visibleAtStart"
               name="visibleAtStart"
               type="checkbox"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               checked={this.state.visibleAtStart}
@@ -482,7 +484,7 @@ class ToolOptions extends Component {
               id="showThankYou"
               name="showThankYou"
               type="checkbox"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
               checked={this.state.showThankYou}
@@ -495,10 +497,30 @@ class ToolOptions extends Component {
             <textarea
               value={this.state.thankYou}
               name="thankYou"
-              onChange={e => {
+              onChange={(e) => {
                 this.handleInputChange(e);
               }}
             />
+          </div>
+          <div>
+            <input
+              id="collectAgain"
+              name="collectAgain"
+              type="checkbox"
+              onChange={(e) => {
+                this.handleInputChange(e);
+              }}
+              checked={this.state.collectAgain}
+            />
+            &nbsp;
+            <label htmlFor="collectAgain">
+              Visa "Tyck Till Igen"{" "}
+              <i
+                className="fa fa-question-circle"
+                data-toggle="tooltip"
+                title="'Stäng' och 'Tyck till igen' knappar visas på thank you sidan. 'Stäng' knapp stänger tyck till fönstret och 'tyck till igen' knapp börjar en ny tyck till."
+              />
+            </label>
           </div>
           <div>
             <label htmlFor="featureNS">Redigeringstjänst</label>
@@ -520,7 +542,7 @@ class ToolOptions extends Component {
                           {
                             featureType: serviceFeatureType,
                             featureNs: serviceFeatureNs,
-                            serviceId: service.id
+                            serviceId: service.id,
                           },
                           () => {
                             this.describeLayer(service);
@@ -540,10 +562,10 @@ class ToolOptions extends Component {
           <FieldEditor
             form={this.state.form}
             parent={this}
-            onUpdate={form => {
+            onUpdate={(form) => {
               console.log("Update");
               this.setState({
-                form: form
+                form: form,
               });
             }}
           />
@@ -551,7 +573,7 @@ class ToolOptions extends Component {
             <ColorButtonBlue
               variant="contained"
               className="btn"
-              onClick={e => {
+              onClick={(e) => {
                 e.preventDefault();
                 this.save();
               }}

commit 4d1953373a06fad70b01818dff7fa50c52bb4557

Hotfix to lint error introduced in 825087bd2d1fba4a5ef50c50449bc6db3bc9658c

new-client/src/plugins/collector/components/Page.js
@@ -579,7 +579,7 @@ class Page extends Component {
         color="primary"
         className={classes.buttonRight}
         onClick={() => {
-          this.props.model.app.windows.forEach(window => {
+          this.props.model.app.windows.forEach((window) => {
             if (window.type === "collector") {
               window.closeWindow();
             }