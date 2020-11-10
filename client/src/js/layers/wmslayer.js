6690ccdfaed06d72fbf5c405793e83eff0a330ee
Mattias Andrén <mattias.andren@zinet.se>
2020-11-10 14:36:10 +0100

    Added 'withCredentials: true' to WMS GetFeatureInfo calls for WmsLayer

--- client/src/js/layers/wmslayer.js
+++ client/src/js/layers/wmslayer.js
@@ -156,6 +156,7 @@ var WmsLayer = {
 
         var request = $.ajax({
           url: HAJK2.searchProxy + url,
+          xhrFields: { withCredentials: true },
           success: (data) => {
             var features = new ol.format.GeoJSON().readFeatures(data);
             this.featureInformationCallback(features, this.getLayer());
