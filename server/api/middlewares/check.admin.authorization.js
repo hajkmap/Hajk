333fa939d77d4f78aec1cd9b9ec60b4a77828312
Jacob Wodzyński <jacob.wod@gmail.com>
2020-12-07 13:09:23 +0100

    Save some CPU cycles by saving user header value

--- server/api/middlewares/check.admin.authorization.js
+++ server/api/middlewares/check.admin.authorization.js
@@ -1,8 +1,8 @@
 import log4js from "log4js";
-
 import ad from "../services/activedirectory.service";
 
 const logger = log4js.getLogger("router");
+
 export default async function (req, res, next) {
   logger.trace("Attempt to access admin methods");
 
@@ -16,25 +16,30 @@ export default async function (req, res, next) {
     return next();
   }
 
+  // Save user name to eliminate multiple calls to the same method
+  const user = ad.getUserFromRequestHeader(req);
+
   // Check if current user is member of the admins AD group
   const allowed = await ad.isUserMemberOf(
-    ad.getUserFromRequestHeader(req),
+    user,
     process.env.RESTRICT_ADMIN_ACCESS_TO_AD_GROUP
   );
 
   if (allowed) {
     logger.trace(
       "Access to admin granted. %o is member of %o.",
-      ad.getUserFromRequestHeader(req),
+      user,
       process.env.RESTRICT_ADMIN_ACCESS_TO_AD_GROUP
     );
+    // Access granted! Go on with the next middleware.
     return next();
   } else {
     logger.warn(
       "Access to admin forbidden. %o is not member of %o.",
-      ad.getUserFromRequestHeader(req),
+      user,
       process.env.RESTRICT_ADMIN_ACCESS_TO_AD_GROUP
     );
     res.status(403).send("Forbidden");
+    // Here we aren't calling next() so no following middlewares will run
   }
 }
