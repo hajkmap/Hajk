1ab2dc80169bc1ee31a75c2d2f98da6b8ce3c7df
Hitomi Westrin <hitomi.westrin@gmail.com>
2021-01-07 14:49:24 +0100

    Added error checking and ensure that any transformation error will just leave all coordinates as they were before

--- new-client/src/plugins/coordinates/CoordinatesModel.js
+++ new-client/src/plugins/coordinates/CoordinatesModel.js
@@ -20,7 +20,7 @@ class CoordinatesModel {
     this.source = new VectorSource();
     this.vector = new Vector({
       source: this.source,
-      name: "coordinateLayer",
+      name: "coordinateLayer"
     });
     this.map.addLayer(this.vector);
   }
@@ -42,10 +42,10 @@ class CoordinatesModel {
    * @summary Removes any previous markers and adds a new one to the given coordinates.
    * @memberof CoordinatesModel
    */
-  addMarker = (coordinates) => {
+  addMarker = coordinates => {
     // Prepare the feature
     const feature = new Feature({
-      geometry: new Point(coordinates),
+      geometry: new Point(coordinates)
     });
 
     // Style it with a nice icon
@@ -53,8 +53,8 @@ class CoordinatesModel {
       image: new Icon({
         anchor: [0.5, 1],
         scale: 0.15,
-        src: "marker.png",
-      }),
+        src: "marker.png"
+      })
     });
     feature.setStyle(styleMarker);
 
@@ -67,23 +67,24 @@ class CoordinatesModel {
 
   centerOnMarker = () => {
     if (this.vector.getSource().getFeatures().length > 0) {
-      this.map
-        .getView()
-        .setCenter(
-          this.vector
-            .getSource()
-            .getFeatures()[0]
-            .getGeometry()
-            .getCoordinates()
-        );
+      this.map.getView().setCenter(
+        this.vector
+          .getSource()
+          .getFeatures()[0]
+          .getGeometry()
+          .getCoordinates()
+      );
     }
   };
 
   zoomOnMarker = () => {
     if (this.vector.getSource().getFeatures().length > 0) {
-      this.map
-        .getView()
-        .fit(this.vector.getSource().getFeatures()[0].getGeometry());
+      this.map.getView().fit(
+        this.vector
+          .getSource()
+          .getFeatures()[0]
+          .getGeometry()
+      );
     }
   };
   transform(coordinates, to) {
@@ -97,7 +98,7 @@ class CoordinatesModel {
    *
    * @memberof CoordinatesModel
    */
-  handleDrawEnd = (e) => {
+  handleDrawEnd = e => {
     // Grab coordinates from the Point that has been drawn
     this.coordinates = e.feature.getGeometry().getCoordinates();
 
@@ -112,13 +113,13 @@ class CoordinatesModel {
         image: new CircleStyle({
           radius: 5,
           stroke: new Stroke({
-            color: "rgba(0, 0, 0, 0.7)",
+            color: "rgba(0, 0, 0, 0.7)"
           }),
           fill: new Fill({
-            color: "rgba(255, 255, 255, 0.2)",
-          }),
-        }),
-      }),
+            color: "rgba(255, 255, 255, 0.2)"
+          })
+        })
+      })
     });
     this.draw.on("drawend", this.handleDrawEnd);
     this.map.addInteraction(this.draw);
@@ -135,7 +136,7 @@ class CoordinatesModel {
     this.map.clickLock.delete("coordinates");
   }
 
-  handleInput = (event) => {
+  handleInput = event => {
     // Gets the transform that was specified on the TextField
     // it is used to transform the coordinates to the map projection which is the one updateTransforms expects
     var transformCode =
@@ -149,15 +150,15 @@ class CoordinatesModel {
     // We need to look in grand grand parent's children to find the other input field and pick the one
     // which does not have the same transform. From there, we can get the other input field
     event.target.parentElement.parentElement.parentElement.children.forEach(
-      (item) => {
+      item => {
         if (axis !== item.attributes["axis"].value) {
           // Index to update
           var idx = item.attributes["axis"].value === "X" ? 0 : 1;
 
           // Need to collect the value which is inside an input grandchild
-          item.children.forEach((child) => {
+          item.children.forEach(child => {
             if (child.localName === "div") {
-              child.children.forEach((grandchild) => {
+              child.children.forEach(grandchild => {
                 if (grandchild.localName === "input") {
                   updatedCoordinates[idx] = grandchild.value;
                 }
@@ -170,8 +171,16 @@ class CoordinatesModel {
     // transform the coordinations if needed
     updatedCoordinates = [
       parseFloat(updatedCoordinates[0]),
-      parseFloat(updatedCoordinates[1]),
+      parseFloat(updatedCoordinates[1])
     ];
+    if (
+      isNaN(updatedCoordinates[0]) ||
+      isNaN(updatedCoordinates[1]) ||
+      !isFinite(updatedCoordinates[0]) ||
+      !isFinite(updatedCoordinates[1])
+    ) {
+      return;
+    }
     if (transformCode !== this.map.getView().getProjection()._code) {
       updatedCoordinates = transform(
         updatedCoordinates,
@@ -194,23 +203,27 @@ class CoordinatesModel {
     let transformedCoordinates;
 
     if (this.transformations.length > 0) {
-      // If there are defined transformations, loop through them
-      transformedCoordinates = this.transformations.map((transformation) => {
-        const container = {};
+      try {
+        // If there are defined transformations, loop through them
+        transformedCoordinates = this.transformations.map(transformation => {
+          const container = {};
 
-        container.code = transformation.code ?? "";
-        container.precision = transformation.precision ?? 3;
-        container.default = transformation.default ?? false;
-        container.hint = transformation.hint ?? "";
-        container.title = transformation.title ?? "";
-        container.xtitle = transformation.xtitle ?? "";
-        container.ytitle = transformation.ytitle ?? "";
-        container.inverseAxis = transformation.inverseAxis ?? false;
-        container.coordinates =
-          this.transform(this.coordinates, transformation.code) ?? "";
+          container.code = transformation.code ?? "";
+          container.precision = transformation.precision ?? 3;
+          container.default = transformation.default ?? false;
+          container.hint = transformation.hint ?? "";
+          container.title = transformation.title ?? "";
+          container.xtitle = transformation.xtitle ?? "";
+          container.ytitle = transformation.ytitle ?? "";
+          container.inverseAxis = transformation.inverseAxis ?? false;
+          container.coordinates =
+            this.transform(this.coordinates, transformation.code) ?? "";
 
-        return container;
-      });
+          return container;
+        });
+      } catch (e) {
+        return;
+      }
     } else {
       // If no transformations are defined, fall back to default WGS84
       transformedCoordinates = [
@@ -223,8 +236,8 @@ class CoordinatesModel {
           xtitle: "Lng",
           ytitle: "Lat",
           inverseAxis: true,
-          coordinates: this.transform(this.coordinates, "EPSG:4326"),
-        },
+          coordinates: this.transform(this.coordinates, "EPSG:4326")
+        }
       ];
     }
 
