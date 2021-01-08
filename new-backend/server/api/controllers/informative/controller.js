306868f49be596c08283374916343c97cf2441d5
Jacob Wodzyński <jacob.wod@gmail.com>
2021-01-08 15:21:50 +0100

    Added logging of admin events: - This will hopefully be useful in larger organizations where multiple admin users work and modify the same Hajk setup. - All endpoints that create, update or delete map configs and layers are now logged to a separate file (logs/admin_events.log). The file is rotated daily. If Hajk is setup to run on multiple instances (clustered setup), a separate admin_events file will be created for each instance (to ensure easy debugging). - Since getting the user name is a kind of expensive operation, I found a way to save it between middlewares. So when the first middleware that does the admin check (check.admin.authorization.js) runs, it saves the value of user name to res.locals. A lot more can be optimized now, when I found a way to reuse stuff easily between middlewares, but that's a different story.

--- new-backend/server/api/controllers/informative/controller.js
+++ new-backend/server/api/controllers/informative/controller.js
@@ -1,4 +1,8 @@
 import InformativeService from "../../services/informative.service";
+import log4js from "log4js";
+
+// Create a logger for admin events, those will be saved in a separate log file.
+const ael = log4js.getLogger("adminEvent");
 
 export class Controller {
   create(req, res) {
@@ -6,8 +10,12 @@ export class Controller {
     InformativeService.create(documentName, mapName).then((r) => {
       // FIXME: The buggy admin expects 200 and this string on success,
       // but I think that we'd do better with a meaningful JSON response.
-      if (r && !r.error) res.status(200).send("Document created");
-      else res.status(500).send(r.error.message);
+      if (r && !r.error) {
+        res.status(200).send("Document created");
+        ael.info(
+          `${res.locals.authUser} created a new document, ${documentName}.json, and connected it to map ${mapName}.json`
+        );
+      } else res.status(500).send(r.error.message);
     });
   }
 
@@ -20,15 +28,23 @@ export class Controller {
 
   saveByName(req, res) {
     InformativeService.saveByName(req.params.name, req.body).then((r) => {
-      if (r && !r.error) res.status(200).send("File saved");
-      else res.status(500).send(r.error.message);
+      if (r && !r.error) {
+        res.status(200).send("File saved");
+        ael.info(
+          `${res.locals.authUser} saved document ${req.params.name}.json`
+        );
+      } else res.status(500).send(r.error.message);
     });
   }
 
   deleteByName(req, res) {
     InformativeService.deleteByName(req.params.name).then((r) => {
-      if (r && !r.error) res.status(200).send("File saved");
-      else res.status(500).send(r.error.message);
+      if (r && !r.error) {
+        res.status(200).send("File saved");
+        ael.info(
+          `${res.locals.authUser} deleted document ${req.params.name}.json`
+        );
+      } else res.status(500).send(r.error.message);
     });
   }
 
--- new-backend/server/api/controllers/settings/controller.js
+++ new-backend/server/api/controllers/settings/controller.js
@@ -1,11 +1,20 @@
 import SettingsService from "../../services/settings.service";
+import log4js from "log4js";
+
+// Create a logger for admin events, those will be saved in a separate log file.
+const ael = log4js.getLogger("adminEvent");
 
 export class Controller {
   putSettingsToMapFile(req, res) {
     SettingsService.updateMapFile(req.query.mapFile, req.body, req.url).then(
       (r) => {
         // If r contains mapConfig, update has succeeded
-        if (r.mapConfig) res.status(200).json(r.mapConfig);
+        if (r.mapConfig) {
+          // Send response
+          res.status(200).json(r.mapConfig);
+          // Log admin action
+          ael.info(`${res.locals.authUser} saved map ${req.query.mapFile}`);
+        }
         // If r contains message, it looks like we got back an error object
         // FIXME: Just check if 'r instanceof Error' and if so, send(r.toString()) so we also get Error name, not only message
         else if (r.message) res.status(500).send(r.message);
@@ -18,7 +27,17 @@ export class Controller {
   putLayerOfType(req, res) {
     SettingsService.createOrUpdateLayer(req.params.type, req.body).then((r) => {
       // r.status will be either 200 (layer updated) or 201 (layer created)
-      if (r.newLayer) res.status(r.status).json(r.newLayer);
+      if (r.newLayer) {
+        res.status(r.status).json(r.newLayer);
+
+        // Print a nice admin log message. Will result in something like:
+        // [2021-01-01 11:11] INFO: foobar123 added wmslayer with id 42 ("Some layer")
+        ael.info(
+          `${res.locals.authUser} ${r.status === 200 ? "added" : "updated"} ${
+            req.params.type
+          } with id ${r.newLayer.id} ("${r.newLayer.caption}")`
+        );
+      }
       // If r contains message, it looks like we got back an error object
       else if (r.message) res.status(500).send(r.message);
       else res.status(500).send(r);
@@ -28,8 +47,13 @@ export class Controller {
   deleteLayerFromStore(req, res) {
     SettingsService.deleteLayer(req.params.type, req.params.layerId).then(
       (r) => {
-        if (r && !r.error) res.json(r);
-        else res.status(500).send(r.error.message);
+        if (r && !r.error) {
+          res.json(r);
+          // Print an admin log message
+          ael.info(
+            `${res.locals.authUser} deleted ${req.params.type} with id ${req.params.layerId}`
+          );
+        } else res.status(500).send(r.error.message);
       }
     );
   }
