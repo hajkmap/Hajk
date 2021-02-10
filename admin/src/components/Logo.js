f9057f619a6492435797259e9cca0675cbf87b72
Jacob Wodzyński <jacob.wod@gmail.com>
2021-02-10 13:35:15 +0100

    Moved the upcoming Admin to a feature branch.

--- admin/src/components/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Logo = (props) => {
-  return <img alt="Logo" src="/static/logo.svg" {...props} />;
-};
-
-export default Logo;
--- admin/src/utils/ConfigContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ConfigContext = React.createContext(null);
-
-export default ConfigContext;
