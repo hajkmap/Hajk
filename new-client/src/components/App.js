73cfd9e2f6bc89a529245a729a2302e0058019f4
Tobias Johansson <t.johansson@sweco.se>
2021-02-10 08:04:02 +0100

    Remove unecessary aria-label due to title already present
    Remove unecessary aria-label due to title already present

--- new-client/src/components/App.js
+++ new-client/src/components/App.js
@@ -546,7 +546,6 @@ class App extends React.PureComponent {
                 }
               >
                 <IconButton
-                  aria-label="pin"
                   onClick={this.togglePermanent}
                   onMouseEnter={this.handleMouseEnter}
                   onMouseLeave={this.handleMouseLeave}
