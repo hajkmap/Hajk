369c220f1695985047f269800bfda9f62a67c53c
Jacob Wodzyński <jacob.wod@gmail.com>
2021-03-04 10:48:33 +0100

    Bugfix: color of selected feature in Search. - If admin set an rgba-color with transparent alpha channel, the search marker would become invisible, because that color is used as icon's color. - The fix is to ensure that icons special treatment: we ensure that alpha value is 0.8, but keep the fill color setting otherwise.

--- new-client/src/components/Search/utils/FeatureStyle.js
+++ new-client/src/components/Search/utils/FeatureStyle.js
@@ -84,8 +84,12 @@ export default class FeatureStyle {
         ? this.#defaultSelectionStyleSettings
         : this.#defaultHighlightStyleSettings;
 
+    // Create new color to be used for icons: start with fill color but make sure that it's
+    // _not_ transparent by setting the alpha channel value to 0.8.
+    const iconColor = settings.fillColor.replace(/(?<=,)[\d.]*(?=\))/, "0.8");
+
     // Default SVG icon to be used as marker. Placed here so we can grab the current style's fill color.
-    const svgString = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32pt" height="32pt" fill="${settings.fillColor}"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>`;
+    const svgString = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32pt" height="32pt" fill="${iconColor}"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>`;
     const defaultMarker = `data:image/svg+xml;base64,${window.btoa(svgString)}`; // We need base64 for kml-exports to work.
     // For the 'highlight' style, we want the marker icon to be 30% larger than other styles
     const multiplier = type === "highlight" ? 1.3 : 1;
