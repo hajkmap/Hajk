071a6edc093294c81e9e14e1922bea67a4adc7db
Henrik Hallberg <43059093+sweco-sesqqq@users.noreply.github.com>
2021-03-31 13:33:32 +0200

    Revert "Vt/core changes 4 (#716)" (#729)
    This reverts commit 0eb2ccaf04d7eddc20316b7333322ab0c38f339e.

--- new-client/src/components/App.js
+++ new-client/src/components/App.js
@@ -561,9 +561,7 @@ class App extends React.PureComponent {
 
   renderSearchComponent() {
     // FIXME: We should get config from somewhere else now when Search is part of Core
-    const renderSearchComponent = !this.appModel.plugins.search?.options
-      ?.renderElsewhere;
-    if (renderSearchComponent && this.appModel.plugins.search) {
+    if (this.appModel.plugins.search) {
       return (
         <Search
           map={this.appModel.getMap()}
