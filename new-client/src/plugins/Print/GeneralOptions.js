10edf726942599d2d3b2c603c07786f1c462eec3
Henrik Hallberg <henrik.hallberg@sweco.se>
2021-04-05 15:27:56 +0200

    Work towards the requirements in #724 - admin not updated yet!
    1. Added user choice for margin around the map-image
    2. Added disclaimer and copyright
    3. Default textColor now from settings
    3. Textcolor now shown to user with badge instead of backgroundcolor on the iconButton
    
    (cherry picked from commit ab3c75fcb9f42d85061ddd86812678c672bbe80f)

--- new-client/src/plugins/Print/GeneralOptions.js
+++ new-client/src/plugins/Print/GeneralOptions.js
@@ -53,6 +53,7 @@ class GeneralOptions extends React.PureComponent {
   render() {
     const {
       classes,
+      useMargin,
       orientation,
       format,
       scale,
@@ -79,6 +80,20 @@ class GeneralOptions extends React.PureComponent {
               <MenuItem value={"a3"}>A3</MenuItem>
               <MenuItem value={"a4"}>A4</MenuItem>
               <MenuItem value={"a5"}>A5</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="useMargin">Ram runt kartbilden</InputLabel>
+            <Select
+              value={useMargin}
+              onChange={handleChange}
+              inputProps={{
+                name: "useMargin",
+                id: "useMargin",
+              }}
+            >
+              <MenuItem value={true}>Ja</MenuItem>
+              <MenuItem value={false}>Nej</MenuItem>
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
