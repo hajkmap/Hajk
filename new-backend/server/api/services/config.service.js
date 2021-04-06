5f37c708760b80da9db30a7dcc4f7446eaea5d2f
Per Söderström <per.soderstrom@kungsbacka.se>
2021-04-06 19:07:36 +0200

    Add locking WFST edit layers with AD

--- new-backend/server/api/services/config.service.js
+++ new-backend/server/api/services/config.service.js
@@ -172,12 +172,13 @@ class ConfigService {
 
   /**
    * @summary Take a map config as JSON object and return
-   * only those parts of itthat the current user has access to.
+   * only those parts of it that the current user has access to.
    *
    * @description The following content will be washed:
    *  - Part 1: tools (access to each of them can be restricted)
    *  - Part 2: groups and layers (in LayerSwitcher's options)
    *  - Part 3: WFS search services (in Search's options)
+   *  - Part 4: WFST edit services (in Edit's options)
    *
    * @param {*} mapConfig
    * @param {*} user
@@ -271,6 +272,27 @@ class ConfigService {
           )
       );
       mapConfig.tools[searchIndexInTools].options.layers = layers;
+    }
+
+    // Part 4: Wash WFST edit services
+    const editIndexInTools = mapConfig.tools.findIndex(
+      (t) => t.type === "edit"
+    );
+
+    if (editIndexInTools !== -1) {
+      let { activeServices } = mapConfig.tools[editIndexInTools].options;
+      // Wash WFST edit layers
+      activeServices = await asyncFilter(
+        activeServices, // layers in edit tool are named activeServices
+        async (layer) =>
+          await this.filterByGroupVisibility(
+            layer.visibleForGroups,
+            user,
+            `WFST edit layer "${layer.id}"`
+          )
+      );
+
+      mapConfig.tools[editIndexInTools].options.activeServices = activeServices;
     }
 
     return mapConfig;
