a97383b7bf52a716794c929b128c7643cde0c275
jesade-vbg <jesade.vbg@gmail.com>
2021-06-04 08:28:51 +0200

    FeatureProps Output filters for infoclick. Initial commit.

--- new-client/src/components/FeatureInfo/FeaturePropsParsing.js
+++ new-client/src/components/FeatureInfo/FeaturePropsParsing.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import { withStyles } from "@material-ui/core";
 import gfm from "remark-gfm";
+import FeaturePropFilters from "./FeaturePropsFilters";
 import {
   Divider,
   Link,
@@ -174,6 +175,8 @@ export default class FeaturePropsParsing {
           ) - 1
         }}`;
       }
+    } else if (placeholder.indexOf("|") > 0) {
+      return FeaturePropFilters.applyFilters(this.properties, placeholder);
     }
     // Just a "normal" placeholder, e.g. {foobar}
     else {
@@ -350,8 +353,10 @@ export default class FeaturePropsParsing {
       // The loop below extracts all placeholders and replaces them with actual values
       // current feature's property collection.
       // Match any word character, range of unicode characters (åäö etc), @ sign, dash or dot
-      (this.markdown.match(/{[\w\u00C0-\u00ff@\-.]+}/g) || []).forEach(
+      // eslint-disable-next-line no-useless-escape
+      (this.markdown.match(/{[\s'\w\u00C0-\u00ff@\-|,'.():]+}/g) || []).forEach(
         (placeholder) => {
+          console.log("!!!!", placeholder);
           // placeholder is a string, e.g. "{intern_url_1@@documenthandler}" or "{foobar}"
           // Let's replace all occurrences of the placeholder like this:
           // {foobar} -> Some nice FoobarValue
