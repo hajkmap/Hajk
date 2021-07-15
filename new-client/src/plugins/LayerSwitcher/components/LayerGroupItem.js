2539e843b5a9e2012e9c9975d6aa4be523483858
Hitomi Westrin <hitomi.westrin@gmail.com>
2021-07-15 19:19:27 +0200

    Added check so multiple empty styles are not added to the style strings as this causes incorrect responses from Geoserver

--- new-client/src/plugins/LayerSwitcher/components/LayerGroupItem.js
+++ new-client/src/plugins/LayerSwitcher/components/LayerGroupItem.js
@@ -377,7 +377,9 @@ class LayerGroupItem extends Component {
         // Join them into a string that will be used to
         // reset STYLES param for the GET request.
         STYLES: Object.entries(this.props.layer.layersInfo)
-          .filter((k) => subLayersToShow.indexOf(k[0]) !== -1)
+          .filter(
+            (k) => subLayersToShow.indexOf(k[0]) !== -1 && k[1].style !== ""
+          )
           .map((l) => l[1].style)
           .join(","),
       });
