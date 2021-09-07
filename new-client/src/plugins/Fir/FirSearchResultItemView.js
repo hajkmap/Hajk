164b7566e5fcf0570363c0b1d47bedd1284c150b
jesade-vbg <jesade.vbg@gmail.com>
2021-09-07 11:45:52 +0200

    FIR - added search result item view

--- new-client/src/plugins/Fir/FirSearchResultItemView.js
+++ new-client/src/plugins/Fir/FirSearchResultItemView.js
@@ -24,9 +24,82 @@ class FirSearchResultItemView extends React.PureComponent {
 
   render() {
     const { classes } = this.props;
+
+    let data = {
+      list: [
+        { key: "realestate", title: "Fastighet", value: "TESTÄPPLET 2" },
+        { key: "area", title: "Område", value: "1" },
+        { key: "owner1", title: "Ägare", value: "Andersson, Greta Gunhild" },
+        { key: "owner2", title: "Ägare", value: "Andersson, Nisse Greger" },
+        { key: "owner3", title: "Ägare", value: "" },
+        { key: "note_owner", title: "Notering ägare", value: "" },
+        {
+          key: "address",
+          title: "Fastighetsadress",
+          value: "Nissevägen 33, 432 XX Varberg",
+        },
+        {
+          key: "total_area",
+          title: "Registrerad totalarea (m2)",
+          value: "800",
+        },
+      ],
+      urls: [
+        {
+          key: "report_simple",
+          title: "Fastighetsrapport (förenklad)",
+          url:
+            "https://fastighetsrapport-utv.varberg.se/Report/Fastighetenkel/pdf/xxxxxxxxxx",
+          target: "_blank",
+        },
+        {
+          key: "report_full",
+          title: "Fastighetsrapport (komplett)",
+          url:
+            "https://fastighetsrapport-utv.varberg.se/Report/Fastighet/pdf/xxxxxxxxxxx",
+          target: "_blank",
+        },
+        {
+          key: "building_permit",
+          title: "Bygglovsanteckning",
+          url:
+            "https://bygglovsanteckning-utv.varberg.se/?fnr=xxxxxxxxxxxx&fastighet=xxxxxxxxxxxx",
+          target: "_blank",
+        },
+      ],
+    };
+
     return (
       <>
-        <div className={classes.root}>...{this.model.name}...</div>
+        <div className={classes.root}>
+          <table className={classes.resultItemTable}>
+            <tbody>
+              {data.list.map((item) => {
+                return (
+                  <tr>
+                    <td>{item.title}</td>
+                    <td>{item.value || "\u00A0"}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className={classes.urlList}>
+            {data.urls.map((link) => {
+              return (
+                <div>
+                  <a
+                    href={link.url}
+                    target={link.target || "_ blank"}
+                    title={link.title}
+                  >
+                    {link.title}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </>
     );
   }
@@ -34,6 +107,27 @@ class FirSearchResultItemView extends React.PureComponent {
 
 const styles = (theme) => ({
   root: {},
+  resultItemTable: {
+    borderSpacing: 0,
+    width: "100%",
+    marginBottom: theme.spacing(2),
+    "& tr:nth-child(even) td": {
+      backgroundColor: theme.palette.type === "dark" ? "#565656" : "#e8e8e8",
+    },
+    "& tr td:first-child": {
+      fontWeight: 500,
+    },
+    "& td": {
+      verticalAlign: "top",
+      padding: "2px 6px",
+    },
+  },
+  urlList: {
+    "& a": {
+      display: "block",
+      padding: "2px 0",
+    },
+  },
 });
 
 export default withStyles(styles)(withSnackbar(FirSearchResultItemView));
