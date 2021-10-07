5a237a2226728335952d4dec6d9c6115ccd4a524
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-10-07 10:55:05 +0200

    DownloadLink and LayerGroup now mui5 compatible

--- new-client/src/plugins/LayerSwitcher/components/DownloadLink.js
+++ new-client/src/plugins/LayerSwitcher/components/DownloadLink.js
@@ -1,9 +1,7 @@
 import React from "react";
-import makeStyles from "@mui/styles/makeStyles";
 import IconButton from "@mui/material/IconButton";
 import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 
-const useStyles = makeStyles((theme) => ({}));
 /**
  * @summary Renders a Download button which on click downloads the current layer as KML
  *
@@ -16,7 +14,6 @@ function DownloadLink({ index = 0, layer, enableDownloadLink = false }) {
     : null;
   const wmsUrl = layer.get("url");
   const downloadUrl = `${wmsUrl}/kml?layers=${layerName}&mode=download`;
-  const classes = useStyles();
 
   const handleDownloadClick = () => {
     document.location = downloadUrl;
@@ -26,7 +23,6 @@ function DownloadLink({ index = 0, layer, enableDownloadLink = false }) {
     ? layerName !== null && (
         <IconButton
           aria-label="download"
-          className={classes.button}
           onClick={handleDownloadClick}
           size="small"
         >
