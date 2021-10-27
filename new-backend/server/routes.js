212d4d9d79daa91168bfd7443f4cbe8fba1596cd
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-10-27 07:36:02 +0200

    Removed the old fme-server routes (the proxy in the last commit is a better way forward)

--- new-backend/server/routes.js
+++ new-backend/server/routes.js
@@ -1,6 +1,5 @@
 import configRouter from "./api/controllers/config/router.js";
 import configRouterV2 from "./api/controllers/configV2/router.js";
-import fmeServerRouter from "./api/controllers/fmeServer/router.js";
 import mapconfigRouter from "./api/controllers/mapconfig/router.js";
 import settingsRouter from "./api/controllers/settings/router.js";
 import informativeRouter from "./api/controllers/informative/router.js";
@@ -9,7 +8,6 @@ import adRouter from "./api/controllers/ad/router.js";
 export default function routes(app) {
   app.use("/api/v1/config", configRouter);
   app.use("/api/v2/config", configRouterV2);
-  app.use("/api/v1/fme", fmeServerRouter);
   app.use("/api/v1/informative", informativeRouter);
   app.use("/api/v1/mapconfig", mapconfigRouter);
   app.use("/api/v1/settings", settingsRouter);
