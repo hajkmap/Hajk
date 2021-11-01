7628f0a2cea21597572eea38cf9e11a5ce343a8b
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-11-01 19:48:12 +0100

    Small fix regarding error handling in useProductParameters hook

--- new-client/src/plugins/FmeServer/hooks/useProductParameters.js
+++ new-client/src/plugins/FmeServer/hooks/useProductParameters.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 
 export default function useProductParameters(groupName, workspaceName, model) {
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [parameters, setParameters] = useState([]);
 
   useEffect(() => {
     const fetchParameters = async () => {
       setLoading(true);
+      setError(false);
       const { error, parameters } = await model.getProductParameters(
         groupName,
         workspaceName
