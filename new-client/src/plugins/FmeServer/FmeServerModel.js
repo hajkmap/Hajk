c728076e9e197d9a110ab52634b99c089bbdc45b
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-11-01 16:15:08 +0100

    Added a method to perform a request to run a workspace on FME-server

--- new-client/src/plugins/FmeServer/FmeServerModel.js
+++ new-client/src/plugins/FmeServer/FmeServerModel.js
@@ -32,10 +32,38 @@ class FmeServerModel {
     try {
       const response = await hfetch(url);
       const data = await response.json();
-      console.log("data: ", data);
       return { error: false, parameters: data };
     } catch (error) {
       return { error: true, parameters: [] };
+    }
+  };
+
+  // Posts a request to run a workspace on FME-server.
+  // Returns an object containing eventual error and eventual
+  // jobId which can be used to check the status of the job.
+  submitProductRequest = async (groupName, productName, parameters) => {
+    // If the product is missing for some reason, we return an error
+    // and null for the job id.
+    const product = this.getProduct(groupName, productName);
+    if (!product) {
+      return { error: true, jobId: null };
+    }
+    // If not, let's create the url used to submit the workspace.
+    const url = this.#createSubmitProductRequestUrl(product);
+    // And then try to submit the job using the url...
+    try {
+      const response = await hfetch(url, {
+        method: "POST",
+        body: parameters,
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      return { error: false, jobId: data };
+    } catch (error) {
+      return { error: true, jobId: null };
     }
   };
 
@@ -44,6 +72,15 @@ class FmeServerModel {
     return `${this.#mapServiceBase}/fmeproxy/fmerest/v3/repositories/${
       product.repository
     }/items/${product.workspace}/parameters`;
+  };
+
+  // Returns the url needed to post a request to start a workspace.
+  #createSubmitProductRequestUrl = (product) => {
+    return `${
+      this.#mapServiceBase
+    }/fmeproxy/fmerest/v3/transformations/submit/${product.repository}/${
+      product.workspace
+    }`;
   };
 }
 export default FmeServerModel;
