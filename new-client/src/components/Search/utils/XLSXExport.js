b3ebeca42464f4d81ecb16b41857979f88638897
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-11-17 10:16:57 +0100

    Further improvements in the xlsx-export (in search). See desc.
    Now exports are aligned even if keys are missing on some features. The geometry-field is also removed from the export.

--- new-client/src/components/Search/utils/XLSXExport.js
+++ new-client/src/components/Search/utils/XLSXExport.js
@@ -86,14 +86,49 @@ export default class XLSXExport {
   #createXLSXSheet = (featureCollection) => {
     if (featureCollection?.value?.features?.length > 0) {
       const exportArray = [];
-      // Keys from first feature. We assume that all features in the collections has the same keys.
-      const keys = Object.keys(
-        featureCollection.value.features[0].getProperties()
-      );
+      // We have to make sure that we get the keys from the feature with most keys.
+      // Otherwise we might miss some feature properties, since some features might
+      // not contain all the keys.
+      const keys = featureCollection.value.features.reduce((keys, feature) => {
+        // Let's get the keys for the current feature
+        const allKeys = feature.getKeys();
+        // Then we'll get the geometry name (since we want to remove the geometry
+        // property key). (We don't want the geometry in the export).
+        const geometryName = feature.getGeometryName();
+        // Let's filter the array of keys and remove the geometry key.
+        const valueKeys = allKeys.filter((key) => key !== geometryName);
+        // If we've managed to get more keys than previously, we store
+        // those keys.
+        if (valueKeys.length > keys.length) {
+          return valueKeys;
+        }
+        // Otherwise, we keep the previous keys.
+        return keys;
+      }, []);
+
+      // We want the keys at the beginning (top) of the export, so lets
+      // push them to the exportArray first.
       exportArray.push(keys);
 
+      // When we got the keys, we can extract the corresponding values
+      // from each feature.
       featureCollection.value.features.forEach((feature) => {
-        exportArray.push(Object.values(feature.getProperties()));
+        // First we have to get all the properties
+        const featureProperties = feature.getProperties();
+        // Then we initiate a values array
+        const values = [];
+        // Then we push the value for each key to the value
+        // array. Remember that some values might be nullish,
+        // but they are still important, otherwise the fields in the
+        // xlsx export might get a messed up order.
+        keys.forEach((key) => {
+          // If the value is missing, we replace it with an empty string.
+          const value = featureProperties[key] ?? "";
+          // Then push we push it to the array.
+          values.push(value);
+        });
+        // Then we push the array of values to the export array!
+        exportArray.push(values);
       });
 
       return XLSX.utils.aoa_to_sheet(exportArray);
