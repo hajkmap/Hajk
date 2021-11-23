58c052d71a403f5cb0b5a8d643c58a44a480fe9e
Jesper Adeborn <73874338+jesade-vbg@users.noreply.github.com>
2021-11-23 09:28:53 +0100

    Feature/855 fir plugin (#911)
    * FIR-Tool Initial commit. GUI only, no logic or connections.
    
    * FIR UI - tbc....
    
    * FIR - added search result item view
    
    * FIR - Added pagination etc to search results.
    
    * FIR - Export UI additions etc.
    
    * FIR - wip.......
    
    * FIR - basic text search is working for all 3 search types. etc.
    
    * FIR - added draw and search geometry etc etc.
    
    * FIR - Added import
    
    * FIR - Added buffer, labels etc.
    
    * FIR - Fixed result sorting
    
    * FIR - Reverse pagination, result scroll addition, added marker for selected feature, etc
    
    * FIR - add/remove by mapclick. added some styling  etc
    
    * FIR - friday commit.
    
    * FIR - Split of the different exports. etc
    
    * FIR - Neighbor search, first working version. Allow updates from other sources in search results. etc etc etc.
    
    * FIR - Added Property export, Resident Export. Added spinners to buttons. Bugfixes etc etc. All functionality seems to be working so now it's time to make this possible to configure.......... tbc
    
    * FIR - Some cleanup and error handling.
    
    * FIR - Eliminate hardcoded options etc. WIP.
    
    * FIR - Added configuration to wfs & wms etc
    
    * FIR - Configuration work continues..
    
    * FIR - config continues....
    
    * FIR - Removed svg files and replaced with base64 icons.
    
    * FIR - Configuration continues.....
    
    * FIR - Config changes, selection behaviour changes, some more error handling and config missing warnings.
    
    * FIR - Tbc
    
    * FIR - Added example config.
    
    Co-authored-by: Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>

---
+++ new-client/src/plugins/Fir/FirExportView.js
@@ -0,0 +1,121 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { withSnackbar } from "notistack";
+import FirExportEdpView from "./FirExportEdpView";
+import FirExportPropertyListView from "./FirExportPropertyListView";
+import FirExportResidentListView from "./FirExportResidentListView";
+
+class FirExportView extends React.PureComponent {
+  state = {
+    results: [],
+  };
+
+  static propTypes = {
+    model: PropTypes.object.isRequired,
+    app: PropTypes.object.isRequired,
+    localObserver: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
+  };
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.model = this.props.model;
+    this.localObserver = this.props.localObserver;
+    this.globalObserver = this.props.app.globalObserver;
+
+    this.localObserver.subscribe("fir.results.filtered", (list) => {
+      this.setState({ results: [...list] });
+      this.forceUpdate();
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <>
+        <div className={classes.root}>
+          <div className={classes.info}>
+            <span className={classes.num}>{this.state.results.length}</span>{" "}
+            objekt finns tillgängliga för export.
+          </div>
+          {this.model.config.propertyList ? (
+            <FirExportPropertyListView
+              results={this.state.results}
+              model={this.model}
+              app={this.app}
+              localObserver={this.localObserver}
+            />
+          ) : (
+            ""
+          )}
+
+          {this.model.config.residentList ? (
+            <FirExportResidentListView
+              results={this.state.results}
+              model={this.model}
+              app={this.app}
+              localObserver={this.localObserver}
+            />
+          ) : (
+            ""
+          )}
+
+          {this.model.config.edp ? (
+            <FirExportEdpView
+              results={this.state.results}
+              model={this.model}
+              app={this.app}
+              localObserver={this.localObserver}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+      </>
+    );
+  }
+}
+
+const styles = (theme) => ({
+  info: {
+    padding: theme.spacing(2),
+  },
+  num: {
+    fontWeight: 500,
+    fontSize: "1rem",
+  },
+  heading: {
+    fontWeight: 500,
+  },
+  formControl: {
+    marginBottom: theme.spacing(3),
+  },
+  formControlOneMargin: {
+    marginBottom: theme.spacing(1),
+  },
+  checkboxLabel: {
+    fontSize: "0.875rem",
+    fontWeight: "400",
+  },
+  checkbox: {
+    paddingTop: "0.25rem",
+    paddingBottom: "0.25rem",
+  },
+  checkboxGroupContainer: {
+    paddingBottom: theme.spacing(2),
+  },
+  containerTopPadded: {
+    paddingTop: theme.spacing(2),
+  },
+  containerTopDoublePadded: {
+    paddingTop: theme.spacing(4),
+  },
+  textField: {
+    width: "50%",
+  },
+});
+
+export default withStyles(styles)(withSnackbar(FirExportView));
