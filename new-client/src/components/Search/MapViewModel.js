4c14f2bb688b40778cb6373b15f06e5930bc489d
Jacob Wodzyński <jacob.wod@gmail.com>
2021-11-26 08:24:01 +0100

    Minor fix and some proposals: - The fix is to make sure that the we have an initial value on the searchInVisibleLayers key - The proposals are marked as TODOs in comments, please see in code

--- new-client/src/components/Search/MapViewModel.js
+++ new-client/src/components/Search/MapViewModel.js
@@ -72,9 +72,16 @@ class MapViewModel {
     const visibleLayers = this.getVisibleLayers();
 
     const visibleSearchLayers = [];
+    // TODO: visibleLayers.filter() first to find out which
+    // layers do exist in searchSources. In that way, we could
+    // omit the loop-within-a-loop below.
     visibleLayers.forEach((vl) => {
       if (vl.layersInfo !== undefined) {
         const subLayers = Object.values(vl.layersInfo);
+        // TODO: Could there be a chance where we have more than
+        // one element in the subLayers array? I'm thinking of
+        // WMS group layers that are exposed as searchable -
+        // those can consist of multiple sub layers.
         const sl = subLayers[0];
         searchSources.forEach((wms) => {
           const vsl = [];
