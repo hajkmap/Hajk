hajkmap/Hajk

commit 65ca528d054cbb0cbc857c21ecc0bece71471e03

Feature/847 support for common datatypes (#884)

new-client/src/plugins/LayerSwitcher/components/LayerGroupItem.js
@@ -377,7 +377,9 @@ class LayerGroupItem extends Component {
         // Join them into a string that will be used to
         // reset STYLES param for the GET request.
         STYLES: Object.entries(this.props.layer.layersInfo)
-          .filter((k) => subLayersToShow.indexOf(k[0]) !== -1)
+          .filter(
+            (k) => subLayersToShow.indexOf(k[0]) !== -1 && k[1].style !== ""
+          )
           .map((l) => l[1].style)
           .join(","),
       });

backend/mapservice/Models/Config/WFSTConfig.cs
@@ -29,12 +29,20 @@ namespace MapService.Models.Config
 
         public List<EditableField> editableFields { get; set; }
 
+        public List<EditableField> nonEditableFields { get; set; }
+
         public bool editPoint{ get; set; }
+
+        public bool editMultiPoint { get; set; }
 
         public bool editPolygon { get; set; }
 
-        public bool editLine { get; set; }        
-        
+        public bool editMultiPolygon { get; set; }
+
+        public bool editLine { get; set; }
+
+        public bool editMultiLine { get; set; }
+
         public bool infoVisible { get; set; }
 
         public string infoTitle { get; set; }

backend/mapservice/Models/EditableField.cs
@@ -12,6 +12,10 @@ namespace MapService.Models
 
         public string name { get; set; }
 
+        public string alias { get; set; }
+
+        public string description { get; set; }
+
         public string dataType { get; set; }
 
         public string textType { get; set; }

new-admin/src/index.css
@@ -362,6 +362,10 @@ body.dragging * {
   margin: 0 !important;
 }
 
+.edit-fields-table-default {
+  width: 184px !important;
+}
+
 .list-value:first-child {
   margin-left: 0 !important;
 }

new-admin/src/views/tools/edit.jsx
@@ -95,6 +95,10 @@ class ToolOptions extends Component {
     if (typeof value === "string" && value.trim() !== "") {
       value = !isNaN(Number(value)) ? Number(value) : value;
     }
+
+    if (name === "instruction") {
+      value = btoa(value);
+    }
     this.setState({
       [name]: value,
     });

new-client/src/plugins/Collector/components/Toolbar.js
@@ -113,7 +113,7 @@ class Toolbar extends Component {
     if (!data) {
       return (
         <Typography>
-          Uppdatateringen lyckades men det upptäcktes inte några ändringar.
+          Uppdateringen lyckades men det upptäcktes inte några ändringar.
         </Typography>
       );
     }

new-client/src/plugins/Edit/EditModel.js
@@ -25,6 +25,7 @@ class EditModel {
     this.modify = undefined;
     this.key = undefined;
     this.editFeature = undefined;
+    this.editFeatureBackup = undefined;
     this.editSource = undefined;
     this.removeFeature = undefined;
     this.shell = undefined;
@@ -132,12 +133,18 @@ class EditModel {
       })
         .then((response) => {
           response.text().then((wfsResponseText) => {
-            this.refreshLayer(src.layers[0]);
-            this.vectorSource
-              .getFeatures()
-              .filter((f) => f.modification !== undefined)
-              .forEach((f) => (f.modification = undefined));
-            done(this.parseWFSTresponse(wfsResponseText));
+            const resXml = this.parseWFSTresponse(wfsResponseText);
+            if (resXml.ExceptionReport || !resXml.TransactionResponse) {
+              // do not delete the data so the user can submit it again
+              done(resXml);
+            } else {
+              this.refreshLayer(src.layers[0]);
+              this.vectorSource
+                .getFeatures()
+                .filter((f) => f.modification !== undefined)
+                .forEach((f) => (f.modification = undefined));
+              done(resXml);
+            }
           });
         })
         .catch((response) => {
@@ -346,24 +353,28 @@ class EditModel {
     });
   };
 
-  urlFromObject(url, obj) {
-    return Object.keys(obj).reduce((str, key, i, a) => {
-      str = str + key + "=" + obj[key];
-      if (i < a.length - 1) {
-        str = str + "&";
-      }
-      return str;
-    }, (url += "?"));
-  }
-
   loadData(source, extent, done) {
-    const url = this.urlFromObject(source.url, {
+    // Prepare the URL for retrieving data in a proper manner:
+    const url = new URL(source.url);
+
+    // Read potential existing search params from provided WFS URL
+    const existingSearchParams = Object.fromEntries(url.searchParams.entries());
+
+    // Merge those with other params that we want to append to the search params string
+    const mergedSearchParams = {
+      ...existingSearchParams,
       service: "WFS",
       version: "1.1.0",
       request: "GetFeature",
       typename: source.layers[0],
       srsname: source.projection,
-    });
+    };
+
+    // Create a new URLSearchParams object from the merged object…
+    const searchParams = new URLSearchParams(mergedSearchParams);
+    // …and update our URL's search string with the new value
+    url.search = searchParams.toString();
+
     hfetch(url)
       .then((response) => {
         if (response.status !== 200) {
@@ -482,6 +493,7 @@ class EditModel {
     this.draw.on("drawend", (event) => {
       event.feature.modification = "added";
       this.editAttributes(event.feature);
+      this.deactivateInteraction();
     });
     this.map.addInteraction(this.draw);
     this.map.clickLock.add("edit");
@@ -571,6 +583,7 @@ class EditModel {
   }
 
   resetEditFeature = () => {
+    this.editFeatureBackup = this.editFeature;
     this.editFeature = undefined;
     this.observer.publish("editFeature", this.editFeature);
   };

commit f899720b3a4cb354cea6bbdeefa8fbb5e44175ef

Fix bug introduced in #884, closes #940.

new-client/src/plugins/LayerSwitcher/components/LayerGroupItem.js
@@ -377,9 +377,7 @@ class LayerGroupItem extends Component {
         // Join them into a string that will be used to
         // reset STYLES param for the GET request.
         STYLES: Object.entries(this.props.layer.layersInfo)
-          .filter(
-            (k) => subLayersToShow.indexOf(k[0]) !== -1 && k[1].style !== ""
-          )
+          .filter((k) => subLayersToShow.indexOf(k[0]) !== -1)
           .map((l) => l[1].style)
           .join(","),
       });