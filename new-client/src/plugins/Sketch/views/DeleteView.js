78a1faefcdec95acc0bbfe3acbc5ff2446fd1ce3
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-01-05 08:15:29 +0100

    Added a button to remove all drawn features

--- new-client/src/plugins/Sketch/views/DeleteView.js
+++ new-client/src/plugins/Sketch/views/DeleteView.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import Information from "../components/Information";
 
-const DeleteView = (props) => {
+const DeleteView = ({ id, model, drawModel }) => {
   // We have to get some information about the current activity (view)
-  const activity = props.model.getActivityFromId(props.id);
+  const activity = model.getActivityFromId(id);
   return (
-    <Grid container>
-      <Grid item xs={12}>
+    <Grid
+      container
+      direction="column"
+      justify="space-between"
+      style={{ height: "100%" }}
+    >
+      <Grid item>
         <Information text={activity.information} />
+      </Grid>
+      <Grid item>
+        <Button
+          style={{ width: "100%" }}
+          variant="contained"
+          onClick={drawModel.removeDrawnFeatures}
+        >
+          Ta bort alla rit-objekt
+        </Button>
       </Grid>
     </Grid>
   );
