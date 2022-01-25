2bb3af936d766b06adacee3b08be7bc5eccf5a79
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-01-25 14:52:07 +0100

    Rewrite of CookieNotice.js

--- new-client/src/components/CookieNotice.js
+++ new-client/src/components/CookieNotice.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { object } from "prop-types";
+import React from "react";
 import {
   Button,
   Checkbox,
@@ -13,183 +12,179 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
+// Default settings for the cookie-notice text and url if none is supplied from the configuration.
+const DEFAULT_MESSAGE =
+  "Vi använder nödvändiga kakor (cookies) för att webbplatsen ska fungera på ett bra sätt för dig. Vi använder också funktionella kakor för att ge dig bästa möjliga funktion om du godkänner användningen av dessa.";
+const DEFAULT_URL =
+  "https://pts.se/sv/bransch/regler/lagar/lag-om-elektronisk-kommunikation/kakor-cookies/";
+
+// We're using several labeled checkboxes, let's create a component so that we keep DRY.
+const LabeledCheckbox = ({ checked, disabled, label, onChange }) => {
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          color="primary"
+          disabled={disabled ?? false}
+          checked={checked}
+          onChange={onChange}
+        />
+      }
+      label={label}
+    />
+  );
+};
+
 const useStyles = makeStyles((theme) => ({
   dialogContainer: {
     "& .MuiDialog-container": {
-      justifyContent: "flex-center",
       alignItems: "flex-end",
     },
   },
   dialogText: {
     color: theme.palette.text.primary,
-    paddingBottom: theme.spacing(2),
-  },
-  textButton: {
-    marginLeft: theme.spacing(1),
-  },
-  link: {
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    paddingLeft: theme.spacing(1),
   },
 }));
-
-CookieNotice.propTypes = {
-  globalObserver: object.isRequired,
-  appModel: object.isRequired,
-};
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-/**
- *  *
- * @export
- * @param {*} props
- * @returns React.Component
- */
 function CookieNotice({ globalObserver, appModel }) {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [checkBoxFunctional, setCheckBoxFunctional] = useState(false);
-  const [checkBox3rdPart, setCheckBox3rdPart] = useState(false);
+  const { cookieManager, config } = appModel;
+
+  // We should initialize the dialog:s open-state to whatever the manager states.
+  const [open, setOpen] = React.useState(cookieManager.showCookieNotice());
+  const [functionalChecked, setFunctionalChecked] = React.useState(false);
+  const [thirdPartChecked, setThirdPartChecked] = React.useState(false);
 
   const defaultCookieNoticeMessage =
-    appModel.config?.mapConfig?.map?.defaultCookieNoticeMessage ??
-    "Vi använder nödvändiga kakor (cookies) för att webbplatsen ska fungera på ett bra sätt för dig. Vi använder också funktionella kakor för att ge dig bästa möjliga funktion om du godkänner användningen av dessa.";
+    config?.mapConfig?.map?.defaultCookieNoticeMessage || DEFAULT_MESSAGE;
   const defaultCookieNoticeUrl =
-    appModel.config?.mapConfig?.map?.defaultCookieNoticeUrl ??
-    "https://pts.se/sv/bransch/regler/lagar/lag-om-elektronisk-kommunikation/kakor-cookies/";
+    config?.mapConfig?.map?.defaultCookieNoticeUrl || DEFAULT_URL;
+  const showThirdPartCheckbox =
+    config?.mapConfig?.map?.cookieUse3dPart ?? false;
 
-  const use3dPartCookies =
-    appModel.config?.mapConfig?.map?.cookieUse3dPart ?? false;
-  let jsxUse3dPartCheckbox = null;
-  if (use3dPartCookies) {
-    jsxUse3dPartCheckbox = (
-      <>
-        <FormControlLabel
-          control={
-            <Checkbox
-              color="primary"
-              checked={checkBox3rdPart}
-              onChange={(event) => {
-                setCheckBox3rdPart(event.target.checked);
-              }}
-            />
-          }
-          label={"3:e Part"}
-        />
-      </>
-    );
-  }
-
-  const handleClose = () => {
-    //setOpen(false); // Uncomment this to convert this dialog to a "modeless" dialog
-  };
-
-  if (appModel.cookieManager.showCookieNotice() === true) {
-    globalObserver.subscribe("core.appLoaded", () => {
+  // We're subscribing to the globalObserver-events in an useEffect so that we can
+  // make sure to clean up subscriptions on unMount. (The return-statement of useEffect).
+  React.useEffect(() => {
+    // An event that allows other components to show the cookie-notice so that
+    // the user can re-think their decision...
+    globalObserver.subscribe("core.showCookieBanner", () => {
       setOpen(true);
     });
-  }
+    return () => {
+      globalObserver.unsubscribe("core.showCookieBanner");
+    };
+  }, [globalObserver]);
 
-  globalObserver.subscribe("core.showCookieBanner", () => {
-    setOpen(true);
-  });
+  // Handler for when the user clicks "Allow selected", i.e. we should
+  // check which boxes are ticked, and set the cookie-level accordingly.
+  const handleAllowSelectedClick = React.useCallback(() => {
+    // Required cookies are selected "automatically", so if they clicked
+    // "Allow selected" they have at least accepted that (cookie-level 1).
+    let cookieLevel = 1;
+    // If the checkbox for functional-cookies is checked we bump the cookie-level.
+    if (functionalChecked) {
+      cookieLevel = cookieLevel | 2;
+    }
+    // If the checkbox for third-part-cookies is checked we bump the cookie-level.
+    if (thirdPartChecked) {
+      cookieLevel = cookieLevel | 4;
+    }
+    // Then we'll set the cookie-level in the manager.
+    cookieManager.setCookieLevels(cookieLevel);
+    // Make sure to close the dialog when the user has made the choice.
+    setOpen(false);
+  }, [functionalChecked, cookieManager, thirdPartChecked]);
+
+  // Handler for when the user clicks "Allow all", i.e. we should
+  // ignore which boxes are ticked, and set the cookie-level to allow all.
+  const handleAllowAllClick = React.useCallback(() => {
+    // IF the user accepts all they are at least accepting required- and
+    // functional-cookies. (Cookie-level 3).
+    let cookieLevel = 3;
+    // If we are showing the option for third-part-cookies, they are obviously
+    // accepting that as well.
+    if (showThirdPartCheckbox) {
+      cookieLevel = cookieLevel | 4;
+    }
+    // Then we'll set the cookie-level in the manager.
+    cookieManager.setCookieLevels(cookieLevel);
+    // Make sure to close the dialog when the user has made the choice.
+    setOpen(false);
+  }, [cookieManager, showThirdPartCheckbox]);
 
   return (
-    <div>
-      <Dialog
-        fullWidth={true}
-        maxWidth={"md"}
-        className={classes.dialogContainer}
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="cookie-dialog-content-text"
-      >
-        <DialogContent>
-          <DialogContentText
-            className={classes.dialogText}
-            id="cookie-dialog-content-text"
+    <Dialog
+      fullWidth={true}
+      maxWidth={"md"}
+      className={classes.dialogContainer}
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      aria-describedby="cookie-dialog-content-text"
+    >
+      <DialogContent>
+        <DialogContentText
+          className={classes.dialogText}
+          id="cookie-dialog-content-text"
+        >
+          {`${defaultCookieNoticeMessage} `}
+          <Link
+            href={defaultCookieNoticeUrl}
+            underline="always"
+            rel="noreferrer"
+            target="_blank"
           >
-            {defaultCookieNoticeMessage}
-
-            <Link
-              href={defaultCookieNoticeUrl}
-              className={classes.link}
-              underline="always"
-              rel="noreferrer"
-              target="_blank"
-            >
-              {"Mer information om kakor"}
-            </Link>
-          </DialogContentText>
-          <Grid></Grid>
-          <Grid container direction="row-reverse">
-            <Grid>
-              <Button
-                color="primary"
-                variant="contained"
-                className={classes.textButton}
-                onClick={() => {
-                  let cookieLevel = 1; // Required
-                  if (checkBoxFunctional) {
-                    cookieLevel = cookieLevel | 2; // Functional
-                  }
-                  if (checkBox3rdPart) {
-                    cookieLevel = cookieLevel | 4; // ThirdParty
-                  }
-                  appModel.cookieManager.setCookieLevels(cookieLevel);
-                  setOpen(false);
+            {"Mer information om kakor"}
+          </Link>
+        </DialogContentText>
+        <Grid container direction="row" alignItems="center" justify="flex-end">
+          <Grid item>
+            <LabeledCheckbox
+              disabled={true}
+              checked={true}
+              label={"Nödvändiga"}
+            />
+            <LabeledCheckbox
+              onChange={(event) => {
+                setFunctionalChecked(event.target.checked);
+              }}
+              checked={functionalChecked}
+              label={"Funktionella"}
+            />
+            {showThirdPartCheckbox && (
+              <LabeledCheckbox
+                onChange={(event) => {
+                  setThirdPartChecked(event.target.checked);
                 }}
-              >
-                {"Tillåt valda"}
-              </Button>
-              <Button
-                color="primary"
-                variant="contained"
-                className={classes.textButton}
-                onClick={() => {
-                  let cookieLevel = 3; // Required AND Functional
-                  if (use3dPartCookies) {
-                    cookieLevel = cookieLevel | 4;
-                  }
-                  appModel.cookieManager.setCookieLevels(cookieLevel);
-                  setOpen(false);
-                }}
-              >
-                {"Tillåt Alla"}
-              </Button>
-            </Grid>
-            <Grid>
-              <FormControlLabel
-                control={
-                  <Checkbox color="primary" checked={true} disabled={true} />
-                }
-                label={"Nödvändiga"}
+                checked={thirdPartChecked}
+                label={"3:e Part"}
               />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    color="primary"
-                    checked={checkBoxFunctional}
-                    onChange={(event) => {
-                      setCheckBoxFunctional(event.target.checked);
-                    }}
-                  />
-                }
-                label={"Funktionella"}
-              />
-              {jsxUse3dPartCheckbox}
-            </Grid>
+            )}
           </Grid>
-        </DialogContent>
-      </Dialog>
-    </div>
+          <Grid item>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleAllowSelectedClick}
+            >
+              {"Tillåt valda"}
+            </Button>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleAllowAllClick}
+              style={{ marginLeft: 8 }}
+            >
+              {"Tillåt Alla"}
+            </Button>
+          </Grid>
+        </Grid>
+      </DialogContent>
+    </Dialog>
   );
 }
 
