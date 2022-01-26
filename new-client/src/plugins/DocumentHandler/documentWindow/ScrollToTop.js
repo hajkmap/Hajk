b3b4f663ee2ae6faf3717f3b13e3737b8a6571a7
Jacob Wodzyński <jacob.wod@gmail.com>
2022-01-26 15:19:21 +0100

    Rewritten ScrollToTop

--- new-client/src/plugins/DocumentHandler/documentWindow/ScrollToTop.js
+++ new-client/src/plugins/DocumentHandler/documentWindow/ScrollToTop.js
@@ -1,43 +1,31 @@
 import React from "react";
-import Fab from "@mui/material/Fab";
+import { Box, Fab } from "@mui/material";
 import NavigationIcon from "@mui/icons-material/Navigation";
-import makeStyles from "@mui/styles/makeStyles";
-import clsx from "clsx";
 import { darken } from "@mui/material/styles";
 import { visuallyHidden } from "@mui/utils";
+import { styled } from "@mui/material/styles";
 
-const useStyles = ({ color }) =>
-  makeStyles((theme) => ({
-    scrollToTopButton: {
-      position: "fixed",
-      bottom: theme.spacing(2),
-      right: theme.spacing(3),
-    },
-    customColor: color && {
+const FabScrollToTopButton = styled(Fab)(({ color, theme }) => {
+  return {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(3),
+    ...(color && {
       backgroundColor: color,
-      color: theme.palette.getContrastText(color),
       "&:hover": {
         backgroundColor: darken(color, 0.3),
       },
-    },
-  }));
+    }),
+  };
+});
 
-const ScrollToTop = ({ onClick, color }) => {
-  const styleProps = { color };
-  const { scrollToTopButton, customColor } = useStyles(styleProps)();
-
+const ScrollToTop = (props) => {
   return (
-    <Fab
-      size="small"
-      className={clsx(scrollToTopButton, {
-        [customColor]: color,
-      })}
-      color={color ? "default" : "primary"}
-      onClick={onClick}
-    >
-      <span style={visuallyHidden}>Scrolla till toppen av dokumentet</span>
+    // Pass on all props to the styled component
+    <FabScrollToTopButton size="small" {...props}>
+      <Box sx={visuallyHidden}>Scrolla till toppen av dokumentet</Box>
       <NavigationIcon />
-    </Fab>
+    </FabScrollToTopButton>
   );
 };
 
