10bad494eb585c172beed60f85d99829cd57b91a
Jacob Wodzyński <jacob.wod@gmail.com>
2022-01-27 12:38:55 +0100

    Converted basic styling to sx attributes.

--- new-client/src/plugins/DocumentHandler/documentWindow/Progress.js
+++ new-client/src/plugins/DocumentHandler/documentWindow/Progress.js
@@ -1,31 +1,27 @@
 import React from "react";
-import withStyles from "@mui/styles/withStyles";
 import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
 
-const styles = (theme) => ({
-  grid: {
-    height: "100%",
-  },
-  progress: {
-    height: "100%",
-  },
-});
 class Progress extends React.PureComponent {
   render() {
-    const { classes } = this.props;
-
     return (
       <Grid
         alignItems="center"
         justifyContent="center"
         container
-        className={classes.grid}
+        sx={{
+          height: "100%",
+        }}
       >
-        <CircularProgress className={classes.progress} justify="center" />
+        <CircularProgress
+          justify="center"
+          sx={{
+            height: "100%",
+          }}
+        />
       </Grid>
     );
   }
 }
 
-export default withStyles(styles)(Progress);
+export default Progress;
