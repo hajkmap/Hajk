551e2eeb7b8005276a8254bb18541de47264f030
jesade-vbg <jesade.vbg@gmail.com>
2022-01-28 15:57:48 +0100

    Another typo....

--- new-client/src/utils/ConfigMapper.js
+++ new-client/src/utils/ConfigMapper.js
@@ -145,7 +145,7 @@ export default class ConfigMapper {
         extent: properties.mapConfig.map.extent,
         singleTile: args.singleTile || false,
         hidpi: args.hidpi,
-        useCustomHdpi: args.useCustomHdpi,
+        useCustomHiDpi: args.useCustomHiDpi,
         customRatio: args.customRatio,
         imageFormat: args.imageFormat || "image/png",
         serverType:
