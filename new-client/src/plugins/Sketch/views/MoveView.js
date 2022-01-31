7407c97d40487e0ee84df559f076a2c4a67d2b41
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-01-31 14:02:58 +0100

    Added ui for feature movement without user map interactions

--- new-client/src/plugins/Sketch/views/MoveView.js
+++ new-client/src/plugins/Sketch/views/MoveView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
+import { Button, Grid, Paper, TextField } from "@material-ui/core";
 import { Tooltip, Typography, Switch } from "@material-ui/core";
 import Information from "../components/Information";
 
@@ -27,6 +27,56 @@ const TranslateToggler = ({ translateEnabled, setTranslateEnabled }) => {
   );
 };
 
+const FeatureMoveSelector = () => {
+  return (
+    <Paper style={{ padding: 8, marginTop: 8 }}>
+      <Grid container item justify="center" alignItems="center">
+        <Grid item xs={12}>
+          <Typography variant="body2" align="center">
+            Fast förflyttning
+          </Typography>
+        </Grid>
+        <Grid item xs={12} style={{ marginTop: 16 }}>
+          <Tooltip title="Ange hur många meter du vill flytta objekten.">
+            <TextField
+              label="Förflyttningsavstånd (meter)"
+              variant="outlined"
+              fullWidth
+              type="number"
+              size="small"
+              value={1}
+            />
+          </Tooltip>
+        </Grid>
+        <Grid item xs={12} style={{ marginTop: 16 }}>
+          <Tooltip title="Ange i vilken riktning du vill flytta objekten. 0 grader är rakt norrut, 90 grader är rakt åt öster, osv.">
+            <TextField
+              label="Förflyttningsriktning (grader)"
+              variant="outlined"
+              fullWidth
+              type="number"
+              size="small"
+              value={1}
+            />
+          </Tooltip>
+        </Grid>
+        <Grid container spacing={1} style={{ marginTop: 8 }}>
+          <Grid item xs={6}>
+            <Button variant="contained" fullWidth size="small" disabled={true}>
+              Ångra
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="contained" fullWidth size="small">
+              Ok
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
 const MoveView = (props) => {
   // We have to get some information about the current activity (view)
   const activity = props.model.getActivityFromId(props.id);
@@ -40,13 +90,7 @@ const MoveView = (props) => {
           translateEnabled={props.translateEnabled}
           setTranslateEnabled={props.setTranslateEnabled}
         />
-        {props.moveFeatures.length > 0 && (
-          <Grid item xs={12}>
-            <Typography>
-              Här ska vi ha förflyttning med meter och grader
-            </Typography>
-          </Grid>
-        )}
+        {props.moveFeatures.length > 0 && <FeatureMoveSelector />}
       </Grid>
     </Grid>
   );
