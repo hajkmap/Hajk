5bee1bf07645e46b6ea1d0c0cec103f66cc35ed0
Mattias <mattias.andren@zinet.se>
2022-02-02 07:41:32 +0100

    Cookie implementation for Drawer, Information, Introduction and Search (#994)
    * Cookie implementation for Drawer, Information, Introduction and Search
    
    * Added some explanatory comments so that other developers can understand whats going on.
    
    Co-authored-by: Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>

--- new-client/src/components/Drawer/DrawerToggleButtons.js
+++ new-client/src/components/Drawer/DrawerToggleButtons.js
@@ -8,6 +8,8 @@ import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import CloseIcon from "@material-ui/icons/Close";
 import { Paper, Hidden } from "@material-ui/core";
 
+import { functionalOk as functionalCookieOk } from "models/Cookie";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     marginRight: theme.spacing(1),
@@ -67,7 +69,9 @@ function DrawerToggleButtons({
     if (v === null) {
       window.localStorage.removeItem("activeDrawerContent");
     } else {
-      window.localStorage.setItem("activeDrawerContent", v);
+      if (functionalCookieOk()) {
+        window.localStorage.setItem("activeDrawerContent", v);
+      }
     }
 
     // Let the outside world know that a button has been pressed.
--- new-client/src/components/Search/Search.js
+++ new-client/src/components/Search/Search.js
@@ -13,6 +13,7 @@ import KmlExport from "./utils/KmlExport";
 import XLSXExport from "./utils/XLSXExport";
 import { encodeCommas, decodeCommas } from "../../utils/StringCommaCoder";
 import LocalStorageHelper from "../../utils/LocalStorageHelper";
+import { functionalOk as functionalCookieOk } from "models/Cookie";
 
 const styles = () => ({
   inputRoot: {
@@ -438,9 +439,11 @@ class Search extends React.PureComponent {
         enableLabelOnHighlight: searchOptions.enableLabelOnHighlight,
       });
 
-    // Always save the current settings to local storage, so it can be
-    // retrieved on app reload.
-    LocalStorageHelper.set("searchOptions", searchOptions);
+    // If functional cookies are allowed, we'll save the current settings
+    // to local storage, so it can be retrieved on app reload.
+    if (functionalCookieOk()) {
+      LocalStorageHelper.set("searchOptions", searchOptions);
+    }
   };
 
   handleOnClickOrKeyboardSearch = () => {
