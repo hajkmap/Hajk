a1f4f243a7a99f97e296a9bffa8f1efb3e3cfb3f
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-02-07 10:31:09 +0100

    Added ui for selecting name on workspace-save

--- new-client/src/plugins/Sketch/views/SaveUploadView.js
+++ new-client/src/plugins/Sketch/views/SaveUploadView.js
@@ -1,8 +1,57 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Button, Grid, Paper, TextField, Tooltip } from "@material-ui/core";
 import Information from "../components/Information";
 
+const DrawSaver = (props) => {
+  const handleInputChange = (e) => {
+    props.setWorkspaceName(e.target.value);
+  };
+
+  // We only allow workspace-names that consist of three characters or more.
+  const workspaceNameHasEnoughChars = props.workspaceName.length > 2;
+
+  return (
+    <Paper style={{ padding: 8 }}>
+      <Grid container alignItems="center" justify="space-between">
+        <Grid item xs={8}>
+          <Tooltip title="Ange att namn så att arbetsytan kan identifieras senare.">
+            <TextField
+              size="small"
+              variant="outlined"
+              style={{ maxWidth: "100%" }}
+              onChange={handleInputChange}
+              value={props.workspaceName}
+            />
+          </Tooltip>
+        </Grid>
+        <Grid container item xs={3} justify="flex-end">
+          <Tooltip
+            title={
+              workspaceNameHasEnoughChars
+                ? "Klicka för att spara de ritobjekt som finns i kartan."
+                : "Minst tre tecken måste anges för att ritobjekten ska kunna sparas."
+            }
+          >
+            <span>
+              <Button
+                size="small"
+                variant="contained"
+                disabled={!workspaceNameHasEnoughChars}
+              >
+                Spara
+              </Button>
+            </span>
+          </Tooltip>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
 const SaveUploadView = (props) => {
+  // If the user wants to save their work, they'll have to choose a name
+  // so that the workspace can be identified in the list of saved workspaces later.
+  const [workspaceName, setWorkspaceName] = React.useState("");
   // We have to get some information about the current activity (view)
   const activity = props.model.getActivityFromId(props.id);
   return (
@@ -11,7 +60,10 @@ const SaveUploadView = (props) => {
         <Information text={activity.information} />
       </Grid>
       <Grid item xs={12}>
-        <Information text={"Skapa nytt objekt"} />
+        <DrawSaver
+          workspaceName={workspaceName}
+          setWorkspaceName={setWorkspaceName}
+        />
       </Grid>
       <Grid item xs={12}>
         <Information text={"Sparade objekt"} />
