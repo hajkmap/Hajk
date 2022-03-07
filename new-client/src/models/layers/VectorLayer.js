396d9921160d8e7ca10d164531f82ac92698773e
perosoderstrom <51197693+perosoderstrom@users.noreply.github.com>
2022-03-07 09:58:39 +0100

    Feature/additional vector layer styling (#878) (#953)
    Closes #878.

--- new-client/src/models/layers/VectorLayer.js
+++ new-client/src/models/layers/VectorLayer.js
@@ -4,6 +4,7 @@ import GeoJSON from "ol/format/GeoJSON";
 import GML2 from "ol/format/GML2";
 import GML3 from "ol/format/GML3";
 import WFS from "ol/format/WFS";
+import { Fill, Text, Stroke, Icon, Circle, Style } from "ol/style";
 import { all as strategyAll, bbox as bboxStrategy } from "ol/loadingstrategy";
 import { getPointResolution, transform } from "ol/proj";
 
@@ -25,7 +26,6 @@ const vectorLayerProperties = {
   },
   showLabels: true,
 };
-
 class WFSVectorLayer {
   constructor(config, proxyUrl, map) {
     this.config = {
@@ -34,9 +34,7 @@ class WFSVectorLayer {
     };
     this.proxyUrl = proxyUrl;
     this.map = map;
-
     this.type = "vector"; // We're dealing with a vector layer
-
     this.allFeatures = [];
 
     // Read the three filter properties from config to allow filter on load
@@ -57,12 +55,6 @@ class WFSVectorLayer {
       strategy:
         this.config?.loadingStrategy === "all" ? strategyAll : bboxStrategy,
     });
-
-    // if (config.legend[0].url === "") {
-    //   this.generateLegend((imageData) => {
-    //     config.legend[0].url = imageData;
-    //   });
-    // }
 
     this.layer = new VectorLayer({
       information: config.information,
@@ -98,13 +90,24 @@ class WFSVectorLayer {
         .then((response) => response.text())
         .then((text) => this.applySldTextOnLayer(text));
     }
-    // …else used supplied SLD text to style
+    // …else use supplied SLD text to style
     else if (
       typeof this.sldText === "string" &&
       this.sldText.trim().length > 10
     ) {
       this.applySldTextOnLayer(this.sldText);
-    } // …or just fall back to OpenLayer's default styling if no SLD/SLD URL was specified.
+    }
+    // ...else use OpenLayer's style if specified in config
+    else if (
+      config.icon ||
+      config.lineColor ||
+      config.lineWidth ||
+      config.fillColor ||
+      config.lineStyle
+    ) {
+      this.layer.setStyle(this.createStyle());
+    }
+    // …or just fall back to OpenLayer's default styling if no SLD/SLD URL or OpenLayers style was specified.
   }
 
   applySldTextOnLayer = (text) => {
@@ -293,71 +296,115 @@ class WFSVectorLayer {
     });
   }
 
-  // getStyle(forcedPointRadius) {
-  //   if (forcedPointRadius) {
-  //     return createStyle.call(this, undefined, forcedPointRadius);
-  //   }
-  //   return this.style;
-  // }
+  createStyle(forcedPointRadius) {
+    const icon = this.config.icon || "";
+    const fillColor = this.config.fillColor || "rgba(255,255,255,0.4)"; // OpenLayers default
+    const lineColor = this.config.lineColor || "#3399CC"; // OpenLayers default
+    const lineStyle = this.config.lineStyle || "";
+    const lineWidth = this.config.lineWidth || "";
+    const symbolXOffset = this.config.symbolXOffset || "";
+    const symbolYOffset = this.config.symbolYOffset || "";
+    const rotation = 0.0;
+    const align = this.config.labelAlign;
+    const baseline = this.config.labelBaseline;
+    const size = this.config.labelSize;
+    const offsetX = this.config.labelOffsetX;
+    const offsetY = this.config.labelOffsetY;
+    const weight = this.config.labelWeight;
+    const font = weight + " " + size + " " + this.config.labelFont;
+    const labelFillColor = this.config.labelFillColor;
+    const outlineColor = this.config.labelOutlineColor;
+    const outlineWidth = this.config.labelOutlineWidth;
+    const showLabels = this.config.showLabels;
+    const pointSize = forcedPointRadius || this.config.pointSize || 4; // OpenLayers default Circle use point size
+    const iconScale = pointSize / 8; // OpenLayers icon use scale. Medium size (8) => icon scale 1
 
-  // generateLegend(callback) {
-  //   var url = this.proxyUrl + this.createUrl();
-  //   fetch(url).then((response) => {
-  //     response.text().then((gmlText) => {
-  //       const parser = new GML2();
-  //       const features = parser.readFeatures(gmlText);
-  //       const canvas = document.createElement("canvas");
+    function getLineDash() {
+      var scale = (a, f) => a.map((b) => f * b),
+        width = lineWidth,
+        style = lineStyle,
+        dash = [12, 7],
+        dot = [2, 7];
+      switch (style) {
+        case "dash":
+          return width > 3 ? scale(dash, 2) : dash;
+        case "dot":
+          return width > 3 ? scale(dot, 2) : dot;
+        default:
+          return undefined;
+      }
+    }
 
-  //       const scale = 120;
-  //       const padding = 1 / 5;
-  //       const pointRadius = 15;
+    function getFill() {
+      return new Fill({
+        color: fillColor,
+      });
+    }
 
-  //       const vectorContext = toContext(canvas.getContext("2d"), {
-  //         size: [scale, scale],
-  //       });
-  //       const style = this.getStyle(pointRadius)[0];
-  //       vectorContext.setStyle(style);
+    function getText() {
+      return new Text({
+        textAlign: align,
+        textBaseline: baseline,
+        font: font,
+        text: "",
+        fill: new Fill({
+          color: labelFillColor,
+        }),
+        stroke: new Stroke({
+          color: outlineColor,
+          width: outlineWidth,
+        }),
+        offsetX: offsetX,
+        offsetY: offsetY,
+        rotation: rotation,
+      });
+    }
 
-  //       var featureType = "Point";
-  //       if (features.length > 0) {
-  //         featureType = features[0].getGeometry().getType();
-  //       }
+    function getImage() {
+      return icon === "" ? getPoint() : getIcon();
+    }
 
-  //       switch (featureType) {
-  //         case "Point":
-  //         case "MultiPoint":
-  //           vectorContext.drawGeometry(new Point([scale / 2, scale / 2]));
-  //           break;
-  //         case "Polygon":
-  //         case "MultiPolygon":
-  //           vectorContext.drawGeometry(
-  //             new Polygon([
-  //               [
-  //                 [scale * padding, scale * padding],
-  //                 [scale * padding, scale - scale * padding],
-  //                 [scale - scale * padding, scale - scale * padding],
-  //                 [scale - scale * padding, scale * padding],
-  //                 [scale * padding, scale * padding],
-  //               ],
-  //             ])
-  //           );
-  //           break;
-  //         case "LineString":
-  //         case "MultiLineString":
-  //           vectorContext.drawGeometry(
-  //             new LineString([
-  //               [scale * padding, scale - scale * padding],
-  //               [scale - scale * padding, scale * padding],
-  //             ])
-  //           );
-  //           break;
-  //         default:
-  //           break;
-  //       }
-  //       callback(canvas.toDataURL());
-  //     });
-  //   });
-  // }
+    function getIcon() {
+      return new Icon({
+        src: icon,
+        scale: iconScale,
+        anchorXUnits: "pixels",
+        anchorYUnits: "pixels",
+        anchor: [symbolXOffset, symbolYOffset],
+      });
+    }
+
+    function getPoint() {
+      return new Circle({
+        fill: getFill(),
+        stroke: getStroke(),
+        radius: parseInt(pointSize, 10) || 4,
+      });
+    }
+
+    function getStroke() {
+      return new Stroke({
+        color: lineColor,
+        width: lineWidth,
+        lineDash: getLineDash(),
+      });
+    }
+
+    function getStyleObj() {
+      var obj = {
+        fill: getFill(),
+        image: getImage(),
+        stroke: getStroke(),
+      };
+      if (showLabels) {
+        obj.text = getText();
+      }
+
+      return obj;
+    }
+
+    return [new Style(getStyleObj())];
+  }
 }
 
 export default WFSVectorLayer;
