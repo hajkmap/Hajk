c181d8ec56711aaed66930a1d4a87901d68aeef4
Jacob Wodzyński <jacob.wod@gmail.com>
2022-03-21 15:20:04 +0100

    Cleanups: remove unused variables

--- new-client/src/components/MapClickViewer/views/FeaturesListView.js
+++ new-client/src/components/MapClickViewer/views/FeaturesListView.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import ImageIcon from "@mui/icons-material/MapTwoTone";
-import { Divider, ListSubheader } from "@mui/material";
+import { ListSubheader } from "@mui/material";
 
 import Breadcrumbs from "./Breadcrumbs";
 import FeatureDetailView from "./FeatureDetailView";
