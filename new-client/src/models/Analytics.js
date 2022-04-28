fed5778318b1681df9eeffdd68ec0932855a088f
Jacob Wodzyński <jacob.wod@gmail.com>
2022-04-28 11:12:16 +0200

    Refactored. Added tracking of page view as well as map load, which also sends name of current map.

--- new-client/src/models/Analytics.js
+++ new-client/src/models/Analytics.js
@@ -1,5 +1,33 @@
 import Plausible from "plausible-tracker";
 
+/**
+ * @description Each of the provided analytics services must subscribe to two
+ * events on the global observer:
+ * analytics.trackPageView - takes no other parameters
+ * analytics.trackEvent - requires one parameter, data. The parameter should have this type:
+ *    {
+ *      eventName: string,
+ *      ...optionalProperties?: string | number
+ *    }
+ * Event name is required, while the remaining properties are optional, but could come
+ * in handy when we want to provide additional payload to the event sent.
+ *
+ * @example
+ * ```
+ * // Track a page view
+ * this.globalObserver.publish("analytics.trackPageView");
+ *
+ * // Track event named "customEvent", supply some payload
+ * this.globalObserver.publish("analytics.trackEvent", {
+ *  eventName: "customEvent",
+ *  paramA: "foo",
+ *  paramB: "bar"
+ * });
+ * ```
+ *
+ * @export
+ * @class Analytics
+ */
 export default class Analytics {
   constructor(config, globalObserver) {
     switch (config?.type) {
@@ -18,11 +46,14 @@ export default class Analytics {
 
         // These events should be called like this and subscribed to by
         // any analytics service. Below we define two events:
-        globalObserver.subscribe("trackPageview", () =>
+        globalObserver.subscribe("analytics.trackPageView", () =>
           plausible.trackPageview()
         );
-        globalObserver.subscribe("trackEvent", (eventName) =>
-          plausible.trackEvent(eventName)
+
+        globalObserver.subscribe(
+          "analytics.trackEvent",
+          ({ eventName, ...rest }) =>
+            plausible.trackEvent(eventName, { props: rest })
         );
         break;
       case "matomo":
