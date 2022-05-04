c2fa31c3712b173f773ce1360155e5e1e2df66c9
Tolfx <matteus@tolfx.dev>
2022-05-04 11:00:26 +0200

    feat: :construction: Dialog popup on which features to copy (#1075)

--- new-client/src/plugins/Sketch/views/AddView.js
+++ new-client/src/plugins/Sketch/views/AddView.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
 
+import Dialog from "../../../components/Dialog/Dialog";
 import DrawTypeSelector from "../components/DrawTypeSelector";
 import Information from "../components/Information";
 import FeatureStyleSelector from "../components/featureStyle/FeatureStyleSelector";
@@ -8,39 +15,88 @@ import FeatureTextSetter from "../components/FeatureTextSetter";
 
 const AddView = (props) => {
   // Let's destruct some properties from the props
-  const { model, activeDrawType, setActiveDrawType } = props;
+  const { model, activeDrawType, setActiveDrawType, localObserver, drawModel } =
+    props;
   // We have to get some information about the current activity (view)
   const activity = model.getActivityFromId(props.id);
 
+  const [selectedFeatures, setSelectedFeatures] = React.useState([]);
+  const handleSelectClick = React.useCallback((clickedFeatures) => {
+    setSelectedFeatures(clickedFeatures);
+  }, []);
+
+  const [selectedValue, setSelectedValue] = React.useState("");
+
+  React.useEffect(() => {
+    localObserver.subscribe("drawModel.select.click", handleSelectClick);
+    return () => {
+      localObserver.unsubscribe("drawModel.select.click");
+    };
+  }, [localObserver, handleSelectClick]);
+
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Information text={activity.information} />
-      </Grid>
-      <Grid item xs={12} style={{ marginTop: 16 }}>
-        <Grid item xs={12} style={{ marginBottom: 4 }}>
-          <Typography align="center">Typ</Typography>
-        </Grid>
+    <>
+      <Dialog
+        options={{
+          text: (
+            <>
+              <div>
+                <RadioGroup
+                  aria-label="ringtone"
+                  name="ringtone"
+                  value={selectedValue}
+                  onChange={(e) => setSelectedValue(e.target.value)}
+                >
+                  {selectedFeatures.map((feature, index) => (
+                    <FormControlLabel
+                      value={index}
+                      key={feature.getId()}
+                      control={<Radio />}
+                      label={feature.values_.namn}
+                    />
+                  ))}
+                </RadioGroup>
+              </div>
+            </>
+          ),
+          headerText: "Välj vilken feature du vill kopiera",
+          buttonText: "OK",
+          useLegacyNonMarkdownRenderer: true,
+        }}
+        open={selectedFeatures.length >= 2}
+        onClose={() => {
+          drawModel.drawSelectedIndex(selectedValue);
+        }}
+      ></Dialog>
+      <Grid container>
         <Grid item xs={12}>
-          <DrawTypeSelector
-            activeDrawType={activeDrawType}
-            setActiveDrawType={setActiveDrawType}
-          />
+          <Information text={activity.information} />
         </Grid>
+        <Grid item xs={12} style={{ marginTop: 16 }}>
+          <Grid item xs={12} style={{ marginBottom: 4 }}>
+            <Typography align="center">Typ</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <DrawTypeSelector
+              activeDrawType={activeDrawType}
+              setActiveDrawType={setActiveDrawType}
+            />
+          </Grid>
+        </Grid>
+        <FeatureStyleSelector
+          activeDrawType={activeDrawType}
+          drawStyle={props.drawStyle}
+          drawModel={props.drawModel}
+          setDrawStyle={props.setDrawStyle}
+          textStyle={props.textStyle}
+          setTextStyle={props.setTextStyle}
+        />
+        <FeatureTextSetter
+          localObserver={props.localObserver}
+          drawModel={props.drawModel}
+        />
       </Grid>
-      <FeatureStyleSelector
-        activeDrawType={activeDrawType}
-        drawStyle={props.drawStyle}
-        drawModel={props.drawModel}
-        setDrawStyle={props.setDrawStyle}
-        textStyle={props.textStyle}
-        setTextStyle={props.setTextStyle}
-      />
-      <FeatureTextSetter
-        localObserver={props.localObserver}
-        drawModel={props.drawModel}
-      />
-    </Grid>
+    </>
   );
 };
 
