hajkmap/Hajk

commit 09d36a73adf1de537ec9435c56789c8bb8ec8ac6

Added clarifying text for admins in Admin UI to distinguish displayFields from shortDisplayFields: - Bonus: class -> className in JSX.

new-admin/src/views/mapsettings.jsx
@@ -112,14 +112,14 @@ $.fn.editable = function (component) {
       id5 = Math.floor(Math.random() * 1e5),
       id6 = Math.floor(Math.random() * 1e5),
       id7 = Math.floor(Math.random() * 1e5),
-      ok = $('<span class="btn btn-success">OK</span>'),
-      layerOk = $('<span class="btn btn-success">OK</span>'),
-      layerOk2 = $('<span class="btn btn-success">OK</span>'),
+      ok = $('<span className="btn btn-success">OK</span>'),
+      layerOk = $('<span className="btn btn-success">OK</span>'),
+      layerOk2 = $('<span className="btn btn-success">OK</span>'),
       presetTools = $("<div></div>"),
       tools = $("<div></div>"),
       layerTools = $("<div></div>"),
-      abort = $('<span class="btn btn-default">Avbryt</span>'),
-      abort2 = $('<span class="btn btn-default">Avbryt</span>'),
+      abort = $('<span className="btn btn-default">Avbryt</span>'),
+      abort2 = $('<span className="btn btn-default">Avbryt</span>'),
       label = $(`<label for="${id}">Expanderad vid start&nbsp;</label>`),
       label2 = $(`<label for="${id2}">Toggla alla-knapp&nbsp;</label>`),
       label3 = $(`<label for="${id3}">Synlig vid start&nbsp;</label><br />`),
@@ -130,17 +130,17 @@ $.fn.editable = function (component) {
       checkbox2 = $(`<input id="${id2}" type="checkbox"/>`),
       checkbox3 = $(`<input id="${id3}" type="checkbox"/>`),
       checkbox4 = $(`<input id="${id4}" type="text" value="Nytt namn"/><br />`),
-      remove = $('<span class="fa fa-minus-circle"></span>'),
+      remove = $('<span className="fa fa-minus-circle"></span>'),
       input = $("<input />"),
       input2 = $(
         `<input id="${id5}" type="text" placeholder="Ny länk"/><br />`
       ),
       input3 = $(`<input id="${id6}" type="text" /><br />`),
       input4 = $(`<textarea id="${id7}" type="text"></textarea>`),
-      expanded = $('<div class="expanded-at-start"></div>'),
-      toggled = $('<div class="expanded-at-start"></div>'),
-      visible = $('<div class=""></div>'),
-      editPreset = $('<div class=""></div>'),
+      expanded = $('<div className="expanded-at-start"></div>'),
+      toggled = $('<div className="expanded-at-start"></div>'),
+      visible = $('<div className=""></div>'),
+      editPreset = $('<div className=""></div>'),
       elem = node.get(0) || {};
 
     ok.css(btnCSS).click(store);
@@ -799,10 +799,10 @@ class Menu extends Component {
 
     var layer = $(`
       <li
-        class="layer-node"
+        className="layer-node"
         data-id=${id}
         data-type="layer">
-        <span class="layer-name">${layerName}</span>
+        <span className="layer-name">${layerName}</span>
       </li>
     `);
 
@@ -818,13 +818,13 @@ class Menu extends Component {
     var id = this.createGuid();
     var group = $(`
       <li
-        class="group-node"
+        className="group-node"
         data-id="${id}"
         data-type="group"
         data-toggled="${toggled}"
         data-expanded="${expanded}"
         data-name="${name}">
-        <span class="group-name">${name}</span>
+        <span className="group-name">${name}</span>
         <ul></ul>
       </li>
       `);

new-admin/src/views/layerforms/wmslayerform.jsx
@@ -33,8 +33,8 @@ const defaultState = {
   hidpi: true,
   useCustomDpiList: false,
   customDpiList: [
-    { "pxRatio": 0, "dpi": 90 },
-    { "pxRatio": 2, "dpi": 180 }
+    { pxRatio: 0, dpi: 90 },
+    { pxRatio: 2, dpi: 180 },
   ],
   customRatio: 0,
   imageFormat: "",
@@ -121,8 +121,6 @@ class WMSLayerForm extends Component {
       });
       this.validateField("select-legend-icon");
     });
-
-
   }
 
   componentWillUnmount() {
@@ -330,16 +328,17 @@ class WMSLayerForm extends Component {
 
   renderLayerInfoInput(layerInfo) {
     var currentLayer = this.findInCapabilities(layerInfo.id);
+    console.log("currentLayer: ", currentLayer);
     var imageLoader = this.state.imageLoad ? (
       <i className="fa fa-refresh fa-spin" />
     ) : null;
 
     this.setState({
-      style: currentLayer.Style || [],
+      style: currentLayer?.Style || [],
     });
 
     let addedLayersInfo = this.state.addedLayersInfo;
-    addedLayersInfo[layerInfo.id].styles = currentLayer.Style;
+    addedLayersInfo[layerInfo.id].styles = currentLayer?.Style;
     this.setState({
       addedLayersInfo: addedLayersInfo,
     });
@@ -353,8 +352,8 @@ class WMSLayerForm extends Component {
       : null;
 
     return (
-      <div class="layerDialog">
-        <div class="form-row split3070">
+      <div className="layerDialog">
+        <div className="form-row split3070">
           <div>
             <label>Visningsnamn</label>
           </div>
@@ -376,40 +375,39 @@ class WMSLayerForm extends Component {
               }}
               type="text"
             />
-            </div>
+          </div>
         </div>
-        <div class="form-row">
+        <div className="form-row">
           <div>
             <label>Inforuta</label>
           </div>
 
-            <textarea
-              class="infoClick"
-              style={{ width: "100%" }}
-              value={layerInfo.infobox}
-              onChange={(e) => {
-                let addedLayersInfo = this.state.addedLayersInfo;
-                addedLayersInfo[layerInfo.id].infobox = e.target.value;
-                this.setState(
-                  {
-                    addedLayersInfo: addedLayersInfo,
-                  },
-                  () => {
-                    this.renderLayerInfoDialog(layerInfo);
-                  }
-                );
-              }}
-              type="text"
-            />
-
+          <textarea
+            className="infoClick"
+            style={{ width: "100%" }}
+            value={layerInfo.infobox}
+            onChange={(e) => {
+              let addedLayersInfo = this.state.addedLayersInfo;
+              addedLayersInfo[layerInfo.id].infobox = e.target.value;
+              this.setState(
+                {
+                  addedLayersInfo: addedLayersInfo,
+                },
+                () => {
+                  this.renderLayerInfoDialog(layerInfo);
+                }
+              );
+            }}
+            type="text"
+          />
         </div>
-        <div class="form-row split0">
+        <div className="form-row split0">
           <div>
             <label>Teckenförklaringsikon</label>
           </div>
           <div>
             <input
-              style={{marginRight: "5px"}}
+              style={{ marginRight: "5px" }}
               type="text"
               value={layerInfo.legendIcon}
               onChange={(e) => {
@@ -456,7 +454,7 @@ class WMSLayerForm extends Component {
           </div>
         </div>
 
-        <div class="form-row split0">
+        <div className="form-row split0">
           <div>
             <label>Stil</label>
           </div>
@@ -485,7 +483,7 @@ class WMSLayerForm extends Component {
 
         <div className="separator">Infoclick</div>
 
-        <div class="form-row split0">
+        <div className="form-row split0">
           <div>
             <label>Infoklick</label>
           </div>
@@ -509,7 +507,7 @@ class WMSLayerForm extends Component {
             />
           </div>
         </div>
-        <div class="form-row split0">
+        <div className="form-row split0">
           <div>
             <label>Infoclick-ikon</label> (
             <a
@@ -544,9 +542,14 @@ class WMSLayerForm extends Component {
 
         <div className="separator">Infoclick och sökning</div>
 
-        <div class="form-row split50">
+        <div className="form-row split50">
           <div>
-            <label>Visningsfält (i resultatlistan)</label>
+            <label>
+              Visningsfält (i resultatlistan){" "}
+              <abbr title="Visas i sökresultatlistan. Dessutom kan visas som etikett i kartan när användaren selekterat ett sökresultat, om 'Visa resultat i kartan' är aktivt för sökverktyget. Anges som kommaseparerad lista.">
+                (?)
+              </abbr>
+            </label>
           </div>
           <div>
             <input
@@ -555,7 +558,8 @@ class WMSLayerForm extends Component {
               value={layerInfo.searchDisplayName}
               onChange={(e) => {
                 let addedLayersInfo = this.state.addedLayersInfo;
-                addedLayersInfo[layerInfo.id].searchDisplayName = e.target.value;
+                addedLayersInfo[layerInfo.id].searchDisplayName =
+                  e.target.value;
                 this.setState(
                   {
                     addedLayersInfo: addedLayersInfo,
@@ -568,9 +572,14 @@ class WMSLayerForm extends Component {
             />
           </div>
         </div>
-        <div class="form-row split50">
+        <div className="form-row split50">
           <div>
-            <label>Sekundära visningsfält (i resultatlistan)</label>
+            <label>
+              Sekundära visningsfält (i resultatlistan){" "}
+              <abbr title="Visas i sökresultatlistan som en andra rad med något mindre textstorlek under den första raden (Visningsfält). ">
+                (?)
+              </abbr>
+            </label>
           </div>
           <div>
             <input
@@ -593,9 +602,14 @@ class WMSLayerForm extends Component {
             />
           </div>
         </div>
-        <div class="form-row split50">
+        <div className="form-row split50">
           <div>
-            <label>Kort visningsfält (etikett i kartan)</label>
+            <label>
+              Kort visningsfält{" "}
+              <abbr title="Visas som etikett bredvid sökresultat i ett första läge, om 'Visa resultat i kartan' är aktivt för sökverktyget. Anges som kommaseparerad lista.">
+                (?)
+              </abbr>
+            </label>
           </div>
           <div>
             <input
@@ -621,7 +635,7 @@ class WMSLayerForm extends Component {
 
         <div className="separator">Sökning</div>
 
-        <div class="form-row split3070">
+        <div className="form-row split3070">
           <div>
             <label>Url</label>
           </div>
@@ -645,9 +659,14 @@ class WMSLayerForm extends Component {
             />
           </div>
         </div>
-        <div class="form-row split3070">
+        <div className="form-row split3070">
           <div>
-            <label>Sökfält</label>
+            <label>
+              Sökfält{" "}
+              <abbr title="Styr vilka attribut (kolumner i tabellen) som sökning sker mot. Anges som kommaseparerad lista.">
+                (?)
+              </abbr>
+            </label>
           </div>
           <div>
             <input
@@ -656,7 +675,8 @@ class WMSLayerForm extends Component {
               value={layerInfo.searchPropertyName}
               onChange={(e) => {
                 let addedLayersInfo = this.state.addedLayersInfo;
-                addedLayersInfo[layerInfo.id].searchPropertyName = e.target.value;
+                addedLayersInfo[layerInfo.id].searchPropertyName =
+                  e.target.value;
                 this.setState(
                   {
                     addedLayersInfo: addedLayersInfo,
@@ -670,7 +690,7 @@ class WMSLayerForm extends Component {
           </div>
         </div>
 
-        <div class="form-row split3070">
+        <div className="form-row split3070">
           <div>
             <label>Utdataformat</label>
           </div>
@@ -681,7 +701,8 @@ class WMSLayerForm extends Component {
               value={layerInfo.searchOutputFormat}
               onChange={(e) => {
                 let addedLayersInfo = this.state.addedLayersInfo;
-                addedLayersInfo[layerInfo.id].searchOutputFormat = e.target.value;
+                addedLayersInfo[layerInfo.id].searchOutputFormat =
+                  e.target.value;
                 this.setState(
                   {
                     addedLayersInfo: addedLayersInfo,
@@ -694,7 +715,7 @@ class WMSLayerForm extends Component {
             />
           </div>
         </div>
-        <div class="form-row split3070">
+        <div className="form-row split3070">
           <div>
             <label>Geometrifält</label>
           </div>
@@ -906,11 +927,14 @@ class WMSLayerForm extends Component {
           minMaxZoomAlertOnToggleOnly:
             layer.minMaxZoomAlertOnToggleOnly ?? false,
           useCustomDpiList: layer.useCustomDpiList ?? false,
-          customDpiList: layer.customDpiList?.length > 0 ? layer.customDpiList : [
-            { "pxRatio": 0, "dpi": 90 },
-            { "pxRatio": 2, "dpi": 180 },
-            { "pxRatio": 3, "dpi": 270 }
-          ],
+          customDpiList:
+            layer.customDpiList?.length > 0
+              ? layer.customDpiList
+              : [
+                  { pxRatio: 0, dpi: 90 },
+                  { pxRatio: 2, dpi: 180 },
+                  { pxRatio: 3, dpi: 270 },
+                ],
         },
         () => {
           this.setServerType();
@@ -1160,7 +1184,9 @@ class WMSLayerForm extends Component {
       singleTile: this.getValue("singleTile"),
       hidpi: this.getValue("hidpi"),
       useCustomDpiList: this.state.useCustomDpiList,
-      customDpiList: this.state.useCustomDpiList ? this.state.customDpiList : [],
+      customDpiList: this.state.useCustomDpiList
+        ? this.state.customDpiList
+        : [],
       customRatio: this.getValue("customRatio"),
       imageFormat: this.getValue("imageFormat"),
       serverType: this.getValue("serverType"),
@@ -1360,33 +1386,35 @@ class WMSLayerForm extends Component {
     this.setState({ workspaceList: sortedWorksapes });
   };
 
-  updateDpiList(e, kv, key){
-    const index = this.state.customDpiList.findIndex(o => o === kv);
+  updateDpiList(e, kv, key) {
+    const index = this.state.customDpiList.findIndex((o) => o === kv);
 
-    if(e.target.value.includes(".") || e.target.value.includes(",")){
-      kv[key] = parseFloat(parseFloat(e.target.value.replace(",", ".")).toFixed(1));
-    }else{
+    if (e.target.value.includes(".") || e.target.value.includes(",")) {
+      kv[key] = parseFloat(
+        parseFloat(e.target.value.replace(",", ".")).toFixed(1)
+      );
+    } else {
       kv[key] = parseInt(e.target.value);
     }
-    
+
     let newValue = [...this.state.customDpiList];
     newValue[index] = kv;
-    this.setState({customDpiList: newValue});
+    this.setState({ customDpiList: newValue });
   }
 
-  removeDpiListRow(e, index){
-    if(this.state.customDpiList.length <= 1){
+  removeDpiListRow(e, index) {
+    if (this.state.customDpiList.length <= 1) {
       return;
     }
     let newValue = [...this.state.customDpiList];
     newValue.splice(index, 1);
-    this.setState({customDpiList: newValue});
+    this.setState({ customDpiList: newValue });
   }
 
-  addDpiListRow(){
+  addDpiListRow() {
     let newValue = [...this.state.customDpiList];
     newValue.push({ pxRatio: 0, dpi: 90 });
-    this.setState({customDpiList: newValue});
+    this.setState({ customDpiList: newValue });
   }
 
   render() {
@@ -1588,49 +1616,81 @@ class WMSLayerForm extends Component {
             type="checkbox"
             ref="input_useCustomDpiList"
             id="input_useCustomDpiList"
-            onChange={(e) => this.setState({ useCustomDpiList: e.target.checked })}
+            onChange={(e) =>
+              this.setState({ useCustomDpiList: e.target.checked })
+            }
             checked={this.state.useCustomDpiList}
           />
           &nbsp;
           <label htmlFor="input_useCustomDpiList">
-            Custom DPI {" "}
+            Custom DPI{" "}
             <i
               className="fa fa-question-circle"
               data-toggle="tooltip"
               title="Hämta 'kartbilder' med specifik dpi vid pixelRatio-brytpunkt."
             />
           </label>
-          {this.state.useCustomDpiList
-            ? 
+          {this.state.useCustomDpiList ? (
             <div>
-              <div style={{display: "flex", color: "#c0c0c0"}}>
-                <div style={{width: "155px"}}>pixel ratio</div>
-                <div style={{width: "155px"}}>dpi</div>
-              </div>              
-            {
-            this.state.customDpiList.map((kv, index) =>
-              <div key={`${kv.pxRatio}__${kv.dpi}__${index}`} style={{display: "flex"}}>
-                <div>
-                  <input type="text" defaultValue={kv.pxRatio} style={{width: "150px"}}
-                    onBlur={(e) => { this.updateDpiList(e, kv, "pxRatio"); }}/>
-                </div>
-                <div>
-                  <input type="text" defaultValue={kv.dpi} style={{width: "150px"}}
-                    onBlur={(e) => { this.updateDpiList(e, kv, "dpi"); }}/>
-                    <button type="button" className="btn btn-default" style={{fontWeight: "bold"}} disabled={this.state.customDpiList.length === 1}
-                      onClick={(e)=>{ this.removeDpiListRow(e, index); }}>-</button>
-                    {index === this.state.customDpiList.length-1 ?
-                    <button type="button" className="btn btn-default" style={{fontWeight: "bold", marginLeft: "5px"}}
-                      onClick={(e)=>{ this.addDpiListRow(); }}>+</button>
-                    : ""  
-                  }
-                </div>
+              <div style={{ display: "flex", color: "#c0c0c0" }}>
+                <div style={{ width: "155px" }}>pixel ratio</div>
+                <div style={{ width: "155px" }}>dpi</div>
               </div>
-            )
-            }
+              {this.state.customDpiList.map((kv, index) => (
+                <div
+                  key={`${kv.pxRatio}__${kv.dpi}__${index}`}
+                  style={{ display: "flex" }}
+                >
+                  <div>
+                    <input
+                      type="text"
+                      defaultValue={kv.pxRatio}
+                      style={{ width: "150px" }}
+                      onBlur={(e) => {
+                        this.updateDpiList(e, kv, "pxRatio");
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="text"
+                      defaultValue={kv.dpi}
+                      style={{ width: "150px" }}
+                      onBlur={(e) => {
+                        this.updateDpiList(e, kv, "dpi");
+                      }}
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-default"
+                      style={{ fontWeight: "bold" }}
+                      disabled={this.state.customDpiList.length === 1}
+                      onClick={(e) => {
+                        this.removeDpiListRow(e, index);
+                      }}
+                    >
+                      -
+                    </button>
+                    {index === this.state.customDpiList.length - 1 ? (
+                      <button
+                        type="button"
+                        className="btn btn-default"
+                        style={{ fontWeight: "bold", marginLeft: "5px" }}
+                        onClick={(e) => {
+                          this.addDpiListRow();
+                        }}
+                      >
+                        +
+                      </button>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+              ))}
             </div>
-            : null }
-        </div>        
+          ) : null}
+        </div>
         <div>
           <input
             type="checkbox"

new-admin/src/views/search.jsx
@@ -821,7 +821,12 @@ class Search extends Component {
                 </div>
               </div>
               <div>
-                <label>Visningsnamn*</label>
+                <label>
+                  Visningsnamn*{" "}
+                  <abbr title="Visas för användaren i bland annat sökresultatlistan som namnet på datamängden man söker i.">
+                    (?)
+                  </abbr>
+                </label>
                 <input
                   type="text"
                   ref="input_caption"
@@ -838,7 +843,12 @@ class Search extends Component {
                 />
               </div>
               <div>
-                <label>Visningsnamn Admin</label>
+                <label>
+                  Visningsnamn Admin UI{" "}
+                  <abbr title="Visas INTE för användaren. Lokalt namn som endast visas i administrationsgränssnittet och kan användas för att särskilja om flera lager behöver ha samma Visningsnamn utåt.">
+                    (?)
+                  </abbr>
+                </label>
                 <input
                   type="text"
                   ref="input_internalLayerName"
@@ -850,7 +860,12 @@ class Search extends Component {
                 />
               </div>
               <div>
-                <label>Inforuta</label>
+                <label>
+                  Inforuta{" "}
+                  <abbr title="Styr hur resultatet visas i detaljvyn. Referera till dokumentationen för Sökverktyget samt Infoclick (för tillåten syntax i infoboxen). Se Hajks Wiki på GitHub.">
+                    (?)
+                  </abbr>
+                </label>
                 <textarea
                   ref="input_infobox"
                   value={this.state.infobox}
@@ -868,7 +883,12 @@ class Search extends Component {
                 />
               </div>
               <div>
-                <label>Sökfält</label>
+                <label>
+                  Sökfält{" "}
+                  <abbr title="Styr vilka attribut (kolumner i tabellen) som sökning sker mot. Anges som kommaseparerad lista.">
+                    (?)
+                  </abbr>
+                </label>
                 <input
                   type="text"
                   ref="input_searchFields"
@@ -885,7 +905,12 @@ class Search extends Component {
                 />
               </div>
               <div>
-                <label>Visningsfält</label>
+                <label>
+                  Visningsfält{" "}
+                  <abbr title="Visas i sökresultatlistan. Dessutom kan visas som etikett i kartan när användaren selekterat ett sökresultat, om 'Visa resultat i kartan' är aktivt för sökverktyget. Anges som kommaseparerad lista.">
+                    (?)
+                  </abbr>
+                </label>
                 <input
                   type="text"
                   ref="input_displayFields"
@@ -902,7 +927,12 @@ class Search extends Component {
                 />
               </div>
               <div>
-                <label>Kort visningsfält</label>
+                <label>
+                  Kort visningsfält{" "}
+                  <abbr title="Visas som etikett bredvid sökresultat i ett första läge, om 'Visa resultat i kartan' är aktivt för sökverktyget. Anges som kommaseparerad lista.">
+                    (?)
+                  </abbr>
+                </label>
                 <input
                   type="text"
                   ref="input_shortDisplayFields"

commit b1e1543fc787ae57c976f74259cfadd1809c3fc3

Fixed a regression introduced in 09d36a73adf1de537ec9435c56789c8bb8ec8ac6: only JSX parts of the code need the class->className change.

new-admin/src/views/mapsettings.jsx
@@ -112,14 +112,14 @@ $.fn.editable = function (component) {
       id5 = Math.floor(Math.random() * 1e5),
       id6 = Math.floor(Math.random() * 1e5),
       id7 = Math.floor(Math.random() * 1e5),
-      ok = $('<span className="btn btn-success">OK</span>'),
-      layerOk = $('<span className="btn btn-success">OK</span>'),
-      layerOk2 = $('<span className="btn btn-success">OK</span>'),
+      ok = $('<span class="btn btn-success">OK</span>'),
+      layerOk = $('<span class="btn btn-success">OK</span>'),
+      layerOk2 = $('<span class="btn btn-success">OK</span>'),
       presetTools = $("<div></div>"),
       tools = $("<div></div>"),
       layerTools = $("<div></div>"),
-      abort = $('<span className="btn btn-default">Avbryt</span>'),
-      abort2 = $('<span className="btn btn-default">Avbryt</span>'),
+      abort = $('<span class="btn btn-default">Avbryt</span>'),
+      abort2 = $('<span class="btn btn-default">Avbryt</span>'),
       label = $(`<label for="${id}">Expanderad vid start&nbsp;</label>`),
       label2 = $(`<label for="${id2}">Toggla alla-knapp&nbsp;</label>`),
       label3 = $(`<label for="${id3}">Synlig vid start&nbsp;</label><br />`),
@@ -130,17 +130,17 @@ $.fn.editable = function (component) {
       checkbox2 = $(`<input id="${id2}" type="checkbox"/>`),
       checkbox3 = $(`<input id="${id3}" type="checkbox"/>`),
       checkbox4 = $(`<input id="${id4}" type="text" value="Nytt namn"/><br />`),
-      remove = $('<span className="fa fa-minus-circle"></span>'),
+      remove = $('<span class="fa fa-minus-circle"></span>'),
       input = $("<input />"),
       input2 = $(
         `<input id="${id5}" type="text" placeholder="Ny länk"/><br />`
       ),
       input3 = $(`<input id="${id6}" type="text" /><br />`),
       input4 = $(`<textarea id="${id7}" type="text"></textarea>`),
-      expanded = $('<div className="expanded-at-start"></div>'),
-      toggled = $('<div className="expanded-at-start"></div>'),
-      visible = $('<div className=""></div>'),
-      editPreset = $('<div className=""></div>'),
+      expanded = $('<div class="expanded-at-start"></div>'),
+      toggled = $('<div class="expanded-at-start"></div>'),
+      visible = $('<div class=""></div>'),
+      editPreset = $('<div class=""></div>'),
       elem = node.get(0) || {};
 
     ok.css(btnCSS).click(store);
@@ -799,10 +799,10 @@ class Menu extends Component {
 
     var layer = $(`
       <li
-        className="layer-node"
+        class="layer-node"
         data-id=${id}
         data-type="layer">
-        <span className="layer-name">${layerName}</span>
+        <span class="layer-name">${layerName}</span>
       </li>
     `);
 
@@ -818,13 +818,13 @@ class Menu extends Component {
     var id = this.createGuid();
     var group = $(`
       <li
-        className="group-node"
+        class="group-node"
         data-id="${id}"
         data-type="group"
         data-toggled="${toggled}"
         data-expanded="${expanded}"
         data-name="${name}">
-        <span className="group-name">${name}</span>
+        <span class="group-name">${name}</span>
         <ul></ul>
       </li>
       `);