5d61c215eaee1d94eca5dac5217e63d83dd0a093
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-09-14 08:53:14 +0200

    Added clarifying comments

--- new-client/src/plugins/Dummy/DummyModel.js
+++ new-client/src/plugins/Dummy/DummyModel.js
@@ -4,25 +4,29 @@ export default class DummyModel {
   #localObserver;
 
   constructor(settings) {
+    // Set some private fields
     this.#map = settings.map;
     this.#app = settings.app;
     this.#localObserver = settings.localObserver;
-
-    this.#initSubscriptions();
+    this.#initSubscriptions(); // Initiate listeners on observer(s)
   }
 
+  // Sets up listeners on observers
   #initSubscriptions = () => {
     this.#localObserver.subscribe("dummyEvent", this.#handleDummyEvent);
   };
 
+  // Example of an event handler for observer-event
   #handleDummyEvent = (message = "") => {
     console.log(`Dummy-event caught in model! Message: ${message}`);
   };
 
+  // Example of public method, returns the map instance
   getMap = () => {
     return this.#map;
   };
 
+  // Example of public method, returns the app instance
   getApp = () => {
     return this.#app;
   };
