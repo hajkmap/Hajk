95fc12e7abff6332468d0434bb6e4aa48ad9bc9c
Martin Arvroth Thorbjörnsson <martin.thorbjornsson@sweco.se>
2022-11-08 08:50:22 +0100

    GEOU-1473: Added model Document and DocumentsList

---
+++ backend-dotnet/MapService/Models/Document.cs
@@ -0,0 +1,36 @@
+﻿namespace MapService.Models
+{
+    public class Document
+    {
+        public string? title { get; set; }
+
+        public string? map { get; set; }
+
+        public List<Chapter> chapters { get; set; }
+
+        public Document()
+        {
+            this.chapters = new List<Chapter>();
+        }
+
+        public class Chapter
+        {
+            public string? header { get; set; }
+
+            public string? headerIdentifier { get; set; }
+
+            public string? html { get; set; }
+
+            public string? keywords { get; set; }
+
+            public bool? expanded { get; set; }
+
+            public List<Chapter> chapters { get; set; }
+
+            public Chapter()
+            {
+                this.chapters = new List<Chapter>();
+            }
+        }
+    }
+}
