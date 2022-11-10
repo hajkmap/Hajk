c12e21681f93161140e04baa7bd805c82aad3cad
Mattias Strömberg <mattias.stromberg@sweco.se>
2022-11-10 16:22:22 +0100

    Add endpoint config/listvideo for backwards compability

--- backend-dotnet/MapService/Controllers/ConfigController.cs
+++ backend-dotnet/MapService/Controllers/ConfigController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MapService.Business.MapConfig;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace MapService.Controllers
 {
@@ -8,6 +9,34 @@ namespace MapService.Controllers
     [ApiController]
     public class ConfigController : ControllerBase
     {
+        private readonly ILogger<ConfigController> _logger;
 
+        public ConfigController(ILogger<ConfigController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        [Route("listvideo")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(Tags = new[] { "Admin - Maps and layers" })]
+        [Obsolete]
+        public ActionResult<IEnumerable<string>> GetListVideo()
+        {
+            var listOfVideos = new List<string>();
+
+            try
+            {
+                listOfVideos = MapConfigHandler.GetListOfVideos().ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Internal Server Error");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+            }
+
+            return listOfVideos;
+        }
     }
 }
