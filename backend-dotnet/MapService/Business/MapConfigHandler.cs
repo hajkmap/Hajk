88b212751e65a4d6d0a42356cb53c62a94b21137
Mattias Strömberg <mattias.stromberg@sweco.se>
2022-11-10 16:17:28 +0100

    Add endpoint mapconfig/listvideo

--- backend-dotnet/MapService/Business/MapConfigHandler.cs
+++ backend-dotnet/MapService/Business/MapConfigHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace MapService.Business.MapConfig
+﻿using MapService.DataAccess;
+
+namespace MapService.Business.MapConfig
 {
-    public class MapConfigHandler
+    public static class MapConfigHandler
     {
+        public static IEnumerable<string> GetListOfVideos()
+        {
+            var videoFileNameList = new List<string>();
+
+            var uploadContentRootPath = AppDomain.CurrentDomain.GetData("UploadContentRootPath") as string;
+
+            if (uploadContentRootPath == null)
+            {
+                return videoFileNameList;
+            }
+
+            var files = FolderDataAccess.GetAllFiles(uploadContentRootPath);
+
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file).ToLower() == ".mp4" || Path.GetExtension(file).ToLower() == ".mov" || Path.GetExtension(file).ToLower() == ".ogg")
+                {
+                    var fileName = Path.GetFileName(file);
+                    videoFileNameList.Add(fileName);
+                }
+            }
+
+            return videoFileNameList;
+        }
     }
 }
--- backend-dotnet/MapService/Controllers/MapConfigController.cs
+++ backend-dotnet/MapService/Controllers/MapConfigController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MapService.Business.MapConfig;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace MapService.Controllers
 {
@@ -8,6 +9,33 @@ namespace MapService.Controllers
     [ApiController]
     public class MapConfigController : ControllerBase
     {
+        private readonly ILogger<MapConfigController> _logger;
 
+        public MapConfigController(ILogger<MapConfigController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        [Route("listvideo")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(Tags = new[] { "Admin - Maps and layers" })]
+        public ActionResult<IEnumerable<string>> GetListVideo()
+        {
+            var listOfVideos = new List<string>();
+
+            try
+            {
+                listOfVideos = MapConfigHandler.GetListOfVideos().ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Internal Server Error");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+            }
+
+            return listOfVideos;
+        }
     }
 }
