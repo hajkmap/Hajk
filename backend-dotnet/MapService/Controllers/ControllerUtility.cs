05a70fe196f704e724f1f27c8e76f0c92f00f838
Martin Arvroth Thorbjörnsson <martin.thorbjornsson@sweco.se>
2022-11-16 14:57:45 +0100

    GEOU-1493: Added Utility class

---
+++ backend-dotnet/MapService/Controllers/ControllerUtility.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace MapService.Controllers
+{
+    public static class ControllerUtility
+    {
+        /// <summary>
+        /// Serialize an anonymous object as a json string and then Deserialize the json string as an Json Object. 
+        /// </summary>
+        /// <param name="anonymousObject">An anonomys object</param>
+        /// <returns>Returns a Json Object</returns>
+        public static JsonObject? ConvertToJsonObject(dynamic anonymousObject)
+        {
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
+            string serializedJson = JsonSerializer.Serialize(anonymousObject, jsonOptions);
+            JsonObject? deserializedJson;
+            try
+            {
+                deserializedJson = JsonSerializer.Deserialize<JsonObject>(serializedJson);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return deserializedJson;
+        }
+    }
+}
--- backend-dotnet/MapService/Controllers/MapConfigController.cs
+++ backend-dotnet/MapService/Controllers/MapConfigController.cs
@@ -260,38 +260,12 @@ namespace MapService.Controllers
                 }
             }
 
-            return Deserialize(layerExportItems);
+            return ControllerUtility.ConvertToJsonObject(layerExportItems);
         }
 
         private JsonObject GetMaps(JsonObject jsonObject)
         {
             return jsonObject;
-        }
-
-        /// <summary>
-        /// Serialize an anonymous object as a json string and then Deserialize the json string as an Json Object. 
-        /// </summary>
-        /// <param name="anonymousObject">An anonomys object</param>
-        /// <returns>Returns a Json Object</returns>
-        private JsonObject? Deserialize(dynamic anonymousObject)
-        {
-            var jsonOptions = new JsonSerializerOptions()
-            {
-                WriteIndented = true,
-                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-            };
-            string serializedJson = JsonSerializer.Serialize(anonymousObject, jsonOptions);
-            JsonObject? deserializedJson;
-            try
-            {
-                deserializedJson = JsonSerializer.Deserialize<JsonObject>(serializedJson);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
-            return deserializedJson;
         }
     }
 }
