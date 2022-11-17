5c0b9bcb42e16055ffaaaed095408dd586d51625
Mattias Strömberg <mattias.stromberg@sweco.se>
2022-11-17 17:32:16 +0100

    Add endpoint /spec

---
+++ backend-dotnet/MapService/Business/SpecificationHandler.cs
@@ -0,0 +1,13 @@
+﻿using MapService.DataAccess;
+using System.Text.Json.Nodes;
+
+namespace MapService.Business
+{
+    public static class SpecificationHandler
+    {
+        public static JsonObject GetSpecification()
+        {
+            return JsonFileDataAccess.GetSpecification();
+        }
+    }
+}
