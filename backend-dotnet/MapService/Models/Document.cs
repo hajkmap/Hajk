14cd13923f63f5f7a9b5288a9df90833cba3587d
Martin Arvroth Thorbjörnsson <martin.thorbjornsson@sweco.se>
2022-12-19 11:13:02 +0100

    GEOU-1546: Rearranged properties

--- backend-dotnet/MapService/Models/Document.cs
+++ backend-dotnet/MapService/Models/Document.cs
@@ -1,19 +1,33 @@
 ﻿namespace MapService.Models
 {
+    /// <summary>
+    /// Class representation of a document. 
+    /// </summary>
     public class Document
     {
-        //public string? title { get; set; }
-
-        public string? map { get; set; }
-
+        /// <summary>
+        /// Gets or sets the chapters in the document. 
+        /// </summary>
         public List<Chapter> chapters { get; set; }
 
+        /// <summary>
+        /// Gets or sets the map name this document is associated with. 
+        /// </summary>
+        public string? map { get; set; }
+
+        /// <summary>
+        /// Creates a new document. 
+        /// </summary>
+        /// <param name="map">The map name this document is associated with, </param>
         public Document(string map)
         {
             this.map = map;
             this.chapters = new List<Chapter>();
         }
 
+        /// <summary>
+        /// Inner class that represents a chapter. 
+        /// </summary>
         public class Chapter
         {
             //public string? header { get; set; }
@@ -28,6 +42,9 @@
 
             //public List<Chapter> chapters { get; set; }
 
+            /// <summary>
+            /// Creates a new chapter. 
+            /// </summary>
             public Chapter()
             {
                 //this.chapters = new List<Chapter>();
