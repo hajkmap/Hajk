3ca7f5d035a0d70a6554061b4aeb61737d5ad699
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2023-09-08 08:36:27 +0200

    The scale-list should apparently be strings

--- new-client/src/plugins/TimeSlider/constants/index.js
+++ new-client/src/plugins/TimeSlider/constants/index.js
@@ -1,5 +1,16 @@
 export const DEFAULT_PRINT_OPTIONS = Object.freeze({
-  scales: [200, 400, 1000, 2000, 5000, 10000, 25000, 50000, 100000, 200000],
+  scales: [
+    "200",
+    "400",
+    "1000",
+    "2000",
+    "5000",
+    "10000",
+    "25000",
+    "50000",
+    "100000",
+    "200000",
+  ],
   scaleMeters: [20, 40, 40, 100, 200, 200, 400, 600, 2000, 4000, 10000, 20000],
   includeScaleBar: true,
   scaleBarPlacement: "bottomLeft",
