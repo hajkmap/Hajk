6e711814a878a925ca4032ec15eafbba039a11fc
Mattias Andrén <mattias.andren@zinet.se>
2023-10-04 10:21:45 +0200

    Fix to use Swagger when the backend runs in a subdirectory (named mapservice).

--- backend-dotnet/MapService/Program.cs
+++ backend-dotnet/MapService/Program.cs
@@ -85,9 +85,17 @@ app.UseSwagger();
 
 app.UseSwaggerUI(c =>
 {
-    //c.SwaggerEndpoint("/api/v1/spec", "HAJK Backend");
-    c.SwaggerEndpoint("/swagger/v2/swagger.yml", "HAJK .NET Backend v2");
-    c.SwaggerEndpoint("/swagger/v1/swagger.yml", "HAJK .NET Backend v1");
+    //c.SwaggerEndpoint("/api/v1/spec", "HAJK Backend");    
+    if (app.Environment.IsDevelopment()) // Development (i.e installed in the root of the site)
+    {
+        c.SwaggerEndpoint("/swagger/v2/swagger.yml", "HAJK .NET Backend v2");
+        c.SwaggerEndpoint("/swagger/v1/swagger.yml", "HAJK .NET Backend v1");
+    }
+    else // Release (i.e installed in folder mapservice in Hajk deployment)
+    {
+        c.SwaggerEndpoint("/mapservice/swagger/v2/swagger.yml", "HAJK .NET Backend v2");
+        c.SwaggerEndpoint("/mapservice/swagger/v1/swagger.yml", "HAJK .NET Backend v1");
+    }
 });
 //}
 
