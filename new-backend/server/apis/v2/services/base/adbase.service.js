d1ab5c02fc6e9f25ffa7e80165f3b2f77ba60f63
jesade-vbg <jesade.vbg@gmail.com>
2023-12-19 11:25:05 +0100

    Added possibility to override groups env using AD_OVERRIDE_USER_GROUPS_WITH_VALUE. Some refactoring.

--- new-backend/server/apis/v2/services/base/adbase.service.js
+++ new-backend/server/apis/v2/services/base/adbase.service.js
@@ -5,6 +5,31 @@ const ErrorNotImplemented = "The method '{name}' is not implemented!";
 class AdBaseService {
   constructor() {
     this.logger = log4js.getLogger("service.auth.v2");
+    this.initEnvOverrides();
+  }
+
+  initEnvOverrides() {
+    // set for later use and warn if they are set as this should not be used in production.
+    this.overrideUser = this.getEnv(process.env.AD_OVERRIDE_USER_WITH_VALUE);
+    this.overrideUserGroups = this.getEnv(
+      process.env.AD_OVERRIDE_USER_GROUPS_WITH_VALUE
+    );
+
+    if (this.overrideUserGroups) {
+      this.overrideUserGroups = this.overrideUserGroups
+        .split(",")
+        .map((group) => group.trim());
+      this.logger.warn(
+        'AD_OVERRIDE_USER_GROUPS_WITH_VALUE is set in .env! Will use "%s" as groups for all AD functions. DON\'T USE THIS IN PRODUCTION!',
+        this.overrideUserGroups
+      );
+    }
+    if (this.overrideUser) {
+      this.logger.warn(
+        'AD_OVERRIDE_USER_WITH_VALUE is set in .env! Will use "%s" as user name for all AD functions. DON\'T USE THIS IN PRODUCTION!',
+        this.overrideUser
+      );
+    }
   }
 
   init() {
@@ -58,6 +83,13 @@ class AdBaseService {
     return [];
   }
 
+  getEnv(env) {
+    if (env !== undefined && ("" + env).trim().length > 0) {
+      return env;
+    }
+    return null;
+  }
+
   getUserFromRequestHeader(req) {
     if (process.env.AD_LOOKUP_ACTIVE !== "true") {
       // If AD_LOOKUP_ACTIVE is anything else than "true", we don't care
@@ -67,16 +99,14 @@ class AdBaseService {
       // AD authentication is active.
       //
       // First see if webmaster wants to override the header value (useful for developing and testing)
-      if (
-        process.env.AD_OVERRIDE_USER_WITH_VALUE !== undefined &&
-        process.env.AD_OVERRIDE_USER_WITH_VALUE.trim().length !== 0
-      ) {
-        this.logger.warn(
-          'AD_OVERRIDE_USER_WITH_VALUE is set in .env! Will use "%s" as user name for all AD functions. DON\'T USE THIS IN PRODUCTION!',
-          process.env.AD_OVERRIDE_USER_WITH_VALUE
-        );
 
-        return process.env.AD_OVERRIDE_USER_WITH_VALUE;
+      if (this.overrideUser) {
+        if (this.postRequestHandler) {
+          this.postRequestHandler(req, this.overrideUser);
+        }
+
+        // user is overridden in .env, return it!
+        return this.overrideUser;
       }
 
       // Now it's time to take care of the _real_ AD authentication!
