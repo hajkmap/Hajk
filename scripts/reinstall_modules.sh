a500df5207edf8ad9a4ccac8385d63bd68469373
Jacob Wodzyński <jacob.wod@gmail.com>
2024-03-14 10:52:52 +0100

    Updated the reinstall modules script to work with the new dir structure.

--- scripts/reinstall_modules.sh
+++ scripts/reinstall_modules.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-# README
-# Super quick and dirty, 30 secs script. Does what it says. 
-# I got tired of typing it all over again while switching 
-# branches with differences in package.json, so here it is.
+# README 
+# Quick and easy script that removes and reinstalls the contents
+# of node_modules dirs inside all NodeJS apps in this repo.
+
+ORIGINAL_DIR=$(pwd)
+
+# Get the directory of the script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Grab the relevant directories, relative to the script
+CLIENT_DIR="$SCRIPT_DIR/../apps/client"
+ADMIN_DIR="$SCRIPT_DIR/../apps/admin"
+BACKEND_DIR="$SCRIPT_DIR/../apps/backend"
 
 echo "Backend…"
-cd new-backend
+cd $BACKEND_DIR
 rm -rf node_modules
 npm i
 npm audit fix
 
 echo "Admin…"
-cd ../new-admin
+cd $ADMIN_DIR
 rm -rf node_modules
 npm i
 npm audit fix
 
 echo "Client…"
-cd ../new-client
+cd $CLIENT_DIR
 rm -rf node_modules
 npm i
 npm audit fix
 
-cd ..
+cd $ORIGINAL_DIR
 
 echo "DONE!"
