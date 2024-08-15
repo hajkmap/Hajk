2a7111b8dca8dc0f443615187478323e25c021e5
Jacob Wodzyński <jacob.wod@gmail.com>
2024-08-15 08:29:01 +0200

    Minor change to print out correct error messages. Turns out mulitple errors can get wrapped into a AggregateError sometimes.

--- apps/backend/server/apis/v2/services/postgres.service.js
+++ apps/backend/server/apis/v2/services/postgres.service.js
@@ -1,5 +1,5 @@
 import log4js from "log4js";
-import pg from "pg"; // TODO: Another PG client, perhaps postgres.js?
+import pg from "pg";
 
 const logger = log4js.getLogger("service.postgres.v2");
 
@@ -24,12 +24,14 @@ class PostgresService {
 
   checkConnection() {
     this.pool.query("SELECT 1", (err) => {
-      if (err)
+      if (err) {
         logger.error(
           "Could not connect to Postgres database due to error:\n",
-          err.message
+          err instanceof AggregateError
+            ? err.errors.map((e) => e.message).join("\n")
+            : err.message
         );
-      else logger.info("Successfully connected to Postgres database");
+      } else logger.info("Successfully connected to Postgres database");
     });
   }
 }
