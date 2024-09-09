20e2744f4240829d2f0848d697be55185a5e2c57
Jacob Wodzyński <jacob.wod@gmail.com>
2024-09-09 08:37:49 +0200

    Removed the dotenv package. Since we bump the sys req on NodeJS, we can as well use the built-in --env-file flag that became available in Node 20.

--- apps/backend/server/index.js
+++ apps/backend/server/index.js
@@ -1,7 +1,6 @@
 // env.js does not have any real export, so there's nothing to do
 // with the imported variable. Let's disable the warning for this line:
 // eslint-disable-next-line no-unused-vars
-import * as dotenv from "./common/env.js";
 import Server from "./common/server.js";
 import routes from "./routes.js";
 
