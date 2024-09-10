db9793b6c63789fe86a436494777b557705b8529
Jacob Wodzyński <jacob.wod@gmail.com>
2024-09-10 15:15:44 +0200

    Removed the experimentalt v3 dir created for test purposes.

--- apps/backend/server/apis/v3.tests/controllers/config/router.ts
+++
@@ -1,4 +0,0 @@
-import * as express from "express";
-import controller from "./controller.ts";
-
-export default express.Router().get("/:map", controller.byMap);
--- apps/backend/server/apis/v3.tests/router.ts
+++
@@ -1,5 +0,0 @@
-import * as express from "express";
-
-import configRouter from "./controllers/config/router.ts";
-
-export default express.Router().use("/config", configRouter);
