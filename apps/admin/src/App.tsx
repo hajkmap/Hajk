14d240dd43505717f80b8aae0e75932164f7d3e7
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2024-09-23 14:13:41 +0200

    Remove starter boilerplate

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useTranslation } from "react-i18next";
+
 import "./App.css";
 import "./i18n";
-import { useTranslation } from "react-i18next";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,23 +11,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>{t("common.hello")}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
