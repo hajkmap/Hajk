8b3b61f8974c8f424f12a4710d5d207a143e496d
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2024-09-23 14:37:33 +0200

    Cleanup

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Typography } from "@mui/material";
 
 import "./App.css";
 import "./i18n";
-import { Button } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
   const { t } = useTranslation();
 
   return (
     <>
-      <div>
-        <Button
-          onClick={() => setCount((count) => count + 1)}
-          color="primary"
-          variant="contained"
-        >
-          count is {count}
-        </Button>
-        <Button
-          onClick={() => setCount((count) => count + 1)}
-          color="secondary"
-          variant="contained"
-        >
-          count is {count}
-        </Button>
-        <p>{t("common.hello")}</p>
-      </div>
+      <Typography variant="h2">{t("common.hello")}</Typography>
     </>
   );
 }
