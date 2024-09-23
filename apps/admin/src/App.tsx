443ef2b3bf695d9c156074e07c65126bd5273dab
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2024-09-23 14:01:18 +0200

    Added deps and translation files

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./i18n";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { t } = useTranslation();
 
   return (
     <>
@@ -21,15 +24,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>{t("common.hello")}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ apps/admin/src/i18n.ts
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import sv from "./i18n/sv.json";
+import en from "./i18n/en.json";
+
+const resources = {
+  en: en,
+  sv: sv,
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "sv",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
