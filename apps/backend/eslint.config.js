b7213f6c90970bbbf4d8299c89d61bd0c330155d
Jacob Wodzyński <jacob.wod@gmail.com>
2024-09-24 12:24:35 +0200

    Added ESlint rules in Backend to enfore kebab-case file naming. Also, added V1 and V2 APIs to the ignore list of ESlint as we won't be fixing those errors.

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -2,11 +2,15 @@
 import globals from "globals";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import checkFile from "eslint-plugin-check-file";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+  {
+    plugins: { "check-file": checkFile },
+  },
   {
     languageOptions: {
       globals: globals.node,
@@ -17,8 +21,31 @@ export default tseslint.config(
       "node_modules/",
       "dist/",
       "public/api-explorer/",
+      "server/apis/v1",
+      "server/apis/v2",
       "*.yaml",
       "*.yml",
     ],
+  },
+  {
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          // all folders within src (except __tests__)should be named in kebab-case
+          "src/**/!(__tests__)": "KEBAB_CASE",
+        },
+      ],
+    },
   }
 );
