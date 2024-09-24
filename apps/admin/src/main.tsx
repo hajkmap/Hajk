c58f4b72f4b48fe3a0c103cb393b0a41be686dde
Jacob Wodzyński <jacob.wod@gmail.com>
2024-09-24 12:30:11 +0200

    Renamed files/imports as needed to follow the new file name rule.

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
+import App from "./app.tsx";
 import "./style/index.css";
-import ThemeRegistry from "./style/themeRegistry.tsx";
+import ThemeRegistry from "./style/theme-registry.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeRegistry>
