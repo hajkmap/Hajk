45a0908a45df903b8d3907a7c3cc228f28845cec
Jacob Wodzyński <jacob.wod@gmail.com>
2024-09-24 12:28:11 +0200

    Added a rule to ESlint that enforces kebab-case in file names in Admin

--- apps/admin/eslint.config.js
+++ apps/admin/eslint.config.js
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import checkFile from "eslint-plugin-check-file";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -28,6 +29,7 @@ export default tseslint.config(
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "check-file": checkFile,
     },
     rules: {
       ...react.configs.recommended.rules,
@@ -37,6 +39,23 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          // all folders within src (except __tests__)should be named in kebab-case
+          "src/**/!(__tests__)": "KEBAB_CASE",
+        },
+      ],
     },
-  },
+  }
 );
