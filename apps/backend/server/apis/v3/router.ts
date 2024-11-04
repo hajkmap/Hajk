c86978c72be55cc4da961f2c0f185a6599a61304
Jacob Wodzyński <jacob.wod@gmail.com>
2024-11-04 15:47:46 +0100

    Super-rough implementation of Passport and its 'local' strategy: - GET /user will return 403, unless user is authenticated - To authenticate, send a POST to /login, providing a JSON object with user credentials (see hard-coded user credentials in server.ts). - Logged in user is saved in Express' session, shared between backend instances using Postgres (via Prisma).

--- apps/backend/server/apis/v3/router.ts
+++ apps/backend/server/apis/v3/router.ts
@@ -7,10 +7,22 @@ import searchRouter from "./controllers/search/router.ts";
 import servicesRouter from "./controllers/services/router.ts";
 import toolsRouter from "./controllers/tools/router.ts";
 
+import passport from "passport";
+import { isAuthenticated } from "../../common/middlewares/auth.middleware.ts";
+
 export default Router()
   .use("/groups", groupsRouter)
   .use("/layers", layersRouter)
   .use("/maps", mapsRouter)
   .use("/search", searchRouter)
   .use("/services", servicesRouter)
-  .use("/tools", toolsRouter);
+  .use("/tools", toolsRouter)
+  .use("/login", passport.authenticate("local"), (req, res) => {
+    res.json({
+      status: "success",
+      message: "You are successfully logged in.",
+    });
+  })
+  .use("/user", isAuthenticated, (req, res) => {
+    res.send({ user: req.user });
+  });
---
+++ apps/backend/server/common/middlewares/auth.middleware.ts
@@ -0,0 +1,14 @@
+import type { NextFunction, Request, Response } from "express";
+import HttpStatusCodes from "../http-status-codes.ts";
+
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response | void {
+  if (req.user) {
+    return next();
+  } else {
+    res.status(HttpStatusCodes.UNAUTHORIZED).end();
+  }
+}
