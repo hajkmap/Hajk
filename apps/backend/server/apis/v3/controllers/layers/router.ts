921d512a8c0f532d677bd9051acf50c5a94c7f3f
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2024-11-06 09:51:19 +0100

    Added authentication check on /layers

--- apps/backend/server/apis/v3/controllers/layers/router.ts
+++ apps/backend/server/apis/v3/controllers/layers/router.ts
@@ -1,9 +1,10 @@
 import * as express from "express";
 import controller from "./controller.ts";
+import { isAuthenticated } from "../../../../common/auth/is-authenticated.middleware.ts";
 
 export default express
   .Router()
-  .get("/", controller.getLayers)
+  .get("/", isAuthenticated, controller.getLayers)
   .get("/types", controller.getLayerTypes)
   .get("/types/:type", controller.getLayersByType)
   .get("/:id", controller.getLayerById);
