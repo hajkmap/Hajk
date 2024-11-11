0f8b5d8ddf0b18b51b2f921459497b7f44007147
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2024-11-11 14:58:02 +0100

    Added roles to UI

--- apps/admin/src/pages/user-roles/index.tsx
+++ apps/admin/src/pages/user-roles/index.tsx
@@ -1,12 +1,30 @@
+import Grid from "@mui/material/Grid2";
+import { List, ListItem, Paper, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import Page from "../../layouts/root/components/page";
+import { useRoles } from "../../api/users";
 
 export default function UserRolesPage() {
   const { t } = useTranslation();
+  const { data: roles, isLoading: rolesLoading } = useRoles();
 
   return (
     <Page title={t("common.userRoles")}>
-      <p>{t("common.userRoles")}</p>
+      {rolesLoading ? (
+        <div>Roles loading...</div>
+      ) : (
+        <Grid size={12}>
+          <List>
+            {roles?.map((role) => (
+              <ListItem key={role.id}>
+                <Paper sx={{ width: "100%", p: 2 }} elevation={4}>
+                  <Typography>{`${role.title}`}</Typography>
+                </Paper>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+      )}
     </Page>
   );
 }
--- apps/admin/src/pages/users/index.tsx
+++ apps/admin/src/pages/users/index.tsx
@@ -3,14 +3,12 @@ import { List, ListItem, Paper, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 import Page from "../../layouts/root/components/page";
-import { useRoles, useUsers } from "../../api/users/hooks";
+import { useUsers } from "../../api/users/hooks";
 import useUserStore from "../../store/use-user-store";
 
 export default function UsersPage() {
   const { t } = useTranslation();
   const { data: users, isLoading: usersLoading } = useUsers();
-
-  const { data: roles, isLoading: rolesLoading } = useRoles();
 
   const activeUser = useUserStore((state) => state.user);
 
@@ -27,22 +25,6 @@ export default function UsersPage() {
                   <Typography>{`${user.fullName}, ${user.email}${
                     user.email === activeUser?.email ? " (du)" : ""
                   }`}</Typography>
-                </Paper>
-              </ListItem>
-            ))}
-          </List>
-        </Grid>
-      )}
-
-      {rolesLoading ? (
-        <div>Roles loading...</div>
-      ) : (
-        <Grid size={12}>
-          <List>
-            {roles?.map((role) => (
-              <ListItem key={role.id}>
-                <Paper sx={{ width: "100%", p: 2 }} elevation={4}>
-                  <Typography>{`${role.title}`}</Typography>
                 </Paper>
               </ListItem>
             ))}
