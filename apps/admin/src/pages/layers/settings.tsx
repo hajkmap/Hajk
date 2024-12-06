7d7df47ef51703b64db86d11eb62b229f4f7342f
Albinahmetaj <albingithub@gmail.com>
2024-12-06 14:05:42 +0100

    update: layersettings form

--- apps/admin/src/pages/layers/settings.tsx
+++ apps/admin/src/pages/layers/settings.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Page from "../../layouts/root/components/page";
 import { useTranslation } from "react-i18next";
-import { Box } from "@mui/material";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import DynamicFormContainer from "../../components/form-factory/dynamic-form-container";
 import { FieldValues } from "react-hook-form";
 import CONTAINER_TYPE from "../../components/form-factory/types/container-types";
@@ -9,6 +9,8 @@ import INPUT_TYPE from "../../components/form-factory/types/input-type";
 import FormRenderer from "../../components/form-factory/form-renderer";
 import { createOnSubmitHandler } from "../../components/form-factory/form-utils";
 import { DefaultUseForm } from "../../components/form-factory/default-use-form";
+import { RenderProps } from "../../components/form-factory/types/render";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 export default function LayerSettings() {
   const { t } = useTranslation();
@@ -21,6 +23,34 @@ export default function LayerSettings() {
   const panelNestedContainer = new DynamicFormContainer<FieldValues>(
     "",
     CONTAINER_TYPE.PANEL
+  );
+  const accordionNestedContainer = new DynamicFormContainer<FieldValues>(
+    t("Inställningar för request"),
+    CONTAINER_TYPE.ACCORDION
+  );
+  const accordionNestedContainer2 = new DynamicFormContainer<FieldValues>(
+    "Inställningar för lager",
+    CONTAINER_TYPE.ACCORDION
+  );
+  const accordionNestedContainer3 = new DynamicFormContainer<FieldValues>(
+    "Presentation vid resultat",
+    CONTAINER_TYPE.ACCORDION
+  );
+  const accordionNestedContainer4 = new DynamicFormContainer<FieldValues>(
+    "Infoklick",
+    CONTAINER_TYPE.ACCORDION
+  );
+  const accordionNestedContainer5 = new DynamicFormContainer<FieldValues>(
+    "Sökinställningar",
+    CONTAINER_TYPE.ACCORDION
+  );
+  const accordionNestedContainer6 = new DynamicFormContainer<FieldValues>(
+    "Infoknapp",
+    CONTAINER_TYPE.ACCORDION
+  );
+  const accordionNestedContainer7 = new DynamicFormContainer<FieldValues>(
+    "Behörighet",
+    CONTAINER_TYPE.ACCORDION
   );
 
   panelNestedContainer.addInput({
@@ -37,7 +67,6 @@ export default function LayerSettings() {
     name: "service",
     title: `Tjänst`,
     defaultValue: "Byggnader v3 WMS",
-    registerOptions: { required: `${t("common.required")}` },
     optionList: [{ title: "Byggnader v3 WMS", value: "Byggnader v3 WMS" }],
   });
 
@@ -47,7 +76,6 @@ export default function LayerSettings() {
     name: "layerService",
     title: `Lagernamn i tjänsten`,
     defaultValue: "",
-    registerOptions: { required: `${t("common.required")}` },
     optionList: [{ title: "Testlayer", value: "Testlayer" }],
   });
 
@@ -91,7 +119,369 @@ export default function LayerSettings() {
     defaultValue: "",
   });
 
+  accordionNestedContainer.addInput({
+    type: INPUT_TYPE.SELECT,
+    gridColumns: 8,
+    name: "imageFormat",
+    title: `Bildformat`,
+    defaultValue: "",
+    optionList: [{ title: "Testlayer", value: "Testlayer" }],
+  });
+
+  accordionNestedContainer.addInput({
+    type: INPUT_TYPE.SELECT,
+    gridColumns: 8,
+    name: "coordinateSystem",
+    title: `Koordinatsystem`,
+    defaultValue: "",
+    optionList: [{ title: "Testlayer", value: "Testlayer" }],
+  });
+
+  accordionNestedContainer.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    name: "dpi",
+    title: "Efterfråga hög DPI",
+    defaultValue: false,
+  });
+
+  accordionNestedContainer.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    name: "singleTile",
+    title: "Single tile",
+    defaultValue: false,
+  });
+
+  accordionNestedContainer.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "customRatio",
+    title: `Custom ratio`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    name: "gwc",
+    title: "GeoWebCache",
+    defaultValue: false,
+  });
+
+  accordionNestedContainer2.addInput({
+    type: INPUT_TYPE.SELECT,
+    gridColumns: 8,
+    name: "style",
+    title: `Stil`,
+    defaultValue: "",
+    optionList: [{ title: "<default>", value: "default" }],
+  });
+
+  accordionNestedContainer2.addCustomInput({
+    type: INPUT_TYPE.CUSTOM,
+    kind: "CustomInputSettings",
+    name: "teckenForklaring",
+    title: "Teckenförklaring",
+    gridColumns: 8,
+    defaultValue: "",
+
+    renderer: (props: RenderProps<FieldValues>) => {
+      const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+          props.field?.onChange(file.name);
+        }
+      };
+
+      return (
+        <Stack spacing={1} direction="row" alignItems="center">
+          <TextField
+            fullWidth
+            variant="filled"
+            label={props.title}
+            value={(props.field?.value as string) || ""}
+            {...props.field}
+            error={!!props.errorMessage}
+            helperText={props.errorMessage}
+          />
+
+          <input
+            accept="*"
+            type="file"
+            id="teckenForklaring-file-upload"
+            style={{ display: "none" }}
+            onChange={handleFileUpload}
+          />
+
+          <label htmlFor="teckenForklaring-file-upload">
+            <Button
+              sx={{ minWidth: "120px" }}
+              variant="contained"
+              component="span"
+              startIcon={<CloudUploadIcon />}
+            >
+              Välj fil
+            </Button>
+          </label>
+        </Stack>
+      );
+    },
+  });
+  accordionNestedContainer2.addCustomInput({
+    type: INPUT_TYPE.CUSTOM,
+    kind: "CustomInputSettings",
+    name: "teckenForklaringIkon",
+    title: "Teckenförklaringsikon",
+    gridColumns: 8,
+    defaultValue: "",
+
+    renderer: (props: RenderProps<FieldValues>) => {
+      const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+          props.field?.onChange(file.name);
+        }
+      };
+
+      return (
+        <Stack spacing={1} direction="row" alignItems="center">
+          <TextField
+            fullWidth
+            variant="filled"
+            label={props.title}
+            value={(props.field?.value as string) || ""}
+            {...props.field}
+            error={!!props.errorMessage}
+            helperText={props.errorMessage}
+          />
+
+          <input
+            accept="*"
+            type="file"
+            id="teckenForklaringIkon-file-upload"
+            style={{ display: "none" }}
+            onChange={handleFileUpload}
+          />
+
+          <label htmlFor="teckenForklaringIkon-file-upload">
+            <Button
+              sx={{ minWidth: "120px" }}
+              variant="contained"
+              component="span"
+              startIcon={<CloudUploadIcon />}
+            >
+              Välj fil
+            </Button>
+          </label>
+        </Stack>
+      );
+    },
+  });
+
+  accordionNestedContainer2.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    name: "attributes",
+    title: "Visa kanpp för attributtabell",
+    defaultValue: false,
+  });
+
+  accordionNestedContainer2.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "opacity",
+    title: `Opacitet`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer2.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "minZoom",
+    title: `Minsta zoom`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer2.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "maxZoom",
+    title: `Maximal zoom`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer2.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    gridColumns: 8,
+    name: "warningZoom",
+    title: `Visa endast varningruta för min/max vid klick`,
+    defaultValue: false,
+  });
+
+  accordionNestedContainer3.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "displayField",
+    title: `Visningsfält`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer3.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "secondaryDisplayField",
+    title: `Sekundära visnigsfält`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer3.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "shortDisplayField",
+    title: `Kort visningsfält`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer4.addCustomInput({
+    type: INPUT_TYPE.CUSTOM,
+    kind: "CustomInputSettings",
+    name: "infoClickPreview",
+    title: "Förhandsvisa infoklick",
+    gridColumns: 4,
+    defaultValue: "",
+
+    renderer: () => {
+      return (
+        <Button sx={{ minWidth: "120px" }} variant="contained" component="span">
+          Förhandsvisa infoklick
+        </Button>
+      );
+    },
+  });
+
+  accordionNestedContainer4.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    gridColumns: 10,
+    name: "infoClick",
+    title: `Infoklick`,
+    defaultValue: false,
+  });
+
+  accordionNestedContainer4.addInput({
+    type: INPUT_TYPE.TEXTAREA,
+    gridColumns: 8,
+    name: "infoBox",
+    title: `Inforuta`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer4.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "infoClickIcon",
+    title: `Infoklick ikon (lista)`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer4.addInput({
+    type: INPUT_TYPE.SELECT,
+    gridColumns: 8,
+    name: "infoClickFormat",
+    title: `Infoclick-format`,
+    defaultValue: "",
+    optionList: [{ title: "Testformat", value: "Testformat" }],
+  });
+  accordionNestedContainer4.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "infoClickSortByAttribute",
+    title: `Infoklick sortera på attribut`,
+    defaultValue: "",
+  });
+
+  accordionNestedContainer4.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    gridColumns: 10,
+    name: "infoClickSortByDesc",
+    title: `Infoklick sortera fallande`,
+    defaultValue: false,
+  });
+  accordionNestedContainer4.addInput({
+    type: INPUT_TYPE.SELECT,
+    gridColumns: 8,
+    name: "infoClickSortType",
+    title: `Infoklick soteringstyp`,
+    defaultValue: "",
+    optionList: [{ title: "String", value: "String" }],
+  });
+
+  accordionNestedContainer5.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    gridColumns: 10,
+    name: "activateSearch",
+    title: `Aktivera sökning`,
+    defaultValue: false,
+  });
+  accordionNestedContainer5.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "url",
+    title: `Url`,
+    defaultValue: "",
+  });
+  accordionNestedContainer5.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "searchField",
+    title: `Sökfält`,
+    defaultValue: "",
+  });
+  accordionNestedContainer5.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "outDataFormat",
+    title: `Utdataformat`,
+    defaultValue: "",
+  });
+  accordionNestedContainer5.addInput({
+    type: INPUT_TYPE.TEXTFIELD,
+    gridColumns: 8,
+    name: "geometryField",
+    title: `Geometrifält`,
+    defaultValue: "",
+  });
+  accordionNestedContainer6.addInput({
+    type: INPUT_TYPE.CHECKBOX,
+    gridColumns: 10,
+    name: "infoButtonActiveLayer",
+    title: `Infoknapp synlig för lager`,
+    defaultValue: false,
+  });
+  accordionNestedContainer6.addInput({
+    type: INPUT_TYPE.TEXTAREA,
+    gridColumns: 8,
+    name: "layerDescription",
+    title: `Visningsbeskrivning av lager`,
+    defaultValue: "",
+  });
+  accordionNestedContainer7.addInput({
+    type: INPUT_TYPE.SELECT,
+    gridColumns: 8,
+    name: "competence",
+    title: `Behörighet`,
+    defaultValue: "",
+    optionList: [
+      { title: "BYGGLOV_PERSONAL_ADM", value: "BYGGLOV_PERSONAL_ADM" },
+      { title: "IT_PERSONAL_ADM", value: "IT_PERSONAL_ADM" },
+      { title: "String", value: "String" },
+    ],
+  });
+
   layerSettingsFormContainer.addContainer(panelNestedContainer);
+  layerSettingsFormContainer.addContainer(accordionNestedContainer);
+  layerSettingsFormContainer.addContainer(accordionNestedContainer2);
+  layerSettingsFormContainer.addContainer(accordionNestedContainer3);
+  layerSettingsFormContainer.addContainer(accordionNestedContainer4);
+  layerSettingsFormContainer.addContainer(accordionNestedContainer5);
+  layerSettingsFormContainer.addContainer(accordionNestedContainer6);
+  layerSettingsFormContainer.addContainer(accordionNestedContainer7);
 
   const defaultValues = formLayerData.getDefaultValues();
   const {
