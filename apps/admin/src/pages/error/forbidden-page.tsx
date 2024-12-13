55efa648bc7fbd4887a738ab0e7226b2f5b6c62b
Albinahmetaj <albingithub@gmail.com>
2024-12-13 13:17:51 +0100

    feature: error pages, react-helmet and framer-motion library
    It's probably not worth having a 3MB library just for some animations, but I thought it was pretty cool to use.
    
    We can of course remove it if requested

---
+++ apps/admin/src/pages/error/forbidden-page.tsx
@@ -0,0 +1,89 @@
+import { Helmet } from "react-helmet";
+import { LazyMotion, m, domAnimation } from "framer-motion";
+import { Box, useTheme } from "@mui/material";
+import DoNotTouchIcon from "@mui/icons-material/DoNotTouch";
+import { useTranslation } from "react-i18next";
+
+export default function Forbidden() {
+  const { palette } = useTheme();
+  const { t } = useTranslation();
+
+  const imgVariants = {
+    initial: { opacity: 0, scale: 0.8 },
+    animate: {
+      opacity: 1,
+      scale: 1,
+      rotate: [0, 0, 0, 0],
+      transition: {
+        duration: 1,
+        ease: "easeIn",
+      },
+    },
+  };
+
+  const textVariants = {
+    initial: { opacity: 0, y: 50 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, ease: "easeOut" },
+    },
+  };
+
+  return (
+    <LazyMotion features={domAnimation}>
+      <Box component="div">
+        <Helmet>
+          <title>403 - Forbidden | Hajk</title>
+        </Helmet>
+
+        <m.div
+          variants={imgVariants}
+          initial="initial"
+          animate="animate"
+          style={{
+            width: "300px",
+            height: "150px",
+            marginBottom: "20px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <Box
+            component="img"
+            src="/forbidden-logo.svg"
+            alt="Forbidden Icon"
+            style={{
+              width: "100%",
+              height: "100%",
+              filter: `invert(${palette.mode === "dark" ? 0 : 1})`,
+            }}
+          />
+        </m.div>
+
+        <Box>
+          <m.h1
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+            style={{ fontSize: "2rem", margin: "0 0 10px" }}
+          >
+            <Box>
+              <DoNotTouchIcon sx={{ fontSize: "4rem" }} />
+            </Box>
+            {t("error.forbidden")}
+          </m.h1>
+
+          <m.p
+            variants={textVariants}
+            initial="initial"
+            animate="animate"
+            style={{ color: palette.text.secondary, marginBottom: "20px" }}
+          >
+            {t("error.forbidden.description")}
+          </m.p>
+        </Box>
+      </Box>
+    </LazyMotion>
+  );
+}
--- apps/admin/src/pages/error/index.tsx
+++ apps/admin/src/pages/error/index.tsx
@@ -1,23 +1,34 @@
 import { useRouteError, isRouteErrorResponse } from "react-router";
 import Page from "../../layouts/root/components/page";
+import { Box } from "@mui/material";
+import Forbidden from "./forbidden-page";
+import NotFound from "./not-found-page";
+import InternalServerPage from "./internal-server-page";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
-
-  let errorMessage = "";
-  if (isRouteErrorResponse(error)) {
-    errorMessage = error.statusText;
-  } else if (error instanceof Error) {
-    errorMessage = error.message;
-  }
 
   return (
-    <Page title="Oops!">
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{errorMessage}</i>
-      </p>
+    <Page title="">
+      <Box
+        sx={{
+          textAlign: "center",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+          py: { xs: 5, md: 12 },
+        }}
+      >
+        {isRouteErrorResponse(error) ? (
+          error.status === 403 ? (
+            <Forbidden />
+          ) : error.status === 404 ? (
+            <NotFound />
+          ) : error.status === 500 ? (
+            <InternalServerPage />
+          ) : null
+        ) : null}
+      </Box>
     </Page>
   );
 }
