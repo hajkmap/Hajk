bf08ffca16f050edf28612316c82ed6dbee41118
Albinahmetaj <albingithub@gmail.com>
2024-12-17 13:28:07 +0100

    added: actionbutton, data-grid

--- apps/admin/src/pages/layers/index.tsx
+++ apps/admin/src/pages/layers/index.tsx
@@ -1,37 +1,90 @@
 import Grid from "@mui/material/Grid2";
-import { List, ListItem, Paper, Typography } from "@mui/material";
+import { Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import { useTranslation } from "react-i18next";
 import Page from "../../layouts/root/components/page";
-import { useLayers } from "../../api/layers";
+import { Layer, useLayers } from "../../api/layers";
 import { useNavigate } from "react-router";
+import { GRID_SWEDISH_LOCALE_TEXT } from "../../i18n/translations/datagrid/sv";
+import useAppStateStore from "../../store/use-app-state-store";
 
 export default function LayersPage() {
   const { t } = useTranslation();
   const { data: layers, isLoading } = useLayers();
   const navigate = useNavigate();
+  const language = useAppStateStore((state) => state.language);
+
+  console.log("layers", layers);
 
   return (
-    <Page title={t("common.layers")}>
+    <>
       {isLoading ? (
         <div>{t("common.loading")}</div>
       ) : (
-        <Grid size={12}>
-          <p>layers.length = {layers && ` (${layers.length})`}</p>
-
-          <List>
-            {layers?.map((layer) => (
-              <ListItem
-                key={layer.id}
-                onClick={() => void navigate(`/layers/${layer.id}`)}
-              >
-                <Paper sx={{ width: "100%", p: 2 }} elevation={4}>
-                  <Typography>{layer.options.caption}</Typography>
-                </Paper>
-              </ListItem>
-            ))}
-          </List>
-        </Grid>
+        <Page
+          title={t("common.layers")}
+          actionButtons={
+            <>
+              <Button color="primary" variant="contained">
+                {t("layers.dialog.addBtn")}
+              </Button>
+            </>
+          }
+        >
+          <Grid size={12}>
+            <DataGrid
+              onCellClick={(params) => {
+                const id: string = (params.row as Layer).id;
+                if (id) {
+                  void navigate(`/layers/${id}`);
+                }
+              }}
+              sx={{
+                maxWidth: "100%",
+                mt: 8,
+                "& .MuiDataGrid-row:hover": {
+                  cursor: "pointer",
+                },
+              }}
+              rows={layers ?? []}
+              columns={[
+                {
+                  field: "id",
+                  flex: 0.3,
+                  headerName: "ID",
+                },
+                { field: "locked", flex: 0.3, headerName: "Locked" },
+                {
+                  field: "name",
+                  flex: 1,
+                  headerName: t("common.name"),
+                  renderCell: (params) => (params?.value as string) || "N/A",
+                },
+              ]}
+              slotProps={{
+                loadingOverlay: {
+                  variant: "skeleton",
+                  noRowsVariant: "skeleton",
+                },
+              }}
+              localeText={
+                language === "sv" ? GRID_SWEDISH_LOCALE_TEXT : undefined
+              }
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 10,
+                  },
+                },
+              }}
+              getRowId={(row) => row.id}
+              hideFooterPagination={layers && layers.length < 10}
+              pageSizeOptions={[10, 25, 50, 100]}
+              disableRowSelectionOnClick
+            />
+          </Grid>
+        </Page>
       )}
-    </Page>
+    </>
   );
 }
