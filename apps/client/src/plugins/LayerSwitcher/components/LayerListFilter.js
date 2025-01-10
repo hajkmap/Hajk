d98658d9b1083aa9765144e09057f86dab403bab
Jakob Larsson <jakob.larsson@devies.se>
2025-01-10 10:44:49 +0100

    feat: use tooltip instead of helpertext for filter

--- apps/client/src/plugins/LayerSwitcher/components/LayerListFilter.js
+++ apps/client/src/plugins/LayerSwitcher/components/LayerListFilter.js
@@ -5,16 +5,21 @@ import { Box, IconButton, InputAdornment, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import ClearIcon from "@mui/icons-material/Clear";
 
+import HajkToolTip from "components/HajkToolTip";
+
+const MIN_FILTER_TOOLTIP_DELAY = 1000;
+
 const LayerListFilter = ({ handleFilterValueChange, handleFilterSubmit }) => {
   const inputRef = useRef(null);
 
   const DEFAULT_MIN_FILTER_LENGTH = 3;
-  const [helperText, setHelperTextState] = useState(null);
-  const setHelperText = (value) => {
+  const [showToolTip, setShowToolTip] = useState(false);
+
+  const updateTooltip = (value) => {
     if (value?.length > 0 && value?.length < DEFAULT_MIN_FILTER_LENGTH) {
-      setHelperTextState("Skriv minst 3 tecken eller tryck enter");
+      setShowToolTip(true);
     } else {
-      setHelperTextState(null);
+      setShowToolTip(false);
     }
   };
 
@@ -28,57 +33,63 @@ const LayerListFilter = ({ handleFilterValueChange, handleFilterSubmit }) => {
           `${theme.spacing(0.2)} solid ${theme.palette.divider}`,
       }}
     >
-      <TextField
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-          endAdornment: (
-            <InputAdornment position="end">
-              {inputRef.current?.value && (
-                <IconButton
-                  onClick={() => {
-                    if (inputRef.current) {
-                      inputRef.current.value = "";
-                      handleFilterValueChange("");
-                      setHelperText("");
-                    }
-                  }}
-                  size="small"
-                >
-                  <ClearIcon />
-                </IconButton>
-              )}
-            </InputAdornment>
-          ),
-        }}
-        size="small"
-        onChange={(event) => {
-          handleFilterValueChange(event.target.value);
-          setHelperText(event.target.value);
-        }}
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            handleFilterSubmit(event.target.value);
-          }
-        }}
-        fullWidth
-        placeholder="Sök lager och grupper"
-        helperText={helperText}
-        FormHelperTextProps={{
-          color: "red",
-        }}
-        inputRef={inputRef}
-        variant="outlined"
-        sx={{
-          background: (theme) =>
-            theme.palette.mode === "dark" ? "inherit" : "#fff",
-          width: 500,
-          maxWidth: "100%",
-        }}
-      />
+      <HajkToolTip
+        title="Skriv minst 3 tecken eller tryck enter"
+        placement="right"
+        enterDelay={MIN_FILTER_TOOLTIP_DELAY}
+        open={showToolTip}
+      >
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="end">
+                {inputRef.current?.value && (
+                  <IconButton
+                    onClick={() => {
+                      if (inputRef.current) {
+                        inputRef.current.value = "";
+                        handleFilterValueChange("");
+                        updateTooltip("");
+                      }
+                    }}
+                    size="small"
+                  >
+                    <ClearIcon />
+                  </IconButton>
+                )}
+              </InputAdornment>
+            ),
+          }}
+          size="small"
+          onChange={(event) => {
+            handleFilterValueChange(event.target.value);
+            updateTooltip(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleFilterSubmit(event.target.value);
+            }
+          }}
+          fullWidth
+          placeholder="Sök lager och grupper"
+          FormHelperTextProps={{
+            color: "red",
+          }}
+          inputRef={inputRef}
+          variant="outlined"
+          sx={{
+            background: (theme) =>
+              theme.palette.mode === "dark" ? "inherit" : "#fff",
+            width: 500,
+            maxWidth: "100%",
+          }}
+        />
+      </HajkToolTip>
     </Box>
   );
 };
