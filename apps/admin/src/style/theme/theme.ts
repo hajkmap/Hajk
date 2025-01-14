ff29d6a8b6ab62398510f898710df52ee1d4ef0b
jesade-vbg <jesade.vbg@gmail.com>
2025-01-14 15:09:02 +0100

    Force default size to small of some Mui components.

--- apps/admin/src/style/theme/theme.ts
+++ apps/admin/src/style/theme/theme.ts
@@ -1,10 +1,49 @@
 import { PaletteMode } from "@mui/material";
 import { Theme, createTheme } from "@mui/material/styles";
 
+const defaultSizeForComponents = "small";
+
+// List of components that should have a default size set
+const defaultSizedComponents = [
+  "MuiButton",
+  "MuiTextField",
+  "MuiSelect",
+  "MuiCheckbox",
+  "MuiRadio",
+  "MuiFormControlLabel",
+  "MuiIconButton",
+  "MuiInput",
+  "MuiInputBase",
+  "MuiInputLabel",
+  "MuiOutlinedInput",
+  "MuiFilledInput",
+  "MuiSwitch",
+  "MuiSlider",
+  "MuiAlert",
+  "MuiToggleButton",
+];
+
+const defaultSizeComponentObject: Record<
+  string,
+  { defaultProps: { size: string } }
+> = {};
+
+// Create a default size for each component
+defaultSizedComponents.forEach((component) => {
+  defaultSizeComponentObject[component] = {
+    defaultProps: {
+      size: defaultSizeForComponents,
+    },
+  };
+});
+
 export function getTheme(mode: PaletteMode): Theme {
   return createTheme({
     palette: {
       mode: mode,
+    },
+    components: {
+      ...defaultSizeComponentObject,
     },
   });
 }
