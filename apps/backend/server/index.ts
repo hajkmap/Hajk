c5d3096d645fd62e5c26b6a5addf527592970707
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2025-01-23 08:37:07 +0100

    Added catch for uncaughtExceptions to avoid restart loop in dev mode

--- apps/backend/server/index.ts
+++ apps/backend/server/index.ts
@@ -1,3 +1,11 @@
 import Server from "./common/server.ts";
 
+// We're gonna need to handle uncaught exceptions so that we avoid getting stuck in a restart loop
+if (process.env.NODE_ENV !== "production") {
+  process.on("uncaughtException", (error) => {
+    console.error("Uncaught Exception:", error.message, "\n");
+    console.error("Stack Trace:", error.stack);
+  });
+}
+
 Server.listen();
