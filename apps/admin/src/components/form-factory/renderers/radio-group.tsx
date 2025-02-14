47bf9def6e7ce463015bbcb8d4726d814ec5ed00
jesade-vbg <jesade.vbg@gmail.com>
2025-02-14 14:34:47 +0100

    Fix so RadioNuttonGroups can have a title.

--- apps/admin/src/components/form-factory/renderers/radio-group.tsx
+++ apps/admin/src/components/form-factory/renderers/radio-group.tsx
@@ -1,4 +1,4 @@
-import { RadioGroup, FormControlLabel, Radio } from "@mui/material";
+import { RadioGroup, FormControlLabel, Radio, Typography } from "@mui/material";
 import { RenderFunction } from "../types/render";
 import { FieldValues } from "react-hook-form";
 
@@ -6,23 +6,27 @@ const renderRadioGroup: RenderFunction<FieldValues> = ({
   field,
   inputProps,
   optionList,
+  title,
 }) => {
   return (
-    <RadioGroup
-      {...field}
-      {...inputProps}
-      value={(field?.value as string) ?? ""}
-      onChange={(_, value) => field?.onChange(value)}
-    >
-      {optionList?.map((option, index) => (
-        <FormControlLabel
-          key={index}
-          value={String(option.value)}
-          control={<Radio />}
-          label={option.title}
-        />
-      ))}
-    </RadioGroup>
+    <div>
+      <Typography variant="body1">{title}</Typography>
+      <RadioGroup
+        {...field}
+        {...inputProps}
+        value={(field?.value as string) ?? ""}
+        onChange={(_, value) => field?.onChange(value)}
+      >
+        {optionList?.map((option, index) => (
+          <FormControlLabel
+            key={index}
+            value={String(option.value)}
+            control={<Radio />}
+            label={option.title}
+          />
+        ))}
+      </RadioGroup>
+    </div>
   );
 };
 
