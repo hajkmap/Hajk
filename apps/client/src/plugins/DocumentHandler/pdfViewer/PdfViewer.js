4a50456f821aed799b4cc184b62caf6fdff2df3c
sunix71 <tomasmarklund@live.se>
2025-03-25 14:17:42 +0100

    pdfjs workerSrc CDN

--- apps/client/src/plugins/DocumentHandler/pdfViewer/PdfViewer.js
+++ apps/client/src/plugins/DocumentHandler/pdfViewer/PdfViewer.js
@@ -7,8 +7,7 @@ import { styled } from "@mui/material/styles";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc =
-  process.env.PUBLIC_URL + "/pdf.worker.min.mjs";
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PdfContainer = styled("div")(() => ({
   maxHeight: "100%",
