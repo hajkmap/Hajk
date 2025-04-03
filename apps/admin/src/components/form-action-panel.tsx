4c19a63a5f61123dfbccdd918823f78b1e3665e1
Olof Svahn <olof.svahn@varberg.se>
2025-04-03 14:06:01 +0200

    formatted prettier

--- apps/admin/src/components/form-action-panel.tsx
+++ apps/admin/src/components/form-action-panel.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { Box, Button, styled, Typography, Paper} from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, styled, Typography, Paper } from "@mui/material";
 import CircularProgress from "../components/progress/circular-progress";
 import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
@@ -30,7 +30,8 @@ const FormActionPanel: React.FC<FormActionProps> = ({
 }) => {
   const navigate = useNavigate();
   const { t } = useTranslation();
-  const saveButtonDisabled = !dirtyFields || Object.keys(dirtyFields).length === 0;
+  const saveButtonDisabled =
+    !dirtyFields || Object.keys(dirtyFields).length === 0;
   const [open, setOpen] = useState<boolean>(false);
 
   const ActionContainer = styled(Box)(({ theme }) => ({
@@ -59,31 +60,39 @@ const FormActionPanel: React.FC<FormActionProps> = ({
 
   const getTimeAgo = (dateString?: string): string => {
     if (!dateString) return "Unknown";
-  
+
     const date = new Date(dateString);
     const now = new Date();
-  
+
     const startOfDay = new Date(now);
     startOfDay.setHours(0, 0, 0, 0);
-  
+
     const startOfDate = new Date(date);
     startOfDate.setHours(0, 0, 0, 0);
-  
+
     const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-  
+
     const secondsInWeek = 7 * 86400;
     const secondsInMonth = 30 * 86400;
     const secondsInYear = 365 * 86400;
-  
-    const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  
-    if (startOfDate.getTime() === startOfDay.getTime()) return `${t("formControl.time.today")} ${time}`;
-    if (startOfDate.getTime() === startOfDay.getTime() - 86400000) return `${t("formControl.time.yesterday")} ${time}`;
-  
-    if (diffInSeconds < secondsInWeek) return `${Math.floor(diffInSeconds / 86400)} ${t("formControl.time.daysAgo")} ${time}`;
-    if (diffInSeconds < secondsInMonth) return `${Math.floor(diffInSeconds / 86400 / 7)} ${t("formControl.time.weekAago")} ${time}`;
-    if (diffInSeconds < secondsInYear) return `${Math.floor(diffInSeconds / secondsInMonth)} ${t("formControl.time.monthsAgo")} ${time}`;
-    
+
+    const time = date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    if (startOfDate.getTime() === startOfDay.getTime())
+      return `${t("formControl.time.today")} ${time}`;
+    if (startOfDate.getTime() === startOfDay.getTime() - 86400000)
+      return `${t("formControl.time.yesterday")} ${time}`;
+
+    if (diffInSeconds < secondsInWeek)
+      return `${Math.floor(diffInSeconds / 86400)} ${t("formControl.time.daysAgo")} ${time}`;
+    if (diffInSeconds < secondsInMonth)
+      return `${Math.floor(diffInSeconds / 86400 / 7)} ${t("formControl.time.weekAago")} ${time}`;
+    if (diffInSeconds < secondsInYear)
+      return `${Math.floor(diffInSeconds / secondsInMonth)} ${t("formControl.time.monthsAgo")} ${time}`;
+
     return `${Math.floor(diffInSeconds / secondsInYear)} ${t("formControl.time.yearsAgo")} ${time}`;
   };
 
@@ -94,14 +103,26 @@ const FormActionPanel: React.FC<FormActionProps> = ({
         void onUpdate();
       }}
       variant="contained"
-      disabled={updateStatus === "pending" || deleteStatus === "pending" || saveButtonDisabled}
+      disabled={
+        updateStatus === "pending" ||
+        deleteStatus === "pending" ||
+        saveButtonDisabled
+      }
     >
-      {updateStatus === "pending" ? <CircularProgress color="primary" size={12} /> : t("common.dialog.saveBtn")}
+      {updateStatus === "pending" ? (
+        <CircularProgress color="primary" size={12} />
+      ) : (
+        t("common.dialog.saveBtn")
+      )}
     </Button>
   );
 
   const renderCancelButton = () => (
-    <Button onClick={() => void navigate(-1)} variant="contained" color="warning">
+    <Button
+      onClick={() => void navigate(-1)}
+      variant="contained"
+      color="warning"
+    >
       {t("common.cancel")}
     </Button>
   );
@@ -119,66 +140,72 @@ const FormActionPanel: React.FC<FormActionProps> = ({
 
   const renderDialog = () => (
     <DialogWrapper
-    open={open}
-    title={t("common.dialog.deleteConfirmation", { name })}
-    onClose={() => setOpen(false)}
-    actions={(
-      <>
-        {renderDialogCancelButton()}
-        {renderDialogDeleteButton()}
-      </>
-    )}
-    fullWidth={true}
-  >
-    <Typography variant="body2" color="text.secondary">
-      {t("common.dialog.deleteWarning")}
-    </Typography>
-  </DialogWrapper>
-  )
+      open={open}
+      title={t("common.dialog.deleteConfirmation", { name })}
+      onClose={() => setOpen(false)}
+      actions={
+        <>
+          {renderDialogCancelButton()}
+          {renderDialogDeleteButton()}
+        </>
+      }
+      fullWidth={true}
+    >
+      <Typography variant="body2" color="text.secondary">
+        {t("common.dialog.deleteWarning")}
+      </Typography>
+    </DialogWrapper>
+  );
 
   const renderDialogDeleteButton = () => (
     <Button
-    onClick={(e) => {
-      e.preventDefault();
-      setOpen(false);
-      void onDelete();
-      void navigate(-1);
-    }}
-    type="submit"
-    variant="contained"
-    color="error"
-    disabled={deleteStatus === "pending"}
-  >
-    {deleteStatus === "pending" ? (
-      <CircularProgress color="primary" size={12} />
-    ) : (
-      t("common.dialog.deleteBtn")
-    )}
-  </Button>
-  )
+      onClick={(e) => {
+        e.preventDefault();
+        setOpen(false);
+        void onDelete();
+        void navigate(-1);
+      }}
+      type="submit"
+      variant="contained"
+      color="error"
+      disabled={deleteStatus === "pending"}
+    >
+      {deleteStatus === "pending" ? (
+        <CircularProgress color="primary" size={12} />
+      ) : (
+        t("common.dialog.deleteBtn")
+      )}
+    </Button>
+  );
 
   const renderStyledPaper = () => (
     <StyledPaper>
-    <Typography variant="h5" sx={{fontSize: '1.2rem', fontWeight:"bold", mb:2}}>
-      {t("common.lastSaved", { lastSavedBy, lastSavedDate: getTimeAgo(lastSavedDate)  })}
-    </Typography>
-    <Typography variant="subtitle1" sx={{fontSize: "1rem"}}>
-      {getTimeAgo(lastSavedDate)}
-    </Typography>
-    <Typography variant="body1">
-      {t("common.of")}
-    </Typography>
-    <Typography variant="body1">
-      {lastSavedBy}
-    </Typography>
-  </StyledPaper>
-  )
+      <Typography
+        variant="h5"
+        sx={{ fontSize: "1.2rem", fontWeight: "bold", mb: 2 }}
+      >
+        {t("common.lastSaved", {
+          lastSavedBy,
+          lastSavedDate: getTimeAgo(lastSavedDate),
+        })}
+      </Typography>
+      <Typography variant="subtitle1" sx={{ fontSize: "1rem" }}>
+        {getTimeAgo(lastSavedDate)}
+      </Typography>
+      <Typography variant="body1">{t("common.of")}</Typography>
+      <Typography variant="body1">{lastSavedBy}</Typography>
+    </StyledPaper>
+  );
 
   const renderDialogCancelButton = () => (
-    <Button onClick={() => setOpen(false)} variant="contained" color="secondary">
-    {t("common.cancel")}
-  </Button>
-  )
+    <Button
+      onClick={() => setOpen(false)}
+      variant="contained"
+      color="secondary"
+    >
+      {t("common.cancel")}
+    </Button>
+  );
 
   return (
     <Box
