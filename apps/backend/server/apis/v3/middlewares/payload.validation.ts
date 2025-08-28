8b8b5e9c2fd8b0426117c2da76eb67696348dfc7
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2025-08-28 07:47:16 +0200

    Removed deprecated ZodSchema

--- apps/backend/server/apis/v3/middlewares/payload.validation.ts
+++ apps/backend/server/apis/v3/middlewares/payload.validation.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
-import { ZodSchema, ZodError } from "zod";
+import { z, ZodError } from "zod";
 import HttpStatusCodes from "../../../common/http-status-codes.ts";
 /*
 Tries to parse the request data using the provided schema.
@@ -8,7 +8,7 @@ If parsing fails, returns a 400 error response with a JSON object containing val
 errors if the error is a Zod error; otherwise, passes the error to the next middleware function (next(error)).
 */
 export const validatePayload = (
-  schema: ZodSchema<unknown>,
+  schema: z.ZodType<unknown>,
   source: "body" | "params" | "query" = "body"
 ) => {
   return (req: Request, res: Response, next: NextFunction) => {
