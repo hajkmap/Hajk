212a5936ea41c535003183aa54d6e4b0e8cb314c
Jacob Wodzyński <jacob.wod@gmail.com>
2025-09-04 13:23:42 +0200

    Fixed regression (linter error) introduced in previous commit - let's stick to the correct return types for middleware.

--- apps/backend/server/common/auth/is-admin.ts
+++ apps/backend/server/common/auth/is-admin.ts
@@ -4,17 +4,20 @@ import { getUserRoles } from "./get-user-roles.ts";
 
 export async function isAdmin(req: Request, res: Response, next: NextFunction) {
   if (!req.user) {
-    return res.status(HttpStatusCodes.UNAUTHORIZED).end();
+    res.status(HttpStatusCodes.UNAUTHORIZED).end();
+    return;
   }
 
   const userRoles = await getUserRoles(req.user);
   const userRoleCodes = userRoles.map((role) => role.code);
 
   if (userRoleCodes.includes("SUPERUSER") || userRoleCodes.includes("ADMIN")) {
-    return next();
+    next();
+    return;
   } else {
-    return res
+    res
       .status(HttpStatusCodes.FORBIDDEN)
       .json({ message: "Forbidden: Only accessible for admin users." });
+    return;
   }
 }
