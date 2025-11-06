hajkmap/Hajk

commit e1b5f59c69b8985a498efee27aa76ea11ca4afbb

Feature: it's now possible to start Hajk with OpenStreetMap background visible at start: - Added the `OSMVisibleAtStart` setting to LayerSwitcher's config - Enabled it as the default background in `simpleMapConfig.json`` - Removed obsolete WMTS from Lantmäteriet which requires a valid token (i.e. not good for default demo purposes) - Fixed an issue so we don't show OSM+another background if map is loaded with query string params

apps/client/src/models/AppModel.js
@@ -548,8 +548,20 @@ class AppModel {
   }
 
   createOSMLayer() {
+    const layerSwitcherConf = this.config.mapConfig.tools.find(
+      (tool) => tool.type === "layerswitcher"
+    );
+
+    // Respect the OSMVisibleAtStart option from layerswitcher config
+    // only if no override is provided in URLSearchParams (else we will
+    // end up with multiple background layers visible at start).
+    const visibleAtStart =
+      this.layersFromParams.length === 0 ??
+      layerSwitcherConf?.options?.OSMVisibleAtStart === true ??
+      false;
+
     const osmLayer = new TileLayer({
-      visible: false,
+      visible: visibleAtStart,
       source: new OSM({
         reprojectionErrorThreshold: 5,
       }),

commit 4040c7c11367620040efc7609a56e1b3b436cc67

Fixed logic for determining if OpenStreetMap layer is visible at start, regression introduced in e1b5f59c69b8985a498efee27aa76ea11ca4afbb

apps/client/src/models/AppModel.js
@@ -556,8 +556,8 @@ class AppModel {
     // only if no override is provided in URLSearchParams (else we will
     // end up with multiple background layers visible at start).
     const visibleAtStart =
-      this.layersFromParams.length === 0 ??
-      layerSwitcherConf?.options?.OSMVisibleAtStart === true ??
+      (this.layersFromParams.length === 0 &&
+        layerSwitcherConf?.options?.OSMVisibleAtStart === true) ??
       false;
 
     const osmLayer = new TileLayer({