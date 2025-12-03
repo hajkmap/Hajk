4aee7af2463ca83a23540404c1dd72b0c9529fb6
OlofSvahnVbg <111860819+OlofSvahnVbg@users.noreply.github.com>
2025-12-03 08:42:11 +0100

    added allow 8 chars to validator (#1741)
    * added allow 8 chars to validator
    
    * Test SSH signing
    
    * comment fix
    Fix validator rule to allow 6–8 characters
    
    ---------
    
    Co-authored-by: Olof Svahn <olof.svahn@varberg.se>

--- apps/client/src/utils/Validator.js
+++ apps/client/src/utils/Validator.js
@@ -1,10 +1,10 @@
 export const isValidLayerId = (id) => {
   return (
     // ... or it is the "fake" OpenStreetMapLayer
-    // … or a 6 characters long alphanumeric string (default in current version of NodeJS backend)
+    // … or a 6 to 8 characters long alphanumeric string (default in current version of NodeJS backend)
     !Number.isNaN(Number(id)) || // A Hajk layer is considered valid if it's ID is either a Number…
     /^[a-f0-9]{32}$/i.test(id) || // … or a MD5 string (default in the first versions of NodeJS backend)…
-    /^[a-z0-9]{6}$/i.test(id)
+    /^[a-z0-9]{6,8}$/i.test(id)
   );
 };
 
