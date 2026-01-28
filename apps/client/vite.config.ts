e8b9b8298c7045b5d5f24c54ed32ef14e3855520
Albinahmetaj <albingithub@gmail.com>
2026-01-28 11:13:41 +0100

    fix(vite): use relative base path to prevent assets 404s

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react()],
   server: {
     port: 3000,
