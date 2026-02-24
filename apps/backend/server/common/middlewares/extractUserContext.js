8dab65611d4598112b735d5d97338d117e427a3e
Jacob Wodzyński <jacob.wod@gmail.com>
2026-02-24 09:17:55 +0100

    Added AD_SKIP_LOOKUP_FOR_PATHS: - Since we now by default decorate _all_ requests with the user object, the lookup takes place even for paths that don't need to have it (e.g. health checks, robots.txt, etc). - This new option makes it possible to opt-out of the user lookup chain entirely, for specified paths.

--- apps/backend/server/common/middlewares/extractUserContext.js
+++ apps/backend/server/common/middlewares/extractUserContext.js
@@ -2,15 +2,22 @@ import ad from "../../apis/v2/services/activedirectory.service.js";
 import { requestContext } from "../utils/requestContext.js";
 
 export default function extractUserContext(req, res, next) {
-  // This was previously called all over the place whenever we needed user details
+  // If the requested path is in the AD_SKIP_LOOKUP_FOR_PATHS list, skip the lookup.
+  // Useful to e.g. silence logs for health check endpoints or robots.txt (as
+  // once we get into ad.getUserFromRequestHeader(), we'll see tons of logs in the TRACE level).
+  if (process.env.AD_SKIP_LOOKUP_FOR_PATHS?.split(",").includes(req.path)) {
+    return next();
+  }
+
+  // Call the real AD lookup function.
   const user = ad.getUserFromRequestHeader(req);
 
-  // Save the user to res.locals. Used everywhere in Express, cheap and safe.
+  // Save the user to res.locals. Used everywhere in Express middleware chain.
   res.locals.authUser = user;
 
-  // The new thing is that we now also save the user object into the request
-  // context's Store. This will give code that _doesn't_ have the access to
+  // Also, let's save the user object into the request context's Store.
+  // This will give code that _doesn't_ have the access to
   // res.locals access to the authenticated user object. One way we'll use it
-  // is in a logging middleware.
+  // is in a logging middleware (see: hajkLogger.js).
   requestContext.run({ user }, () => next());
 }
