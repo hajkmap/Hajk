hajkmap/Hajk

commit 078e211820c7f05fe6e44bcdbef9138d94be1acf

Admin: small additions to make life easier when setting up layers and maps. Layer type (e.g. WMS or WMTS) is shown next to the layer's name.

apps/admin/src/views/mapsettings.jsx
@@ -803,17 +803,13 @@ class Menu extends Component {
    */
   getLayerNameFromIdForDisplay(id) {
     var layer = this.props.model.get("layers").find((layer) => layer.id === id);
-    let ret = "";
     if (layer) {
-      if (layer.internalLayerName?.length > 0) {
-        ret = layer.internalLayerName;
-      } else {
-        ret = layer.caption;
-      }
+      return `${layer.internalLayerName || layer.caption} (${layer.type}) ${layer?.layers.length > 1 ? `(${layer.layers.length} underlager)` : ""}`;
+    
     } else {
-      ret = `---[layer id ${id} not found]---`;
+      return `---[layer id ${id} not found]---`;
     }
-    return ret;
+    
   }
 
   /**
@@ -1277,7 +1273,7 @@ class Menu extends Component {
 
       switch (layer.type) {
         case "WMS":
-          displayType = "";
+          displayType = "(WMS)";
           break;
         case "WMTS":
           displayType = "(WMTS)";
@@ -1292,6 +1288,7 @@ class Menu extends Component {
           break;
       }
 
+
       return (
         <li
           className="layer-item"
@@ -1301,10 +1298,15 @@ class Menu extends Component {
           <span className={cls} />
           &nbsp;
           <span className="main-box">
-            {layer.internalLayerName?.length > 0
-              ? layer.internalLayerName
-              : layer.caption}{" "}
-            {displayType}
+            {layer.internalLayerName || layer.caption}
+            <span className="layer-extra-info">
+              &nbsp;{displayType}
+              {layer?.layers?.length > 1 && (
+                <span className="sub-layers">
+                  &nbsp;({layer.layers.length} underlager)
+                </span>
+              )}
+            </span>
           </span>
         </li>
       );

apps/admin/src/index.css
@@ -498,7 +498,13 @@ body.dragging * {
   overflow: hidden;
 }
 .list-item li:hover {
-  background-color: #dedede;
+  background: #dedede;
+}
+
+.layer-extra-info {
+  font-size: 0.8em;
+  color: #666;
+  font-weight: normal;
 }
 
 .overflow-hidden {
@@ -911,56 +917,55 @@ figure img {
   background: rgba(0, 0, 0, 0.4);
 }
 
-.modal-dialog{
+.modal-dialog {
   margin-top: 1rem;
   margin-bottom: 1rem;
 }
 
-.modal-content{
+.modal-content {
   max-height: calc(100vh - 2rem);
   overflow-y: scroll;
 }
 
 .layerDialog label,
-.layerDialog input{
+.layerDialog input {
   margin: 0;
   margin-bottom: 0;
   margin-top: 0;
 }
 
-.layerDialog textarea.infoClick{
+.layerDialog textarea.infoClick {
   font-family: monospace;
   font-size: 0.8rem;
   height: 180px;
 }
 
-.layerDialog .form-row{
+.layerDialog .form-row {
   margin-bottom: 0.8rem;
 }
 
-
-.layerDialog div[class*="split"]{
+.layerDialog div[class*="split"] {
   display: flex;
   align-items: center;
 }
 
-.layerDialog div[class*="split"] > div:first-child{
+.layerDialog div[class*="split"] > div:first-child {
   padding-right: 1rem;
 }
 
-.layerDialog .split50 > div{
+.layerDialog .split50 > div {
   flex: 0 0 50%;
 }
 
-.layerDialog .split0 > div{
+.layerDialog .split0 > div {
   flex: 0 0 auto;
 }
 
-.layerDialog .split3070 > div:first-child{
+.layerDialog .split3070 > div:first-child {
   flex: 0 0 30%;
 }
 
-.layerDialog .split3070 > div:last-child{
+.layerDialog .split3070 > div:last-child {
   flex: 0 0 70%;
 }
 

apps/admin/src/views/layermanager.jsx
@@ -536,7 +536,7 @@ class Manager extends Component {
       var displayType = "";
       switch (layer.type) {
         case "WMS":
-          displayType = "";
+          displayType = "(WMS)";
           break;
         case "WMTS":
           displayType = "(WMTS)";
@@ -558,9 +558,9 @@ class Manager extends Component {
               {layer.internalLayerName?.length > 0
                 ? layer.internalLayerName
                 : layer.caption}{" "}
-              {displayType}
+              
             </span>
-            <span style={{ fontSize: "x-small" }}>{layer.id}</span>
+            <span style={{ fontSize: "x-small" }}>{displayType} ID: {layer.id}</span>
           </div>
           <div className="options-box">
             <i

commit bb6180dffbea881556fabf150861c59186ee0b5b

Hotfix: regression in 078e211820c7f05fe6e44bcdbef9138d94be1acf could result in an attempt to read `length` of undefined.

apps/admin/src/views/mapsettings.jsx
@@ -804,7 +804,7 @@ class Menu extends Component {
   getLayerNameFromIdForDisplay(id) {
     var layer = this.props.model.get("layers").find((layer) => layer.id === id);
     if (layer) {
-      return `${layer.internalLayerName || layer.caption} (${layer.type}) ${layer?.layers.length > 1 ? `(${layer.layers.length} underlager)` : ""}`;
+      return `${layer.internalLayerName || layer.caption} (${layer.type}) ${layer?.layers?.length > 1 ? `(${layer.layers.length} underlager)` : ""}`;
     
     } else {
       return `---[layer id ${id} not found]---`;