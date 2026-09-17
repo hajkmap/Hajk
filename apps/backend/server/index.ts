ba32ac60fbd91de3bdc1b58c55f51bc4c83acf28
Albinahmetaj <albingithub@gmail.com>
2026-09-17 09:22:45 +0200

    fix(dev): prevent server restart loop on unhandled errors

--- apps/backend/server/index.ts
+++ apps/backend/server/index.ts
@@ -3,8 +3,11 @@ import Server from "./common/server.ts";
 // We're gonna need to handle uncaught exceptions so that we avoid getting stuck in a restart loop
 if (process.env.NODE_ENV !== "production") {
   process.on("uncaughtException", (error) => {
-    console.error("Uncaught Exception:", error.message, "\n");
-    console.error("Stack Trace:", error.stack);
+    console.error("Uncaught Exception:", error);
+  });
+
+  process.on("unhandledRejection", (reason) => {
+    console.error("Unhandled Rejection:", reason);
   });
 }
 
